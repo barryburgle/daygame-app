2373cc3b9f3a3408cade82768631d2c6f6843d59
Barry Burgle <barryburgle@gmail.com>
2023-09-13 23:40:46 +0200

    removing private and using Doubles

--- src/main/model/Session.kt
+++ src/main/model/Session.kt
@@ -9,27 +9,27 @@ import java.time.temporal.WeekFields
 import java.util.Locale
 
 class Session(
-    private val insertTime: Instant,
-    private val date: LocalDate,
-    private val startHour: LocalTime,
-    private val endHour: LocalTime,
-    private val sets: Array<Set>,
-    private val convos: Array<Convo>,
-    private val contacts: Array<Contact>,
-    private val stickingPoints: String
+    val insertTime: Instant,
+    val date: LocalDate,
+    val startHour: LocalTime,
+    val endHour: LocalTime,
+    val sets: Array<Set>,
+    val convos: Array<Convo>,
+    val contacts: Array<Contact>,
+    val stickingPoints: String
 ) {
-    private var sessionTime: Long = startHour.until(endHour, ChronoUnit.SECONDS)
-    private var approachTime: Long = sessionTime / sets.size
-    private var convoRatio: Double = (convos.size / sets.size).toDouble()
-    private var rejectionRatio: Double = 1 - convoRatio
-    private var contactRatio: Double = (contacts.size / sets.size).toDouble()
-    private var index: Double = computeIndex()
-    private var dayOfWeek: DayOfWeek = date.dayOfWeek
-    private var weekNumber: Int = computeWeekOfYear()
+    val sessionTime: Long = startHour.until(endHour, ChronoUnit.SECONDS)
+    val approachTime: Long = sessionTime / sets.size
+    val convoRatio: Double = (convos.size.toDouble() / sets.size.toDouble()).toDouble()
+    val rejectionRatio: Double = 1 - convoRatio
+    val contactRatio: Double = (contacts.size.toDouble() / sets.size.toDouble()).toDouble()
+    val index: Double = computeIndex()
+    val dayOfWeek: DayOfWeek = date.dayOfWeek
+    val weekNumber: Int = computeWeekOfYear()
 
     private fun computeIndex(): Double {
         // TODO: create method for formula change
-        return (sets.size * (12 * sets.size + 20 * convos.size + 30 * contacts.size) / sessionTime).toDouble()
+        return (sets.size.toDouble() * (12 * sets.size + 20 * convos.size + 30 * contacts.size).toDouble() / sessionTime.toDouble())
     }
 
     private fun computeWeekOfYear(): Int {
