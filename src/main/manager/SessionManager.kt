7c99ccc8077f4d2a278026bba97cfc7199817cae
Barry Burgle <barryburgle@gmail.com>
2023-09-13 23:50:55 +0200

    todos

---
+++ src/main/manager/SessionManager.kt
@@ -0,0 +1,20 @@
+package main.manager
+
+import main.model.Session
+
+class SessionManager(
+    topics: Array<Session>?
+) {
+    // TODO: compute index moving average
+    // TODO: compute sets histogram
+    // TODO: compute convos histogram
+    // TODO: compute contacts histogram
+    // TODO: week total sets
+    // TODO: week total convos
+    // TODO: week total contacts
+    // TODO: week average index
+    // TODO: week convo ratio
+    // TODO: week contact ratio
+    // TODO: week total time spent
+    // TODO: week frequency
+}
