fee426f5b362e0fdf00372353d590444941b7feb
Barry Burgle <barryburgle@gmail.com>
2023-09-30 17:59:06 +0200

    batch session as abtract session

--- app/src/test/java/com/barryburgle/gameapp/model/BatchSessionTest.kt
+++ app/src/test/java/com/barryburgle/gameapp/model/BatchSessionTest.kt
@@ -1,4 +1,35 @@
 package com.barryburgle.gameapp.model
 
-class BatchSessionTest {
+import com.barryburgle.gameapp.model.session.BatchSession
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class BatchSessionTest : SessionTestUtils() {
+    val SETS = 3
+    val CONVOS = 2
+    val CONTACTS = 1
+
+
+    @Test
+    fun newSessionTest() {
+        val batchSession = BatchSession(
+            INSERT_TIME,
+            DATE,
+            START_HOUR,
+            END_HOUR,
+            SETS,
+            CONVOS,
+            CONTACTS,
+            STICKING_POINTS
+        )
+        assertInitProperties(batchSession)
+        assertSetsConvosContacts(batchSession)
+        assertComputedProperties(batchSession)
+    }
+
+    private fun assertSetsConvosContacts(batchSession: BatchSession) {
+        assertEquals(SETS, batchSession.sets)
+        assertEquals(CONVOS, batchSession.convos)
+        assertEquals(CONTACTS, batchSession.contacts)
+    }
 }
