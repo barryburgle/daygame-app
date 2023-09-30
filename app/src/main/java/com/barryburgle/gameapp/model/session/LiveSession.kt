3c457785b31a8ee6ae44b6724011bfc77491ec41
Barry Burgle <barryburgle@gmail.com>
2023-09-30 17:59:55 +0200

    live session as abtract session

--- app/src/main/java/com/barryburgle/gameapp/model/session/LiveSession.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/session/LiveSession.kt
@@ -3,28 +3,26 @@ package com.barryburgle.gameapp.model.session
 import com.barryburgle.gameapp.model.Contact
 import com.barryburgle.gameapp.model.Convo
 import com.barryburgle.gameapp.model.Set
-import java.time.DayOfWeek
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalTime
-import java.time.temporal.ChronoUnit
 
 class LiveSession(
-    val insertTime: Instant,
-    val date: LocalDate,
-    val startHour: LocalTime,
-    val endHour: LocalTime,
-    val sets: Array<Set>,
-    val convos: Array<Convo>,
-    val contacts: Array<Contact>,
-    val stickingPoints: String
-) : AbstractSession() {
-    val sessionTime: Long = startHour.until(endHour, ChronoUnit.SECONDS)
-    val approachTime: Long = sessionTime / sets.size
-    val convoRatio: Double = (convos.size.toDouble() / sets.size.toDouble()).toDouble()
-    val rejectionRatio: Double = 1 - convoRatio
-    val contactRatio: Double = (contacts.size.toDouble() / sets.size.toDouble()).toDouble()
-    val index: Double = computeIndex(sets.size, convos.size, contacts.size, sessionTime)
-    val dayOfWeek: DayOfWeek = date.dayOfWeek
-    val weekNumber: Int = computeWeekOfYear(date)
-}
+    insertTime: Instant,
+    date: LocalDate,
+    startHour: LocalTime,
+    endHour: LocalTime,
+    val setArray: Array<Set>,
+    val convoArray: Array<Convo>,
+    val contactArray: Array<Contact>,
+    stickingPoints: String
+) : AbstractSession(
+    insertTime,
+    date,
+    startHour,
+    endHour,
+    setArray.size,
+    convoArray.size,
+    contactArray.size,
+    stickingPoints
+)
--- app/src/test/java/com/barryburgle/gameapp/model/LiveSessionTest.kt
+++ app/src/test/java/com/barryburgle/gameapp/model/LiveSessionTest.kt
@@ -3,57 +3,36 @@ package com.barryburgle.gameapp.model
 import com.barryburgle.gameapp.model.session.LiveSession
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
-import java.time.DayOfWeek
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalTime
-import kotlin.math.roundToInt
 
-internal class LiveSessionTest {
-    val INSERT_TIME = Instant.now()
-    val DATE = LocalDate.of(2023, 9, 13)
-    val START_HOUR = LocalTime.of(16, 0, 0)
-    val END_HOUR = LocalTime.of(18, 0, 0)
+internal class LiveSessionTest : SessionTestUtils() {
     val SETS = Array(3) { Set() }
     val CONVOS = Array(2) { Convo() }
     val CONTACTS = Array(1) { Contact() }
-    val STICKING_POINTS = "sticking-points"
-    val SESSION_TIME: Long = 7200
-    val APPROACH_TIME: Long = 2400
-    val CONVO_RATIO = 0.666
-    val REJECTION_RATIO = 0.334
-    val CONTACT_RATIO = 0.334
-    val INDEX = 0.044
-    val DAY_OF_WEEK = DayOfWeek.WEDNESDAY
-    val WEEK_NUMBER = 37
 
 
     @Test
     fun newSessionTest() {
-        val liveSession = LiveSession(INSERT_TIME, DATE, START_HOUR, END_HOUR, SETS, CONVOS, CONTACTS, STICKING_POINTS)
+        val liveSession = LiveSession(
+            INSERT_TIME,
+            DATE,
+            START_HOUR,
+            END_HOUR,
+            SETS,
+            CONVOS,
+            CONTACTS,
+            STICKING_POINTS
+        )
         assertInitProperties(liveSession)
+        assertSetsConvosContacts(liveSession)
         assertComputedProperties(liveSession)
     }
 
-    private fun assertInitProperties(liveSession: LiveSession) {
-        assertEquals(INSERT_TIME, liveSession.insertTime)
-        assertEquals(DATE, liveSession.date)
-        assertEquals(START_HOUR, liveSession.startHour)
-        assertEquals(END_HOUR, liveSession.endHour)
-        assertEquals(SETS, liveSession.sets)
-        assertEquals(CONVOS, liveSession.convos)
-        assertEquals(CONTACTS, liveSession.contacts)
-        assertEquals(STICKING_POINTS, liveSession.stickingPoints)
-    }
-
-    private fun assertComputedProperties(liveSession: LiveSession) {
-        assertEquals(SESSION_TIME, liveSession.sessionTime)
-        assertEquals(APPROACH_TIME, liveSession.approachTime)
-        assertEquals((CONVO_RATIO * 100).roundToInt(), (liveSession.convoRatio * 100).roundToInt())
-        assertEquals((REJECTION_RATIO * 100).roundToInt(), (liveSession.rejectionRatio * 100).roundToInt())
-        assertEquals((CONTACT_RATIO * 100).roundToInt(), (liveSession.contactRatio * 100).roundToInt())
-        assertEquals((INDEX * 1000).roundToInt(), (liveSession.index * 1000).roundToInt())
-        assertEquals(DAY_OF_WEEK, liveSession.dayOfWeek)
-        assertEquals(WEEK_NUMBER, liveSession.weekNumber)
+    private fun assertSetsConvosContacts(liveSession: LiveSession) {
+        assertEquals(SETS.size, liveSession.sets)
+        assertEquals(SETS, liveSession.setArray)
+        assertEquals(CONVOS.size, liveSession.convos)
+        assertEquals(CONVOS, liveSession.convoArray)
+        assertEquals(CONTACTS.size, liveSession.contacts)
+        assertEquals(CONTACTS, liveSession.contactArray)
     }
 }
