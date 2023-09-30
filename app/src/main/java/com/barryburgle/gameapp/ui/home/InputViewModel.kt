a01d4f760c45d3e2446ce11bc9867534ba454e32
Barry Burgle <barryburgle@gmail.com>
2023-10-01 00:34:25 +0200

    class name change

--- app/src/main/java/com/barryburgle/gameapp/ui/home/InputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/home/InputViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class InputViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is input Fragment"
