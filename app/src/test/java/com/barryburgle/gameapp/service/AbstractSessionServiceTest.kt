9da0f993e8fa14b471e40dc68366c271ef98883e
Barry Burgle <barryburgle@gmail.com>
2023-10-02 23:27:37 +0200

    AbstractSessionService test

--- app/src/test/java/com/barryburgle/gameapp/service/AbstractSessionServiceTest.kt
+++ app/src/test/java/com/barryburgle/gameapp/service/AbstractSessionServiceTest.kt
@@ -1,4 +1,51 @@
 package com.barryburgle.gameapp.service
 
-class AbstractSessionServiceTest {
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.time.Month
+
+class AbstractSessionServiceTest : SessionServiceTestUtils() {
+
+    @Test
+    fun computeSessionTimeTest() {
+        assertEquals(SESSION_TIME, AbstractSessionService.computeSessionTime(START_HOUR, END_HOUR))
+    }
+
+    @Test
+    fun computeApproachTimeTest() {
+        assertEquals(APPROACH_TIME, AbstractSessionService.computeApproachTime(SESSION_TIME, SETS))
+    }
+
+    @Test
+    fun computeRejectionRatioTest() {
+        assertEquals(REJECTION_RATIO, AbstractSessionService.computeRejectionRatio(CONVOS, SETS))
+    }
+
+    @Test
+    fun computeContactRatioTest() {
+        assertEquals(CONTACT_RATIO, AbstractSessionService.computeContactRatio(CONTACTS, SETS))
+    }
+
+    @Test
+    fun computeDayOfWeekTest() {
+        assertEquals(DAY_OF_WEEK, AbstractSessionService.computeDayOfWeek(DATE))
+    }
+
+    @Test
+    fun computeIndexTest() {
+        assertEquals(
+            INDEX,
+            AbstractSessionService.computeIndex(SETS, CONVOS, CONTACTS, SESSION_TIME)
+        )
+    }
+
+    @Test
+    fun computeWeekOfYearTest() {
+        assertEquals(WEEK_NUMBER, AbstractSessionService.computeWeekOfYear(DATE))
+    }
+
+    @Test
+    fun computeMonthOfYearTest() {
+        assertEquals(Month.SEPTEMBER, AbstractSessionService.computeMonthOfYear(DATE))
+    }
 }
