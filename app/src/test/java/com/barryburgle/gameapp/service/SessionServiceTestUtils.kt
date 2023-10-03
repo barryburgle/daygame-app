685e347c44c975c349626919b985e63311932f3d
Barry Burgle <barryburgle@gmail.com>
2023-10-03 13:35:43 +0200

    new values

--- app/src/test/java/com/barryburgle/gameapp/service/SessionServiceTestUtils.kt
+++ app/src/test/java/com/barryburgle/gameapp/service/SessionServiceTestUtils.kt
@@ -1,10 +1,12 @@
 package com.barryburgle.gameapp.service
 
 import java.time.DayOfWeek
+import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalTime
 
 open class SessionServiceTestUtils {
+    val INSERT_TIME = Instant.now()
     val DATE = LocalDate.of(2023, 9, 13)
     val START_HOUR = LocalTime.of(16, 0, 0)
     val END_HOUR = LocalTime.of(18, 0, 0)
@@ -19,4 +21,5 @@ open class SessionServiceTestUtils {
     val SETS = 3
     val CONVOS = 2
     val CONTACTS = 1
+    val STICKING_POINTS = "sticking-points"
 }
