284a1033091056d5c61b59dbbb62a09bbafae961
Barry Burgle <barryburgle@gmail.com>
2023-10-03 13:29:16 +0200

    live initializer

--- app/src/main/java/com/barryburgle/gameapp/service/live/LiveSessionInitializer.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/live/LiveSessionInitializer.kt
@@ -1,4 +1,22 @@
-package com.barryburgle.gameapp.service
+package com.barryburgle.gameapp.service.live
+
+import com.barryburgle.gameapp.model.Contact
+import com.barryburgle.gameapp.model.Convo
+import com.barryburgle.gameapp.model.Set
+import com.barryburgle.gameapp.model.session.AbstractSession
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
 
 interface LiveSessionInitializer {
+    fun init(
+        insertTime: Instant,
+        date: LocalDate,
+        startHour: LocalTime,
+        endHour: LocalTime,
+        setArray: Array<Set>,
+        convoArray: Array<Convo>,
+        contactArray: Array<Contact>,
+        stickingPoints: String
+    ): AbstractSession
 }
