28a54080183302e7754ef0e6c3baa793397101b6
Barry Burgle <barryburgle@gmail.com>
2023-10-04 14:53:29 +0200

    new dao methods

--- app/src/main/java/com/barryburgle/gameapp/dao/session/AbstractSessionDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/session/AbstractSessionDao.kt
@@ -1,19 +1,39 @@
 package com.barryburgle.gameapp.dao.session
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
 import com.barryburgle.gameapp.model.session.AbstractSession
+import java.time.LocalDate
 
-class AbstractSessionDao {
+
+@Dao
+interface AbstractSessionDao {
     companion object {
         val abstractSessionList = mutableListOf<AbstractSession>()
 
         fun insertSession(abstractSession: AbstractSession) {
-            // TODO: substitute with real db insert
+            // TODO: delete
             abstractSessionList.add(abstractSession)
         }
 
         fun selectLastSession(): List<AbstractSession> {
-            // TODO: substitute with real db select
+            // TODO: delete
             return abstractSessionList.takeLast(4)
         }
     }
+
+
+    @Insert(onConflict = REPLACE)
+    fun insert(abstractSession: AbstractSession)
+
+    @Query("SELECT * from abstract_session")
+    fun getAll(): List<AbstractSession>
+
+    @Query("SELECT * from abstract_session WHERE session_date = :sessionDate")
+    fun getByDate(sessionDate: LocalDate): List<AbstractSession>
+
+    @Query("SELECT * from abstract_session ORDER BY insert_time LIMIT :limit")
+    fun selectLastNSessions(limit: Int)
 }
