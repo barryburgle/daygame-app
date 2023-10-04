99f6b2b62c075e8d6c91edc99a1371f4db47d90c
Barry Burgle <barryburgle@gmail.com>
2023-10-04 14:59:03 +0200

    database singleton

--- app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
+++ app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
@@ -1,4 +1,34 @@
 package com.barryburgle.gameapp.database.session
 
-class AbstractSessionDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.model.session.AbstractSession
+
+@Database(entities = arrayOf(AbstractSession::class), version = 1)
+abstract class GameAppDatabase : RoomDatabase() {
+    abstract fun abstractSessionDao(): AbstractSessionDao
+
+    companion object {
+        private var INSTANCE: GameAppDatabase? = null
+
+        fun getInstance(context: Context): GameAppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(GameAppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        GameAppDatabase::class.java, "game_app_db"
+                    )
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
