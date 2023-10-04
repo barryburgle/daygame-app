d74099d61639ca73d9efa6b79dbb0abe9fdb1a11
Barry Burgle <barryburgle@gmail.com>
2023-10-04 14:58:14 +0200

    database worker

--- app/src/main/java/com/barryburgle/gameapp/worker/DatabaseWorker.kt
+++ app/src/main/java/com/barryburgle/gameapp/worker/DatabaseWorker.kt
@@ -1,4 +1,19 @@
 package com.barryburgle.gameapp.worker
 
-class DatabaseWorker {
+import android.os.Handler
+import android.os.HandlerThread
+
+class DatabaseWorker(threadName: String) : HandlerThread(threadName) {
+
+    private lateinit var mWorkerHandler: Handler
+
+    override fun onLooperPrepared() {
+        super.onLooperPrepared()
+        mWorkerHandler = Handler(looper)
+    }
+
+    fun postTask(task: Runnable) {
+        mWorkerHandler.post(task)
+    }
+
 }
