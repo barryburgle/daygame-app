3aa1dfebbd3d4d31016318c4e7f6f60d28333a56
Barry Burgle <barryburgle@gmail.com>
2023-10-13 00:03:02 +0200

    first theme

--- app/src/main/java/com/barryburgle/gameapp/ui/theme/GameAppOriginalTheme.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/theme/GameAppOriginalTheme.kt
@@ -1,4 +1,50 @@
 package com.barryburgle.gameapp.ui.theme
 
-class GameAppOriginalTheme {
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Dark,
+    primaryVariant = Mid,
+    secondary = Clear,
+    background = Color.Black
+)
+
+private val LightColorPalette = lightColors(
+    primary = Dark,
+    primaryVariant = Dark,
+    secondary = Dark,
+    background = Color.White
+)
+
+// TODO: set the following properties and create complete light and dark themes
+/* Other default colors to override
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
+
+@Composable
+fun GameAppOriginalTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
 }
