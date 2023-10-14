52f5286fdf0487435af8272bf4dd5a0332dc2528
Barry Burgle <barryburgle@gmail.com>
2023-10-14 10:03:37 +0200

    new services interface

--- app/src/main/java/com/barryburgle/gameapp/service/SessionService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/SessionService.kt
@@ -1,4 +1,14 @@
 package com.barryburgle.gameapp.service
 
+import com.barryburgle.gameapp.model.session.AbstractSession
+
 interface SessionService {
+
+    // TODO: evaluate if use this interface for services or not
+
+    fun saveSession(
+        abstractSession: AbstractSession
+    )
+
+    fun getSessions(): List<AbstractSession>
 }
