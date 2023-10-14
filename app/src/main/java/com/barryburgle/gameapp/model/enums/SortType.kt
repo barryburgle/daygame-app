39dfce9566b54681b7b2c9cea441cd844399ddeb
Barry Burgle <barryburgle@gmail.com>
2023-10-14 09:55:26 +0200

    sort types

--- app/src/main/java/com/barryburgle/gameapp/model/enums/SortType.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/SortType.kt
@@ -1,4 +1,16 @@
 package com.barryburgle.gameapp.model.enums
 
-enum class SortType {
+enum class SortType(val field: String) {
+    DATE("Date"),
+    SETS("Sets"),
+    CONVOS("Convos"),
+    CONTACS("Contacts"),
+    SESSION_TIME("Session Time"),
+    APPROACH_TIME("Approach Time"),
+    CONVO_RATIO("Convo Ratio"),
+    REJECTION_RATIO("Rejection Ratio"),
+    CONTACT_RATIO("Contact Ratio"),
+    INDEX("Index"),
+    DAY_OF_WEEK("Day of Week"),
+    WEEK_NUMBER("Week Number");
 }
