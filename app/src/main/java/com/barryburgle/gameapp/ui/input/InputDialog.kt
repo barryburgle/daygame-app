c67d852f3e5c358fff40c5c1e0537a071573e2d7
Barry Burgle <barryburgle@gmail.com>
2023-10-14 11:12:16 +0200

    input dialog with time formats

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputDialog.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputDialog.kt
@@ -1,4 +1,72 @@
 package com.barryburgle.gameapp.ui.input
 
-class InputDialog {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.barryburgle.gameapp.event.AbstractSessionEvent
+import com.barryburgle.gameapp.ui.input.state.InputState
+
+@Composable
+fun AddInputDialog(
+    state: InputState,
+    onEvent: (AbstractSessionEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = {
+            onEvent(AbstractSessionEvent.HideDialog)
+        },
+        title = {
+            Text(text = "Add session")
+        },
+        text = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                TextField(value = state.date,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetDate(it)) },
+                    placeholder = { Text(text = "yyyy-MM-dd date") })
+                TextField(value = state.startHour,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetStartHour(it)) },
+                    placeholder = { Text(text = "hh:mm:ss Start Hour") })
+                TextField(value = state.endHour,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetEndHour(it)) },
+                    placeholder = { Text(text = "hh:mm:ss End Hour") })
+                TextField(value = state.sets,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetSets(it)) },
+                    placeholder = { Text(text = "Sets") })
+                TextField(value = state.convos,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetConvos(it)) },
+                    placeholder = { Text(text = "Convos") })
+                TextField(value = state.contacts,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetContacts(it)) },
+                    placeholder = { Text(text = "Contacts") })
+                TextField(value = state.stickingPoints,
+                    onValueChange = { onEvent(AbstractSessionEvent.SetStickingPoints(it)) },
+                    placeholder = { Text(text = "Sticking Points") })
+            }
+        },
+        buttons = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Button(onClick = {
+                    onEvent(AbstractSessionEvent.SaveAbstractSession)
+                }) {
+                    Text(text = "Save")
+                }
+            }
+        }
+    )
 }
