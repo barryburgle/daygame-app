8f70b47a2b063ce93484afb1c7225bc0014d72d0
Barry Burgle <barryburgle@gmail.com>
2023-10-14 09:57:50 +0200

    old queries delete and new queries introduction

--- app/src/main/java/com/barryburgle/gameapp/dao/session/AbstractSessionDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/session/AbstractSessionDao.kt
@@ -1,39 +1,60 @@
 package com.barryburgle.gameapp.dao.session
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import com.barryburgle.gameapp.model.session.AbstractSession
-import java.time.LocalDate
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface AbstractSessionDao {
-    companion object {
-        val abstractSessionList = mutableListOf<AbstractSession>()
-
-        fun insertSession(abstractSession: AbstractSession) {
-            // TODO: delete
-            abstractSessionList.add(abstractSession)
-        }
-
-        fun selectLastSession(): List<AbstractSession> {
-            // TODO: delete
-            return abstractSessionList.takeLast(4)
-        }
-    }
-
 
     @Insert(onConflict = REPLACE)
-    fun insert(abstractSession: AbstractSession)
+    suspend fun insert(abstractSession: AbstractSession)
+
+    @Delete
+    suspend fun delete(abstractSession: AbstractSession)
 
     @Query("SELECT * from abstract_session")
-    fun getAll(): List<AbstractSession>
+    fun getAll(): Flow<List<AbstractSession>>
 
-    @Query("SELECT * from abstract_session WHERE session_date = :sessionDate")
-    fun getByDate(sessionDate: LocalDate): List<AbstractSession>
+    @Query("SELECT * from abstract_session ORDER BY session_date")
+    fun getByDate(): Flow<List<AbstractSession>>
 
-    @Query("SELECT * from abstract_session ORDER BY insert_time LIMIT :limit")
-    fun selectLastNSessions(limit: Int): List<AbstractSession>
+    @Query("SELECT * from abstract_session ORDER BY sets")
+    fun getBySets(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY convos")
+    fun getByConvos(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY contacts")
+    fun getByContacts(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY session_time")
+    fun getBySessionTime(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY approach_time")
+    fun getByApproachTime(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY convo_ratio")
+    fun getByConvoRatio(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY rejection_ratio")
+    fun getByRejectionRatio(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY contact_ratio")
+    fun getByContactRatio(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY `index`")
+    fun getByIndex(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY day_of_week")
+    fun getByDayOfWeek(): Flow<List<AbstractSession>>
+
+    @Query("SELECT * from abstract_session ORDER BY week_number")
+    fun getByWeekNumber(): Flow<List<AbstractSession>>
+
 }
