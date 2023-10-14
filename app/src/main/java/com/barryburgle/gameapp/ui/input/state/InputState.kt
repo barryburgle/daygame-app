ed724f6a52f8dba248310c3ca282807d90cd15d9
Barry Burgle <barryburgle@gmail.com>
2023-10-14 14:35:10 +0200

    input state

--- app/src/main/java/com/barryburgle/gameapp/ui/input/state/InputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/state/InputState.kt
@@ -1,3 +1,17 @@
 package com.barryburgle.gameapp.ui.input.state
 
-data class InputState()
+import com.barryburgle.gameapp.model.enums.SortType
+import com.barryburgle.gameapp.model.session.AbstractSession
+
+data class InputState(
+    val abstractSessions: List<AbstractSession> = emptyList(),
+    var date: String = "",
+    var startHour: String = "",
+    var endHour: String = "",
+    var sets: String = "",
+    var convos: String = "",
+    var contacts: String = "",
+    var stickingPoints: String = "",
+    val sortType: SortType = SortType.DATE,
+    val isAddingSession: Boolean = false
+)
