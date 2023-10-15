65604a0bf400840dc25d3f791b0c65081327b158
Barry Burgle <barryburgle@gmail.com>
2023-10-15 10:24:11 +0200

    passing strings and numbers to parent class

--- app/src/main/java/com/barryburgle/gameapp/model/session/LiveSession.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/session/LiveSession.kt
@@ -3,19 +3,15 @@ package com.barryburgle.gameapp.model.session
 import com.barryburgle.gameapp.model.Contact
 import com.barryburgle.gameapp.model.Convo
 import com.barryburgle.gameapp.model.Set
-import java.time.DayOfWeek
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalTime
 
 class LiveSession(
-    insertTime: Instant,
-    date: LocalDate,
-    startHour: LocalTime,
-    endHour: LocalTime,
-    val setArray: Array<Set>,
-    val convoArray: Array<Convo>,
-    val contactArray: Array<Contact>,
+    insertTime: String,
+    date: String,
+    startHour: String,
+    endHour: String,
+    setArray: Array<Set>,
+    convoArray: Array<Convo>,
+    contactArray: Array<Contact>,
     stickingPoints: String,
     sessionTime: Long,
     approachTime: Long,
@@ -23,7 +19,7 @@ class LiveSession(
     rejectionRatio: Double,
     contactRatio: Double,
     index: Double,
-    dayOfWeek: DayOfWeek,
+    dayOfWeek: Int,
     weekNumber: Int
 ) : AbstractSession(
     null,
