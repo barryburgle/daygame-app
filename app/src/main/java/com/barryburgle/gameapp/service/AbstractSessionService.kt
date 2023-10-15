25372e654816c0d42656481f9989af7f9852a86d
Barry Burgle <barryburgle@gmail.com>
2023-10-15 10:29:01 +0200

    added val useful to child classes

--- app/src/main/java/com/barryburgle/gameapp/service/AbstractSessionService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/AbstractSessionService.kt
@@ -6,12 +6,17 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.Month
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.time.temporal.WeekFields
 import java.util.Locale
 
 open class AbstractSessionService {
     companion object {
+        val DATE_SUFFIX = "T00:00:00.000+00"
+        val TIME_SUFFIX = ".000+00"
+        val SEPARATOR = "T"
+        val savingFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
         val roundingMode: RoundingMode = RoundingMode.HALF_UP
         val scale: Int = 3
 
