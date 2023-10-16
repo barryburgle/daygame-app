273501f7e2c473cc9f5b78ddafe318e77e7f0fb0
Barry Burgle <barryburgle@gmail.com>
2023-10-16 15:44:41 +0200

    new material 3 dark and light themes

--- app/src/main/java/com/barryburgle/gameapp/ui/theme/GameAppOriginalTheme.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/theme/GameAppOriginalTheme.kt
@@ -1,48 +1,85 @@
 package com.barryburgle.gameapp.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
-    primary = Colors.Dark,
-    primaryVariant = Colors.Mid,
-    secondary = Colors.Clear,
-    background = Color.Black
+private val DarkColorPalette = darkColorScheme(
+    primary = Green80,
+    onPrimary = Green20,
+    primaryContainer = Green30,
+    onPrimaryContainer = Green90,
+    inversePrimary = Green40,
+    secondary = DarkGreen80,
+    onSecondary = DarkGreen20,
+    secondaryContainer = DarkGreen30,
+    onSecondaryContainer = DarkGreen90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = Green10,
+    onBackground = Green90,
+    surface = GreenGrey30,
+    onSurface = GreenGrey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey10,
+    surfaceVariant = GreenGrey30,
+    onSurfaceVariant = GreenGrey80,
+    outline = GreenGrey80
 )
 
-private val LightColorPalette = lightColors(
-    primary = Colors.Dark,
-    primaryVariant = Colors.Dark,
-    secondary = Colors.Dark,
-    background = Color.White
+private val LightColorPalette = lightColorScheme(
+    primary = Green40,
+    onPrimary = Color.White,
+    primaryContainer = Green90,
+    onPrimaryContainer = Green10,
+    inversePrimary = Green80,
+    secondary = DarkGreen40,
+    onSecondary = Color.White,
+    secondaryContainer = DarkGreen90,
+    onSecondaryContainer = DarkGreen10,
+    tertiary = Violet40,
+    onTertiary = Color.White,
+    tertiaryContainer = Violet90,
+    onTertiaryContainer = Violet10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = GreenGrey90,
+    onSurface = GreenGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = GreenGrey90,
+    onSurfaceVariant = GreenGrey30,
+    outline = GreenGrey50
 )
-
-// TODO: set the following properties and create complete light and dark themes
-/* Other default colors to override
-surface = Color.White,
-onPrimary = Color.White,
-onSecondary = Color.Black,
-onBackground = Color.Black,
-onSurface = Color.Black,
-*/
 
 @Composable
 fun GameAppOriginalTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val useDynamicColors = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when (darkTheme) {
+        (useDynamicColors && darkTheme) -> dynamicDarkColorScheme(LocalContext.current)
+        (useDynamicColors && !darkTheme) -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
