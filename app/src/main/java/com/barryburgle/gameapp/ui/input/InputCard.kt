c196677d1481139e54f4995c86f54a7ee49e3df4
Barry Burgle <barryburgle@gmail.com>
2023-10-16 16:11:54 +0200

    added button and renamed function

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
@@ -4,21 +4,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.barryburgle.gameapp.event.AbstractSessionEvent
 import com.barryburgle.gameapp.model.session.AbstractSession
 import java.time.DayOfWeek
 
 @ExperimentalMaterial3Api
 @Composable
-fun ImageCard(
+fun InputCard(
     abstractSession: AbstractSession,
+    onEvent: (AbstractSessionEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -61,6 +67,18 @@ fun ImageCard(
                     )
                 }",
                 style = MaterialTheme.typography.bodySmall
+            )
+        }
+        IconButton(onClick = {
+            onEvent(
+                AbstractSessionEvent.DeleteSession(
+                    abstractSession
+                )
+            )
+        }) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete Session"
             )
         }
     }
