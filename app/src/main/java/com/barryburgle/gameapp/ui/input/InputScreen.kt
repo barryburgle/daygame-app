f00c1a0d26a5e03c6a24c866afb3b798e0010bc5
Barry Burgle <barryburgle@gmail.com>
2023-10-16 16:05:53 +0200

    material 3 migration

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
@@ -12,15 +12,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,6 +32,7 @@ import com.barryburgle.gameapp.model.enums.SortType
 import com.barryburgle.gameapp.service.FormatService
 import com.barryburgle.gameapp.ui.input.state.InputState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputScreen(
     state: InputState, onEvent: (AbstractSessionEvent) -> Unit
