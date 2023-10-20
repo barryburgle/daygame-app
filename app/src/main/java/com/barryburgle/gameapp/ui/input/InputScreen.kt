d4cb7c469e8f00c4f0c03fec60d991ef66423a64
Barry Burgle <barryburgle@gmail.com>
2023-10-20 12:11:00 +0200

    navigation bar

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
@@ -14,21 +14,36 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.event.AbstractSessionEvent
 import com.barryburgle.gameapp.model.enums.SortType
+import com.barryburgle.gameapp.ui.input.state.BottomNavigationItem
 import com.barryburgle.gameapp.ui.input.state.InputState
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,10 +51,67 @@ import com.barryburgle.gameapp.ui.input.state.InputState
 fun InputScreen(
     state: InputState, onEvent: (AbstractSessionEvent) -> Unit
 ) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Input",
+            selectedIcon = Icons.Filled.Edit,
+            unselectedIcon = Icons.Outlined.Edit,
+            hasNews = false
+        ),
+        BottomNavigationItem(
+            title = "Output",
+            selectedIcon = Icons.Filled.Star,
+            unselectedIcon = Icons.Outlined.Star,
+            hasNews = false
+        ),
+        BottomNavigationItem(
+            title = "Tools",
+            selectedIcon = Icons.Filled.Build,
+            unselectedIcon = Icons.Outlined.Build,
+            hasNews = false
+        )
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
     Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                            // navController.navigate(item.title)
+                        },
+                        label = { Text(text = item.title) },
+                        icon = {
+                            BadgedBox(badge = {
+                                if (item.badgeCount != null) {
+                                    Badge {
+                                        Text(text = item.badgeCount.toString())
+                                    }
+                                } else if (item.hasNews) {
+                                    Badge()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        item.selectedIcon
+                                    } else {
+                                        item.unselectedIcon
+                                    }, contentDescription = item.title
+                                )
+                            }
+                        })
+                }
+            }
+        },
         floatingActionButton = {
-            FloatingActionButton(modifier = Modifier
-                .offset(y = (-50).dp), onClick = { onEvent(AbstractSessionEvent.ShowDialog) }) {
+            FloatingActionButton(
+                onClick = { onEvent(AbstractSessionEvent.ShowDialog) }) {
                 Icon(
                     imageVector = Icons.Default.Add,
                     contentDescription = "Add a session"
