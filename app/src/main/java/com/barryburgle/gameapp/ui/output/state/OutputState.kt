04cfe3c52f7a9fec6f4e62e05a36cda73cf3abe6
Barry Burgle <barryburgle@gmail.com>
2023-10-24 09:31:29 +0200

    state needs list of all sessions

--- app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
@@ -1,7 +1,9 @@
 package com.barryburgle.gameapp.ui.output.state
 
 import com.barryburgle.gameapp.model.enums.ChartType
+import com.barryburgle.gameapp.model.session.AbstractSession
 
 data class OutputState(
+    val abstractSessions: List<AbstractSession> = emptyList(),
     val chartType: ChartType = ChartType.ABSOLUTE
 )
