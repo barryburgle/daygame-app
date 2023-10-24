27c40eab111942de2518f9302659059611fd42e3
Barry Burgle <barryburgle@gmail.com>
2023-10-24 11:05:45 +0200

    state to pass abstract session list

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
@@ -1,13 +1,26 @@
 package com.barryburgle.gameapp.ui.output
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.model.enums.ChartType
+import com.barryburgle.gameapp.ui.output.state.OutputState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
-class OutputViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is output Fragment"
-    }
-    val text: LiveData<String> = _text
+class OutputViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewModel() {
+    private val _state = MutableStateFlow(OutputState())
+    private val _chartType = MutableStateFlow(ChartType.ABSOLUTE)
+    private val _abstractSessions = abstractSessionDao.getAll()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    // TODO: create a dao method to get last n sessions and then let user set n from tools screen
+    val state =
+        combine(_state, _chartType, _abstractSessions) { state, chartType, abstractSessions ->
+            state.copy(
+                abstractSessions = abstractSessions,
+                chartType = chartType
+            )
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OutputState())
 }
