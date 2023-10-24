320d55b984a5208f244d44bba36b4868d4f52f72
Barry Burgle <barryburgle@gmail.com>
2023-10-24 23:44:17 +0200

    removed chart selector and introduced new flags to specify chart settings

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
@@ -1,21 +1,14 @@
 package com.barryburgle.gameapp.ui.output
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.event.AbstractSessionEvent
-import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.ui.output.state.OutputState
 import com.github.mikephil.charting.data.BarEntry
 
@@ -33,7 +25,7 @@ import com.github.mikephil.charting.data.BarEntry
 fun OutputScreen(
     state: OutputState, onEvent: (AbstractSessionEvent) -> Unit
 ) {
-    val spaceFromTop = 80.dp // TODO: centralize across screens
+    val spaceFromTop = 5.dp // TODO: centralize across screens
     val spaceFromBottom = 60.dp // TODO: centralize across screens
     // TODO: make cards with injectable type of charts
     // TODO: make different types of charts injectable with arrays
@@ -41,54 +33,15 @@ fun OutputScreen(
     Scaffold {
         LazyColumn(
             contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(50.dp),
-            modifier = Modifier.background(MaterialTheme.colorScheme.primary)
-        ) {
-            item {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .offset(y = 10.dp)
-                    ) {
-                        ChartType.values().forEach { chartType ->
-                            Row(
-                                modifier = Modifier
-                                    .clickable {
-                                        // TODO: show different charts
-                                    },
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                RadioButton(
-                                    selected = state.chartType == chartType, onClick = {
-                                        // TODO: show different charts
-                                    },
-                                    colors = RadioButtonDefaults.colors(
-                                        selectedColor = MaterialTheme.colorScheme.secondary,
-                                        unselectedColor = MaterialTheme.colorScheme.surface
-                                    )
-                                )
-                                Text(
-                                    text = chartType.field,
-                                    color = MaterialTheme.colorScheme.secondary
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
             modifier = Modifier
-                .fillMaxSize()
-                .offset(y = spaceFromTop),
+                .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             if (state.abstractSessions.isNotEmpty()) {
                 // TODO: refactor all the following labdas inside items to be called passing the abstract session attribute + description
+                item {
+                    Row(modifier = Modifier.height(spaceFromTop)) {}
+                }
                 item {
                     state.abstractSessions.map { abstractSession ->
                         abstractSession.id?.toInt()?.let {
@@ -100,7 +53,9 @@ fun OutputScreen(
                     }?.let { it ->
                         OutputCard(
                             chartLabel = "Sets",
-                            barEntryList = it as List<BarEntry>
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
                         )
                     }
                 }
@@ -115,7 +70,9 @@ fun OutputScreen(
                     }?.let { it ->
                         OutputCard(
                             chartLabel = "Conversations",
-                            barEntryList = it as List<BarEntry>
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
                         )
                     }
                 }
@@ -130,7 +87,9 @@ fun OutputScreen(
                     }?.let { it ->
                         OutputCard(
                             chartLabel = "Contacts",
-                            barEntryList = it as List<BarEntry>
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
                         )
                     }
                 }
@@ -145,8 +104,10 @@ fun OutputScreen(
                         }
                     }?.let { it ->
                         OutputCard(
-                            chartLabel = "Index [to move]",
-                            barEntryList = it as List<BarEntry>
+                            chartLabel = "Index",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = false
                         )
                     }
                 }
@@ -160,8 +121,10 @@ fun OutputScreen(
                         }
                     }?.let { it ->
                         OutputCard(
-                            chartLabel = "Approach Time [to move]",
-                            barEntryList = it as List<BarEntry>
+                            chartLabel = "Approach Time",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
                         )
                     }
                 }
@@ -176,8 +139,10 @@ fun OutputScreen(
                         }
                     }?.let { it ->
                         OutputCard(
-                            chartLabel = "Conversation Ratio [to move]",
-                            barEntryList = it as List<BarEntry>
+                            chartLabel = "Conversation Ratio",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = true
                         )
                     }
                 }
@@ -192,8 +157,10 @@ fun OutputScreen(
                         }
                     }?.let { it ->
                         OutputCard(
-                            chartLabel = "Contact Ratio [to move]",
-                            barEntryList = it as List<BarEntry>
+                            chartLabel = "Contact Ratio",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = true
                         )
                     }
                 }
