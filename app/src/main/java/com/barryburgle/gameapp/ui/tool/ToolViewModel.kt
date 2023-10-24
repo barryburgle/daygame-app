8c741f05987abe6874004cedd637064807e9b650
Barry Burgle <barryburgle@gmail.com>
2023-10-24 09:29:49 +0200

    importing tools state

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
@@ -3,9 +3,13 @@ package com.barryburgle.gameapp.ui.tool
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.ui.tool.state.ToolsState
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class ToolViewModel : ViewModel() {
-
+class ToolViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewModel() {
+    private val _state = MutableStateFlow(ToolsState())
+    val state = _state
     private val _text = MutableLiveData<String>().apply {
         value = "This is tool Fragment"
     }
