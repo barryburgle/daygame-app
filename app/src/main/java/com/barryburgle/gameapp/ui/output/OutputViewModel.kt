21fd790dca7ad882957153ed249cd5dd12e70103
Barry Burgle <barryburgle@gmail.com>
2023-10-29 17:19:13 +0100

    fetching by weeks and months and passing to state

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
@@ -3,7 +3,8 @@ package com.barryburgle.gameapp.ui.output
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
-import com.barryburgle.gameapp.dao.session.WeekStatDao
+import com.barryburgle.gameapp.dao.session.AggregatedStatDao
+import com.barryburgle.gameapp.event.ChartTypeEvent
 import com.barryburgle.gameapp.manager.SessionManager
 import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.ui.output.state.OutputState
@@ -14,21 +15,38 @@ import kotlinx.coroutines.flow.stateIn
 
 class OutputViewModel(
     private val abstractSessionDao: AbstractSessionDao,
-    private val weekStatDao: WeekStatDao
+    private val aggregatedStatDao: AggregatedStatDao
 ) : ViewModel() {
     private val _state = MutableStateFlow(OutputState())
-    private val _chartType = MutableStateFlow(ChartType.ABSOLUTE)
+    private val _chartType = MutableStateFlow(ChartType.SESSION)
     private val _abstractSessions = abstractSessionDao.getAll(14)
-    private val _weekStats = weekStatDao.groupSetsByWeekNumber()
+    private val _weekStats = aggregatedStatDao.groupStatsByWeekNumber()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    private val _monthStats = aggregatedStatDao.groupStatsByMonth()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     // TODO: create a dao method to get last n sessions and then let user set n from tools screen
-    val state =
-        combine(_state, _chartType, _abstractSessions, _weekStats) { state, chartType, abstractSessions, weekStats ->
-            state.copy(
-                abstractSessions = SessionManager.normalizeSessionsIds(abstractSessions),
-                chartType = chartType,
-                weekStats = SessionManager.normalizeWeekNumbers(weekStats)
-            )
-        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OutputState())
+    val state = combine(
+        _state,
+        _abstractSessions,
+        _weekStats,
+        _monthStats,
+        _chartType
+    ) { state, abstractSessions, weekStats, monthStats, chartType ->
+        state.copy(
+            abstractSessions = SessionManager.normalizeSessionsIds(abstractSessions),
+            weekStats = SessionManager.normalizeIds(weekStats),
+            monthStats = SessionManager.normalizeIds(monthStats),
+            chartType = chartType
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OutputState())
+
+
+    fun onEvent(event: ChartTypeEvent) {
+        when (event) {
+            is ChartTypeEvent.SortCharts -> {
+                _chartType.value = event.chartType
+            }
+        }
+    }
 }
