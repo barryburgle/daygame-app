1fe390d6f7d2a8839a5d667ebc7b5bd0988d4184
Barry Burgle <barryburgle@gmail.com>
2023-10-29 12:30:24 +0100

    setting up week stats in state

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
@@ -3,6 +3,7 @@ package com.barryburgle.gameapp.ui.output
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.dao.session.WeekStatDao
 import com.barryburgle.gameapp.manager.SessionManager
 import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.ui.output.state.OutputState
@@ -11,17 +12,23 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
-class OutputViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewModel() {
+class OutputViewModel(
+    private val abstractSessionDao: AbstractSessionDao,
+    private val weekStatDao: WeekStatDao
+) : ViewModel() {
     private val _state = MutableStateFlow(OutputState())
     private val _chartType = MutableStateFlow(ChartType.ABSOLUTE)
-    private val _abstractSessions = abstractSessionDao.getAll()
+    private val _abstractSessions = abstractSessionDao.getAll(14)
+    private val _weekStats = weekStatDao.groupSetsByWeekNumber()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
     // TODO: create a dao method to get last n sessions and then let user set n from tools screen
     val state =
-        combine(_state, _chartType, _abstractSessions) { state, chartType, abstractSessions ->
+        combine(_state, _chartType, _abstractSessions, _weekStats) { state, chartType, abstractSessions, weekStats ->
             state.copy(
                 abstractSessions = SessionManager.normalizeSessionsIds(abstractSessions),
-                chartType = chartType
+                chartType = chartType,
+                weekStats = SessionManager.normalizeWeekNumbers(weekStats)
             )
         }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OutputState())
 }
--- app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
@@ -1,9 +1,11 @@
 package com.barryburgle.gameapp.ui.output.state
 
+import com.barryburgle.gameapp.model.stat.WeekStat
 import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 data class OutputState(
     val abstractSessions: List<AbstractSession> = emptyList(),
+    val weekStats: List<WeekStat> = emptyList(),
     val chartType: ChartType = ChartType.ABSOLUTE
 )
