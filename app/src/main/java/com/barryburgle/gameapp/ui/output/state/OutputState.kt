6bcf085b54a7ccbdb2c2156d044e34e24e18952e
Barry Burgle <barryburgle@gmail.com>
2023-10-29 17:19:36 +0100

    storing by weeks and months

--- app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
@@ -1,11 +1,12 @@
 package com.barryburgle.gameapp.ui.output.state
 
-import com.barryburgle.gameapp.model.stat.WeekStat
 import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.model.session.AbstractSession
+import com.barryburgle.gameapp.model.stat.AggregatedStat
 
 data class OutputState(
+    val chartType: ChartType = ChartType.SESSION,
     val abstractSessions: List<AbstractSession> = emptyList(),
-    val weekStats: List<WeekStat> = emptyList(),
-    val chartType: ChartType = ChartType.ABSOLUTE
+    val weekStats: List<AggregatedStat> = emptyList(),
+    val monthStats: List<AggregatedStat> = emptyList()
 )
