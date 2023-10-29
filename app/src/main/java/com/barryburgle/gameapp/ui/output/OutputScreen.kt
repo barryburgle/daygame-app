5ed33b1d05dacf2a82330684b2c62ba94f5d6342
Barry Burgle <barryburgle@gmail.com>
2023-10-29 12:30:52 +0100

    plotting week stats

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
@@ -164,6 +164,108 @@ fun OutputScreen(
                         )
                     }
                 }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.sets
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Sets",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
+                        )
+                    }
+                }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.convos
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Conversations",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
+                        )
+                    }
+                }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.contacts
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Contacts",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = true,
+                            ratio = false
+                        )
+                    }
+                }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.avgIndex
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Average Index",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = false
+                        )
+                    }
+                }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.avgConvoRatio
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Average Conversation Ratio",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = true
+                        )
+                    }
+                }
+                item {
+                    state.weekStats.map { weekStat ->
+                        weekStat.weekNumber?.let {
+                            BarEntry(
+                                it.toFloat(),
+                                weekStat.avgContactRatio
+                            )
+                        }
+                    }?.let { it ->
+                        OutputCard(
+                            chartLabel = "Weekly Average Contact Ratio",
+                            barEntryList = it as List<BarEntry>,
+                            integerValues = false,
+                            ratio = true
+                        )
+                    }
+                }
                 item { Row(modifier = Modifier.height(spaceFromTop + spaceFromBottom)) {} }
             } else {
                 item {
@@ -184,4 +286,3 @@ fun OutputScreen(
         }
     }
 }
-
