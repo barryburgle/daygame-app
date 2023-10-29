54629f5c370ba9aa5dc7249eb4c78f05b3da197a
Barry Burgle <barryburgle@gmail.com>
2023-10-29 17:16:46 +0100

    renamed to be more general

--- app/src/main/java/com/barryburgle/gameapp/model/stat/AggregatedStat.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/stat/AggregatedStat.kt
@@ -4,12 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 
 @Entity
-class WeekStat(
+class AggregatedStat(
     @ColumnInfo(name = "sets") var sets: Float,
     @ColumnInfo(name = "convos") var convos: Float,
     @ColumnInfo(name = "contacts") var contacts: Float,
     @ColumnInfo(name = "avg_index") var avgIndex: Float,
     @ColumnInfo(name = "avg_convo_ratio") var avgConvoRatio: Float,
     @ColumnInfo(name = "avg_contact_ratio") var avgContactRatio: Float,
-    @ColumnInfo(name = "week_number") var weekNumber: Int
+    @ColumnInfo(name = "period_number") var periodNumber: Int
 )
