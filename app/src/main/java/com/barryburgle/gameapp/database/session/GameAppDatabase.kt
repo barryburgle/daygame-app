33d9edfc292f3596db740501a742ec441cf839a8
Barry Burgle <barryburgle@gmail.com>
2023-10-29 12:29:53 +0100

    using new dao

--- app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
+++ app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
@@ -5,11 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.dao.session.WeekStatDao
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 @Database(entities = [AbstractSession::class], version = 1)
 abstract class GameAppDatabase : RoomDatabase() {
     abstract val abstractSessionDao: AbstractSessionDao
+    abstract val weekStatDao: WeekStatDao
 
     companion object {
         private const val DATABASE_NAME = "game_app_db"
