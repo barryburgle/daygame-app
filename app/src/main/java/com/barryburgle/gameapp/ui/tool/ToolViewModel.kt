6e6eade7b0c63c45873c85bbbb21eb4748a901ea
Barry Burgle <barryburgle@gmail.com>
2023-11-17 15:15:53 +0100

    obtaining header and rows and passing to state

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
@@ -1,17 +1,24 @@
 package com.barryburgle.gameapp.ui.tool
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
 class ToolViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewModel() {
     private val _state = MutableStateFlow(ToolsState())
-    val state = _state
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is tool Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _abstractSessionHeader = AbstractSession.header()
+    private val _abstractSessions = abstractSessionDao.getAll()
+    val state = combine(_state, _abstractSessions) { state, abstractSessions ->
+        state.copy(
+            abstractSessions = abstractSessions,
+            abstractSessionHeader = _abstractSessionHeader
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ToolsState())
+
 }
