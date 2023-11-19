cbf1bace8303954b6dfe8497703470f349a88b5a
Barry Burgle <barryburgle@gmail.com>
2023-11-19 13:24:27 +0100

    onEvent function

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
@@ -3,12 +3,15 @@ package com.barryburgle.gameapp.ui.tool
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.event.ToolEvent
 import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class ToolViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewModel() {
     private val _state = MutableStateFlow(ToolsState())
@@ -21,4 +24,41 @@ class ToolViewModel(private val abstractSessionDao: AbstractSessionDao) : ViewMo
         )
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ToolsState())
 
+    fun onEvent(event: ToolEvent) {
+        when (event) {
+            is ToolEvent.SetExportFileName -> {
+                _state.update {
+                    it.copy(
+                        exportFileName = event.exportFileName
+                    )
+                }
+            }
+
+            is ToolEvent.SetImportFileName -> {
+                _state.update {
+                    it.copy(
+                        importFileName = event.importFileName
+                    )
+                }
+            }
+
+            is ToolEvent.SetExportFolder -> {
+                _state.update {
+                    it.copy(
+                        exportFolder = event.exportFolder
+                    )
+                }
+            }
+
+            is ToolEvent.SetAbstractSessions -> {
+                _state.update {
+                    it.copy(
+                        abstractSessions = event.abstractSessions
+                    )
+                }
+                val abstractSessions = _state.value.abstractSessions
+                viewModelScope.launch { abstractSessionDao.batchInsert(abstractSessions) }
+            }
+        }
+    }
 }
