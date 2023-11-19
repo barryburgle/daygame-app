1f7d4c13b8da62bd52f035011de995ea68e65da1
Barry Burgle <barryburgle@gmail.com>
2023-11-19 13:23:53 +0100

    new import and export settings

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/state/ToolsState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/state/ToolsState.kt
@@ -3,6 +3,9 @@ package com.barryburgle.gameapp.ui.tool.state
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 data class ToolsState(
+    val exportFileName: String = "",
+    val importFileName: String = "",
+    val exportFolder: String = "",
     val abstractSessionHeader: String = "",
     val abstractSessions: List<AbstractSession> = emptyList()
 )
