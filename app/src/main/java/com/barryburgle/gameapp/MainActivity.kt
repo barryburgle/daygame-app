2ea6ec952ad590ad4bde7db9626b8f86e31601da
Barry Burgle <barryburgle@gmail.com>
2023-11-21 21:38:14 +0100

    requesting permissions

--- app/src/main/java/com/barryburgle/gameapp/MainActivity.kt
+++ app/src/main/java/com/barryburgle/gameapp/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.barryburgle.gameapp
 
+import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.barryburgle.gameapp.database.session.GameAppDatabase
-import com.barryburgle.gameapp.ui.navigation.Navigation
 import com.barryburgle.gameapp.ui.input.InputViewModel
+import com.barryburgle.gameapp.ui.navigation.Navigation
 import com.barryburgle.gameapp.ui.output.OutputViewModel
 import com.barryburgle.gameapp.ui.theme.GameAppOriginalTheme
 import com.barryburgle.gameapp.ui.tool.ToolViewModel
@@ -60,6 +74,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        if (!checkPermission(applicationContext)) {
+            requestPermission(applicationContext)
+        }
         setContent {
             GameAppOriginalTheme {
                 val inputState by inputViewModel.state.collectAsState()
@@ -79,5 +96,61 @@ class MainActivity : ComponentActivity() {
             window,
             false
         )
+    }
+
+    private fun checkPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            val readFile = ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+            val writeFile = ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+            readFile && writeFile
+        }
+    }
+
+    private fun requestPermission(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            intent.addCategory("android.intent.category.DEFAULT")
+            intent.data = Uri.parse(String.format("package:%s", context.packageName))
+            intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent)
+        } else {
+            val requestReadWritePermissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { result ->
+                var count: kotlin.Int = 0
+                result.entries.forEach {
+                    if (it.value == true) {
+                        count++
+                    }
+                    if (count == 2) {
+                        Toast.makeText(
+                            applicationContext,
+                            "File access permission granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "File access permission not granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+            requestReadWritePermissionLauncher.launch(
+                arrayOf(
+                    READ_EXTERNAL_STORAGE,
+                    WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
     }
 }
