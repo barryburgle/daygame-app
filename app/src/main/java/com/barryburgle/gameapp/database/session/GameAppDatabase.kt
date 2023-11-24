1931905a9ed7d4267a5d48d1708ee84103530af4
Barry Burgle <barryburgle@gmail.com>
2023-11-24 16:08:06 +0100

    added setting dao and entity

--- app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
+++ app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
@@ -6,12 +6,15 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
 import com.barryburgle.gameapp.dao.session.AggregatedStatDao
+import com.barryburgle.gameapp.dao.session.SettingDao
 import com.barryburgle.gameapp.model.session.AbstractSession
+import com.barryburgle.gameapp.model.setting.Setting
 
-@Database(entities = [AbstractSession::class], version = 1)
+@Database(entities = [AbstractSession::class, Setting::class], version = 1)
 abstract class GameAppDatabase : RoomDatabase() {
     abstract val abstractSessionDao: AbstractSessionDao
     abstract val aggregatedStatDao: AggregatedStatDao
+    abstract val settingDao: SettingDao
 
     companion object {
         private const val DATABASE_NAME = "game_app_db"
