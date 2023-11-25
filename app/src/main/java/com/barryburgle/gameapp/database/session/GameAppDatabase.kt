c7b9d96b9090dd4b869786ed387f3c832559bfaf
Barry Burgle <barryburgle@gmail.com>
2023-11-25 16:10:31 +0100

    room database migration

--- app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
+++ app/src/main/java/com/barryburgle/gameapp/database/session/GameAppDatabase.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
 import com.barryburgle.gameapp.dao.session.AggregatedStatDao
 import com.barryburgle.gameapp.dao.session.SettingDao
 import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.model.setting.Setting
 
-@Database(entities = [AbstractSession::class, Setting::class], version = 1)
+
+@Database(
+    entities = [AbstractSession::class, Setting::class],
+    version = 2
+)
 abstract class GameAppDatabase : RoomDatabase() {
     abstract val abstractSessionDao: AbstractSessionDao
     abstract val aggregatedStatDao: AggregatedStatDao
@@ -20,13 +26,19 @@ abstract class GameAppDatabase : RoomDatabase() {
         private const val DATABASE_NAME = "game_app_db"
         private var INSTANCE: GameAppDatabase? = null
 
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE IF NOT EXISTS setting (id TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)")
+            }
+        }
+
         fun getInstance(context: Context): GameAppDatabase? {
             if (INSTANCE == null) {
                 synchronized(GameAppDatabase::class) {
                     INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
                         GameAppDatabase::class.java, DATABASE_NAME
-                    ).build()
+                    ).addMigrations(MIGRATION_1_2).build()
                 }
             }
             return INSTANCE
