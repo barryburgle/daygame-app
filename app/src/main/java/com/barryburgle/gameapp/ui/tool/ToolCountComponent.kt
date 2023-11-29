7cfe3cd8371c68cf643e76d33f7948e061463e8b
Barry Burgle <barryburgle@gmail.com>
2023-11-29 14:28:31 +0100

    tool count component

---
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolCountComponent.kt
@@ -0,0 +1,59 @@
+package com.barryburgle.gameapp.ui.tool
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import com.barryburgle.gameapp.event.ToolEvent
+import com.barryburgle.gameapp.ui.input.InputCounter
+
+@Composable
+fun ToolCountComponent(
+    inputTitle: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
+    onEvent: (ToolEvent) -> Unit,
+    saveEvent: (input: String) -> ToolEvent,
+    initialCount: Int
+) {
+    var count by remember {
+        mutableStateOf(initialCount)
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = inputTitle, textAlign = TextAlign.Center)
+        Button(
+            onClick = {
+                --count
+                if (count < 1) {
+                    count = 1
+                }
+                onEvent(saveEvent((count).toString()))
+            }
+        ) {
+            Text(text = "-")
+        }
+        InputCounter(count = count, style = style, modifier = modifier)
+        Button(
+            onClick = {
+                ++count
+                if (count > 10) {
+                    count = 10
+                }
+                onEvent(saveEvent((count).toString()))
+            }
+        ) {
+            Text(text = "+")
+        }
+    }
+}
