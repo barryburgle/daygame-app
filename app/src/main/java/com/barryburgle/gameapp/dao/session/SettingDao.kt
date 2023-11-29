41d4393089c48b865c5aaad711336d3f2611d6f3
Barry Burgle <barryburgle@gmail.com>
2023-11-29 14:28:14 +0100

    getting average last

--- app/src/main/java/com/barryburgle/gameapp/dao/session/SettingDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/session/SettingDao.kt
@@ -9,9 +9,13 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SettingDao {
+    // TODO: get out of package dao.session and put in dao.setting
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(setting: Setting)
 
     @Query("SELECT * FROM setting WHERE id=:id")
     fun getById(id: String): Flow<Setting>
+
+    @Query("SELECT COALESCE(value,4) FROM setting WHERE id='average_last'")
+    fun getAverageLast(): Flow<Int>
 }
