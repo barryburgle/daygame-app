24ba3930215d9a386d0fd4ba83bb88d3925cd6cb
Barry Burgle <barryburgle@gmail.com>
2023-12-12 20:56:24 +0100

    unused import

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
