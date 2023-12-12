0e942aede93773c0263aa30f5f172a7d6cc5ffc5
Barry Burgle <barryburgle@gmail.com>
2023-12-12 19:05:23 +0100

    unused

--- app/src/main/java/com/barryburgle/gameapp/event/ToolEvent.kt
+++ app/src/main/java/com/barryburgle/gameapp/event/ToolEvent.kt
@@ -3,11 +3,6 @@ package com.barryburgle.gameapp.event
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 sealed interface ToolEvent {
-    object SaveExportFileName : ToolEvent
-    object SaveImportFileName : ToolEvent
-    object SaveExportFolder : ToolEvent
-    object SaveLastSessionAverageQuantity : ToolEvent
-    object SaveNotificationTime : ToolEvent
     data class SetExportFileName(val exportFileName: String) : ToolEvent
     data class SetImportFileName(val importFileName: String) : ToolEvent
     data class SetExportFolder(val exportFolder: String) : ToolEvent
