8956312f9946ec23e11646ae304d84e2be61d8c0
Barry Burgle <barryburgle@gmail.com>
2023-12-12 19:08:53 +0100

    refactor

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolViewModel.kt
@@ -3,14 +3,14 @@ package com.barryburgle.gameapp.ui.tool
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
-import com.barryburgle.gameapp.dao.session.SettingDao
+import com.barryburgle.gameapp.dao.setting.SettingDao
 import com.barryburgle.gameapp.event.ToolEvent
 import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.model.setting.Setting
+import com.barryburgle.gameapp.ui.CombineSix
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -19,15 +19,32 @@ class ToolViewModel(
     private val abstractSessionDao: AbstractSessionDao,
     private val settingDao: SettingDao
 ) : ViewModel() {
-    private val _state = MutableStateFlow(ToolsState())
+    private val _state =
+        MutableStateFlow(ToolsState())
     private val _abstractSessionHeader = AbstractSession.header()
     private val _abstractSessions = abstractSessionDao.getAll()
-    val state = combine(_state, _abstractSessions) { state, abstractSessions ->
-        state.copy(
-            abstractSessions = abstractSessions,
-            abstractSessionHeader = _abstractSessionHeader
-        )
-    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ToolsState())
+    private val _exportFilename = settingDao.getExportFilename()
+    private val _importFilename = settingDao.getImportFilename()
+    private val _exportFolder = settingDao.getExportFolder()
+    private val _notificationTime = settingDao.getNotificationTime()
+    val state =
+        CombineSix(
+            _state,
+            _abstractSessions,
+            _exportFilename,
+            _importFilename,
+            _exportFolder,
+            _notificationTime
+        ) { state, abstractSessions, exportFilename, importFilename, exportFolder, notificationTime ->
+            state.copy(
+                exportFileName = exportFilename,
+                importFileName = importFilename,
+                exportFolder = exportFolder,
+                notificationTime = notificationTime,
+                abstractSessionHeader = _abstractSessionHeader,
+                abstractSessions = abstractSessions
+            )
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ToolsState())
 
     fun onEvent(event: ToolEvent) {
         when (event) {
@@ -37,6 +54,9 @@ class ToolViewModel(
                         exportFileName = event.exportFileName
                     )
                 }
+                val exportFileName = _state.value.exportFileName
+                val setting = Setting(SettingDao.EXPORT_FILE_NAME_ID, exportFileName)
+                viewModelScope.launch { settingDao.insert(setting) }
             }
 
             is ToolEvent.SetImportFileName -> {
@@ -45,6 +65,9 @@ class ToolViewModel(
                         importFileName = event.importFileName
                     )
                 }
+                val importFileName = _state.value.importFileName
+                val setting = Setting(SettingDao.IMPORT_FILE_NAME_ID, importFileName)
+                viewModelScope.launch { settingDao.insert(setting) }
             }
 
             is ToolEvent.SetExportFolder -> {
@@ -53,6 +76,9 @@ class ToolViewModel(
                         exportFolder = event.exportFolder
                     )
                 }
+                val exportFolder = _state.value.exportFolder
+                val setting = Setting(SettingDao.EXPORT_FOLDER_ID, exportFolder)
+                viewModelScope.launch { settingDao.insert(setting) }
             }
 
             is ToolEvent.SetAbstractSessions -> {
@@ -71,6 +97,12 @@ class ToolViewModel(
                         lastSessionAverageQuantity = event.lastSessionAverageQuantity.toInt()
                     )
                 }
+                val lastSessionAverageQuantity = _state.value.lastSessionAverageQuantity
+                val setting = Setting(
+                    SettingDao.LAST_SESSION_AVERAGE_QUANTITY_ID,
+                    lastSessionAverageQuantity.toString()
+                )
+                viewModelScope.launch { settingDao.insert(setting) }
             }
 
             is ToolEvent.SetNotificationTime -> {
@@ -79,41 +111,9 @@ class ToolViewModel(
                         notificationTime = event.notificationTime
                     )
                 }
-            }
-
-            ToolEvent.SaveExportFileName -> viewModelScope.launch {
-                val exportFileName = state.value.exportFileName
-                val setting = Setting("export_file_name", exportFileName)
+                val notificationTime = _state.value.notificationTime
+                val setting = Setting(SettingDao.NOTIFICATION_TIME_ID, notificationTime)
                 viewModelScope.launch { settingDao.insert(setting) }
-                _state.update { it.copy(exportFileName = exportFileName) }
-            }
-
-            ToolEvent.SaveImportFileName -> viewModelScope.launch {
-                val importFileName = state.value.importFileName
-                val setting = Setting("import_file_name", importFileName)
-                viewModelScope.launch { settingDao.insert(setting) }
-                _state.update { it.copy(importFileName = importFileName) }
-            }
-
-            ToolEvent.SaveExportFolder -> viewModelScope.launch {
-                val exportFolder = state.value.exportFolder
-                val setting = Setting("export_folder", exportFolder)
-                viewModelScope.launch { settingDao.insert(setting) }
-                _state.update { it.copy(exportFolder = exportFolder) }
-            }
-
-            ToolEvent.SaveLastSessionAverageQuantity -> viewModelScope.launch {
-                val lastSessionAverageQuantity = state.value.lastSessionAverageQuantity
-                val setting = Setting("average_last", lastSessionAverageQuantity.toString())
-                viewModelScope.launch { settingDao.insert(setting) }
-                _state.update { it.copy(lastSessionAverageQuantity = lastSessionAverageQuantity) }
-            }
-
-            ToolEvent.SaveNotificationTime -> viewModelScope.launch {
-                val notificationTime = state.value.notificationTime
-                val setting = Setting("notification_time", notificationTime)
-                viewModelScope.launch { settingDao.insert(setting) }
-                _state.update { it.copy(notificationTime = notificationTime) }
             }
         }
     }
