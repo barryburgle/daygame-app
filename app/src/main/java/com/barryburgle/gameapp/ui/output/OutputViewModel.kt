4813d8822e9e2b5ae4840fba9ce5d22ae510eedb
Barry Burgle <barryburgle@gmail.com>
2024-12-23 11:43:21 +0100

    using new state and combine method

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
@@ -8,7 +8,7 @@ import com.barryburgle.gameapp.dao.setting.SettingDao
 import com.barryburgle.gameapp.event.ChartTypeEvent
 import com.barryburgle.gameapp.manager.SessionManager
 import com.barryburgle.gameapp.model.enums.ChartType
-import com.barryburgle.gameapp.ui.CombineSix
+import com.barryburgle.gameapp.ui.CombineNine
 import com.barryburgle.gameapp.ui.output.state.OutputState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -21,24 +21,34 @@ class OutputViewModel(
 ) : ViewModel() {
     private val _state = MutableStateFlow(OutputState())
     private val _chartType = MutableStateFlow(ChartType.SESSION)
+    private val _setsHistogram = abstractSessionDao.getSetsHistogram()
+    private val _convosHistogram = abstractSessionDao.getConvosHistogram()
+    private val _contactsHistogram = abstractSessionDao.getContactsHistogram()
+
+    // TODO: let user set n for following query from tools screen with a writing query on db
     private val _abstractSessions = abstractSessionDao.getAllLimit(14)
     private val _weekStats = aggregatedStatDao.groupStatsByWeekNumber()
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     private val _monthStats = aggregatedStatDao.groupStatsByMonth()
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     private val _averageLast = settingDao.getAverageLast()
 
-    // TODO: create a dao method to get last n sessions and then let user set n from tools screen
-    val state = CombineSix(
+    val state = CombineNine(
         _state,
+        _setsHistogram,
+        _convosHistogram,
+        _contactsHistogram,
         _abstractSessions,
         _weekStats,
         _monthStats,
         _chartType,
         _averageLast
     )
-    { state, abstractSessions, weekStats, monthStats, chartType, averageLast ->
+    { state, setsHistogram, convosHistogram, contactsHistogram, abstractSessions, weekStats, monthStats, chartType, averageLast ->
         state.copy(
+            setsHistogram = setsHistogram,
+            convosHistogram = convosHistogram,
+            contactsHistogram = contactsHistogram,
             abstractSessions = SessionManager.normalizeSessionsIds(abstractSessions),
             weekStats = SessionManager.normalizeIds(weekStats),
             monthStats = SessionManager.normalizeIds(monthStats),
