3a30bfb5eebabb96be41e514bcab60c48206a714
Barry Burgle <barryburgle@gmail.com>
2024-12-23 11:39:51 +0100

    bugfix

--- app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
@@ -101,6 +101,6 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, R> CombineNine(
         args[5] as T6,
         args[6] as T7,
         args[7] as T8,
-        args[7] as T9,
+        args[8] as T9,
     )
 }
