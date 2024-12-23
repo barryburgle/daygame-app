5fc37149017f2196e89b8c7551cd300820f51c90
Barry Burgle <barryburgle@gmail.com>
2024-12-23 11:42:26 +0100

    new state fields

--- app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
@@ -3,10 +3,14 @@ package com.barryburgle.gameapp.ui.output.state
 import com.barryburgle.gameapp.model.enums.ChartType
 import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.model.stat.AggregatedStat
+import com.barryburgle.gameapp.model.stat.Histogram
 
 data class OutputState(
     val chartType: ChartType = ChartType.SESSION,
     val abstractSessions: List<AbstractSession> = emptyList(),
+    val setsHistogram: List<Histogram> = emptyList(),
+    val convosHistogram: List<Histogram> = emptyList(),
+    val contactsHistogram: List<Histogram> = emptyList(),
     val weekStats: List<AggregatedStat> = emptyList(),
     val monthStats: List<AggregatedStat> = emptyList(),
     val movingAverageWindow: Int = 4
