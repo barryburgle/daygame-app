1a3997ce6b0b65b82a30e5ddf5601ebf37dbfc91
Barry Burgle <barryburgle@gmail.com>
2024-12-23 11:40:01 +0100

    new chart type

--- app/src/main/java/com/barryburgle/gameapp/model/enums/ChartType.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/ChartType.kt
@@ -3,5 +3,6 @@ package com.barryburgle.gameapp.model.enums
 enum class ChartType(val field: String) {
     SESSION("Sessions"),
     WEEK("Weeks"),
-    MONTH("Months");
+    MONTH("Months"),
+    HISTOGRAM("Histograms");
 }
