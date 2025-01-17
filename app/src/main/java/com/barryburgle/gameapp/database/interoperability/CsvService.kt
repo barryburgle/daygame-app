d8208a3e5323cbd554674cbda0cc09145e1df498
Barry Burgle <barryburgle@gmail.com>
2025-01-17 18:03:46 +0100

    export/import bugfix

--- app/src/main/java/com/barryburgle/gameapp/database/interoperability/CsvService.kt
+++ app/src/main/java/com/barryburgle/gameapp/database/interoperability/CsvService.kt
@@ -12,17 +12,16 @@ import java.io.FileWriter
 import java.text.SimpleDateFormat
 import java.util.Date
 
-
 class CsvService {
 
     companion object {
         val localPath = Environment.getExternalStorageDirectory()
         val batchSessionService = BatchSessionService()
+        val separator = ","
         fun exportRows(
             exportFolder: String,
             filename: String,
-            exportRowList: List<AbstractSession>,
-            headerToExport: String,
+            abstractSessionList: List<AbstractSession>,
             exportHeader: Boolean
         ) {
             val exportDir = File(localPath, "/$exportFolder")
@@ -37,13 +36,12 @@ class CsvService {
                 file.createNewFile()
                 val csvWrite = CSVWriter(FileWriter(file))
                 if (exportHeader) {
-                    val exportArrayHeader =
-                        arrayOf(headerToExport)
+                    val exportArrayHeader = generateHeader()
                     csvWrite.writeNext(exportArrayHeader)
                 }
-                for (exportRow in exportRowList) {
+                for (abstractSession in abstractSessionList) {
                     val exportArrayRow =
-                        arrayOf(exportRow.toString())
+                        exportSingleRow(abstractSession)
                     csvWrite.writeNext(exportArrayRow)
                 }
                 csvWrite.close()
@@ -52,11 +50,54 @@ class CsvService {
             }
         }
 
+        fun exportSingleRow(abstractSession: AbstractSession): Array<String> {
+            val abstractSessionFieldList = mutableListOf<String>()
+            abstractSessionFieldList.add(abstractSession.id.toString())
+            abstractSessionFieldList.add(abstractSession.insertTime)
+            abstractSessionFieldList.add(abstractSession.date)
+            abstractSessionFieldList.add(abstractSession.startHour)
+            abstractSessionFieldList.add(abstractSession.endHour)
+            abstractSessionFieldList.add(abstractSession.sets.toString())
+            abstractSessionFieldList.add(abstractSession.convos.toString())
+            abstractSessionFieldList.add(abstractSession.contacts.toString())
+            abstractSessionFieldList.add(abstractSession.stickingPoints)
+            abstractSessionFieldList.add(abstractSession.sessionTime.toString())
+            abstractSessionFieldList.add(abstractSession.approachTime.toString())
+            abstractSessionFieldList.add(abstractSession.convoRatio.toString())
+            abstractSessionFieldList.add(abstractSession.rejectionRatio.toString())
+            abstractSessionFieldList.add(abstractSession.contactRatio.toString())
+            abstractSessionFieldList.add(abstractSession.index.toString())
+            abstractSessionFieldList.add(abstractSession.dayOfWeek.toString())
+            abstractSessionFieldList.add(abstractSession.weekNumber.toString())
+            return abstractSessionFieldList.toTypedArray()
+        }
+
+        fun generateHeader(): Array<String> {
+            val abstractSessionFieldList = mutableListOf<String>()
+            abstractSessionFieldList.add("id")
+            abstractSessionFieldList.add("insert_time")
+            abstractSessionFieldList.add("session_date")
+            abstractSessionFieldList.add("start_hour")
+            abstractSessionFieldList.add("end_hour")
+            abstractSessionFieldList.add("sets")
+            abstractSessionFieldList.add("convos")
+            abstractSessionFieldList.add("contacts")
+            abstractSessionFieldList.add("sticking_points")
+            abstractSessionFieldList.add("session_time")
+            abstractSessionFieldList.add("approach_time")
+            abstractSessionFieldList.add("convo_ratio")
+            abstractSessionFieldList.add("rejection_ratio")
+            abstractSessionFieldList.add("contact_ratio")
+            abstractSessionFieldList.add("index")
+            abstractSessionFieldList.add("day_of_week")
+            abstractSessionFieldList.add("week_number")
+            return abstractSessionFieldList.toTypedArray()
+        }
+
         fun importRows(
             importFolder: String,
             filename: String,
-            exportHeader: Boolean,
-            separator: String
+            exportHeader: Boolean
         ): List<AbstractSession> {
             val csvReader = CSVReader(
                 FileReader(
@@ -70,13 +111,12 @@ class CsvService {
             val startCount: Int = if (exportHeader) 1 else 0
             var abstractSessionList: MutableList<AbstractSession> = mutableListOf()
             for (index in startCount..listOfStrings.lastIndex) {
-                var fields: List<String> = listOfStrings.get(index)[0].split(separator)
-                abstractSessionList.add(mapImportRow(fields))
+                abstractSessionList.add(mapImportRow(listOfStrings.get(index)))
             }
             return abstractSessionList
         }
 
-        fun mapImportRow(fields: List<String>): AbstractSession {
+        fun mapImportRow(fields: Array<String>): AbstractSession {
             return batchSessionService.init(
                 fields[2].substring(0, 10),
                 fields[3].substring(11, 16),
