804698d0f25dfe64240eceaff42a05699c974d8f
Barry Burgle <barryburgle@gmail.com>
2025-01-20 23:08:38 +0100

    input card redesign

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -24,7 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,7 +40,7 @@ fun InputCard(
     modifier: Modifier = Modifier
 ) {
     val countFontSize = 50.sp
-    val perfFontSize = 25.sp
+    val perfFontSize = 15.sp
     val descriptionFontSize = 10.sp
     Card(
         modifier = modifier,
@@ -50,137 +49,160 @@ fun InputCard(
         ),
         shape = MaterialTheme.shapes.large
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Column(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(0.7f),
+                    .padding(16.dp),
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(5.dp)
+                Column(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
                 ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Session ${FormatService.getDate(abstractSession.date)}",
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(0.6f),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            IconButton(onClick = {
+                                onEvent(
+                                    AbstractSessionEvent.DeleteSession(
+                                        abstractSession
+                                    )
+                                )
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.Delete,
+                                    contentDescription = "Delete Session",
+                                    tint = MaterialTheme.colorScheme.onErrorContainer
+                                )
+                            }
+                        }
+                    }
                     Text(
-                        text = "Session ${FormatService.getDate(abstractSession.date)}",
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                    IconButton(onClick = {
-                        onEvent(
-                            AbstractSessionEvent.DeleteSession(
-                                abstractSession
+                        text = "${FormatService.getTime(abstractSession.startHour)} - ${
+                            FormatService.getTime(
+                                abstractSession.endHour
                             )
-                        )
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Delete Session",
-                            tint = MaterialTheme.colorScheme.onErrorContainer
-                        )
-                    }
+                        } on ${
+                            DayOfWeek.of(abstractSession.dayOfWeek).toString().lowercase()
+                        } (${abstractSession.sessionTime} minutes)",
+                        style = MaterialTheme.typography.bodySmall
+                    )
                 }
-                Row {
-                    Column {
-                        Text(
-                            text = "${abstractSession.sessionTime} minutes",
-                            style = MaterialTheme.typography.titleSmall
-                        )
-                        Text(
-                            text = "Start: ${FormatService.getTime(abstractSession.startHour)}",
-                            style = MaterialTheme.typography.titleSmall
-                        )
-                        Text(
-                            text = "End: ${FormatService.getTime(abstractSession.endHour)}",
-                            style = MaterialTheme.typography.titleSmall
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Conversation Ratio: ${abstractSession.convoRatio}",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Text(
-                            text = "Rejection Ratio: ${abstractSession.rejectionRatio}",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Text(
-                            text = "Contact Ratio: ${abstractSession.contactRatio}",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Text(
-                            text = "Weekday: ${DayOfWeek.of(abstractSession.dayOfWeek)}",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                    }
+                Row(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight(),
+                            .fillMaxHeight()
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            describedQuantifier(
+                                quantity = "${abstractSession.sets}",
+                                quantityFontSize = countFontSize,
+                                description = "Sets",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                            describedQuantifier(
+                                quantity = "${abstractSession.convos}",
+                                quantityFontSize = countFontSize,
+                                description = "Conversations",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                            describedQuantifier(
+                                quantity = "${abstractSession.contacts}",
+                                quantityFontSize = countFontSize,
+                                description = "Contacts",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            describedQuantifier(
+                                quantity = "${FormatService.getPerc(abstractSession.convoRatio)}",
+                                quantityFontSize = perfFontSize,
+                                description = "Conversation\nRatio",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                            describedQuantifier(
+                                quantity = "${FormatService.getPerc(abstractSession.contactRatio)}",
+                                quantityFontSize = perfFontSize,
+                                description = "Contact\nRatio",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                            describedQuantifier(
+                                quantity = "${abstractSession.index}",
+                                quantityFontSize = perfFontSize,
+                                description = "Index",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                            describedQuantifier(
+                                quantity = "${abstractSession.approachTime}",
+                                quantityFontSize = perfFontSize,
+                                description = "Minutes\nper set",
+                                descriptionFontSize = descriptionFontSize
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(13.dp))
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight()
+                                    .background(
+                                        MaterialTheme.colorScheme.inversePrimary,
+                                        shape = RoundedCornerShape(10.dp)
+                                    )
+                                    .padding(7.dp)
+                            ) {
+                                Text(
+                                    text = "Sticking Points:",
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                                val stickingPoints =
+                                    if (abstractSession.stickingPoints.isBlank()) "No sticking points" else abstractSession.stickingPoints
+                                Text(
+                                    text = stickingPoints,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                        }
+                    }
+                    Column(
                         verticalArrangement = Arrangement.SpaceBetween,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        describedQuantifier(
-                            quantity = "${abstractSession.index}",
-                            quantityFontSize = perfFontSize,
-                            description = "Index",
-                            descriptionFontSize = descriptionFontSize,
-                            cornerRay = 4.dp
-                        )
-                        describedQuantifier(
-                            quantity = "${abstractSession.approachTime}",
-                            quantityFontSize = perfFontSize,
-                            description = "Approach Time",
-                            descriptionFontSize = descriptionFontSize,
-                            cornerRay = 4.dp
-                        )
                     }
                 }
-                Column(
-                    modifier = Modifier
-                        .width(300.dp)
-                        .background(
-                            MaterialTheme.colorScheme.inversePrimary,
-                            shape = RoundedCornerShape(10.dp)
-                        )
-                        .padding(7.dp)
-                ) {
-                    Text(
-                        text = "Sticking Points:",
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                    val stickingPoints =
-                        if (abstractSession.stickingPoints.isBlank()) "No sticking points" else abstractSession.stickingPoints
-                    Text(
-                        text = stickingPoints,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
-            }
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                describedQuantifier(
-                    quantity = "${abstractSession.sets}",
-                    quantityFontSize = countFontSize,
-                    description = "Sets",
-                    descriptionFontSize = descriptionFontSize,
-                    cornerRay = 4.dp
-                )
-                describedQuantifier(
-                    quantity = "${abstractSession.convos}",
-                    quantityFontSize = countFontSize,
-                    description = "Conversations",
-                    descriptionFontSize = descriptionFontSize,
-                    cornerRay = 4.dp
-                )
-                describedQuantifier(
-                    quantity = "${abstractSession.contacts}",
-                    quantityFontSize = countFontSize,
-                    description = "Contacts",
-                    descriptionFontSize = descriptionFontSize,
-                    cornerRay = 4.dp
-                )
             }
         }
     }
@@ -192,14 +214,17 @@ fun describedQuantifier(
     quantity: String,
     quantityFontSize: TextUnit,
     description: String,
-    descriptionFontSize: TextUnit,
-    cornerRay: Dp
+    descriptionFontSize: TextUnit
 ) {
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxHeight()
     ) {
-        Box(modifier = Modifier.padding(5.dp)) {
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+        ) {
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -210,7 +235,9 @@ fun describedQuantifier(
                 )
                 Text(
                     text = description,
-                    fontSize = descriptionFontSize
+                    fontSize = descriptionFontSize,
+                    lineHeight = 10.sp,
+                    textAlign = TextAlign.Center
                 )
             }
         }
