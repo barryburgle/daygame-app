5a701c840b0355fc8a694b1891099370c17ec982
Barry Burgle <barryburgle@gmail.com>
2025-01-23 11:08:42 +0100

    new parse time method

--- app/src/main/java/com/barryburgle/gameapp/service/FormatService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/FormatService.kt
@@ -8,7 +8,8 @@ import java.time.format.DateTimeFormatter
 class FormatService {
 
     companion object {
-        val SAVE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmX")
+        val SAVE_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmX")
+        val SAVE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmX")
         val DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy")
         val TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm")
 
@@ -28,13 +29,19 @@ class FormatService {
         fun getTime(
             localTime: String
         ): String {
-            return TIME_FORMATTER.format(LocalTime.parse(localTime, SAVE_FORMATTER))
+            return TIME_FORMATTER.format(LocalTime.parse(localTime, SAVE_DATE_FORMATTER))
         }
 
         fun parseDate(
             localDate: String
         ): LocalDate {
-            return LocalDate.parse(localDate, SAVE_FORMATTER)
+            return LocalDate.parse(localDate, SAVE_DATE_FORMATTER)
+        }
+
+        fun parseTime(
+            localDate: String
+        ): LocalTime {
+            return LocalTime.parse(localDate, SAVE_TIME_FORMATTER)
         }
     }
 }
