3bd5f2d5793a8836ecca6429493e1fa4108f41c2
Barry Burgle <barryburgle@gmail.com>
2025-01-24 23:21:05 +0100

    new stats view model

---
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/StatsViewModel.kt
@@ -0,0 +1,27 @@
+package com.barryburgle.gameapp.ui.stats
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.ui.stats.state.StatsState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+class StatsViewModel(
+    private val abstractSessionDao: AbstractSessionDao
+) : ViewModel() {
+    private val _state =
+        MutableStateFlow(StatsState())
+    private val _abstractSessions = abstractSessionDao.getAll()
+    val state =
+        combine(
+            _state,
+            _abstractSessions,
+        ) { state, abstractSessions ->
+            state.copy(
+                abstractSessions = abstractSessions,
+            )
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), StatsState())
+}
