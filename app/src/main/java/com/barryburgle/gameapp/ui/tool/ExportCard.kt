de222b50f631f4ec929aeeacf99b9391b6c35d00
Barry Burgle <barryburgle@gmail.com>
2025-01-25 01:04:02 +0100

    card title fix

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ExportCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ExportCard.kt
@@ -3,10 +3,10 @@ package com.barryburgle.gameapp.ui.tool
 import android.widget.Toast
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -29,8 +29,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.database.interoperability.CsvService
 import com.barryburgle.gameapp.event.ToolEvent
-import com.barryburgle.gameapp.model.session.AbstractSession
-import com.barryburgle.gameapp.ui.theme.Shapes
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
 
 @Composable
@@ -54,118 +52,122 @@ fun ExportCard(
         val textFieldHeight = 55.dp
         val textFieldWidth = 230.dp
         val localContext = LocalContext.current.applicationContext
-        Text(
-            text = "Import/Export",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier
-                .background(
-                    MaterialTheme.colorScheme.surface,
-                    Shapes.large
-                )
-                .padding(10.dp),
-            color = MaterialTheme.colorScheme.onSurface
-        )
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                OutlinedTextField(
-                    value = state.exportFolder,
-                    onValueChange = { onEvent(ToolEvent.SetExportFolder(it)) },
-                    placeholder = { Text(text = "Import/Export Folder") },
-                    shape = MaterialTheme.shapes.large,
+                Column(
                     modifier = Modifier
-                        .height(textFieldHeight)
-                        .width(textFieldWidth)
-                )
-                OutlinedTextField(
-                    value = state.exportFileName,
-                    onValueChange = { onEvent(ToolEvent.SetExportFileName(it)) },
-                    placeholder = { Text(text = "Export file name") },
-                    shape = MaterialTheme.shapes.large,
-                    modifier = Modifier
-                        .height(textFieldHeight)
-                        .width(textFieldWidth)
-                )
-                OutlinedTextField(
-                    value = state.importFileName,
-                    onValueChange = { onEvent(ToolEvent.SetImportFileName(it)) },
-                    placeholder = { Text(text = "Import file name") },
-                    shape = MaterialTheme.shapes.large,
-                    modifier = Modifier
-                        .height(textFieldHeight)
-                        .width(textFieldWidth)
-                )
-            }
-            Column(
-                modifier = Modifier.fillMaxHeight(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                Button(onClick = {
-                    CsvService.exportRows(
-                        state.exportFolder,
-                        state.exportFileName,
-                        state.abstractSessions,
-                        state.exportHeader
-                    )
-                    Toast.makeText(localContext, "Successfully exported", Toast.LENGTH_SHORT).show()
-                }) {
-                    Text(text = "Export")
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Export",
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
                 }
-                Button(onClick = {
-                    val importFileName = state.importFileName
-                    onEvent(
-                        ToolEvent.SetAbstractSessions(
-                            CsvService.importRows(
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Text(
+                            text = "Export folder:",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        OutlinedTextField(
+                            value = state.exportFolder,
+                            onValueChange = { onEvent(ToolEvent.SetExportFolder(it)) },
+                            placeholder = { Text(text = "Insert here the export folder") },
+                            shape = MaterialTheme.shapes.large,
+                            modifier = Modifier
+                                .height(textFieldHeight)
+                                .width(textFieldWidth)
+                        )
+                        Text(
+                            text = "Export file name:",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        OutlinedTextField(
+                            value = state.exportFileName,
+                            onValueChange = { onEvent(ToolEvent.SetExportFileName(it)) },
+                            placeholder = { Text(text = "Insert here the export file name") },
+                            shape = MaterialTheme.shapes.large,
+                            modifier = Modifier
+                                .height(textFieldHeight)
+                                .width(textFieldWidth)
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Spacer(modifier = Modifier.height(0.dp))
+                        Button(onClick = {
+                            CsvService.exportRows(
                                 state.exportFolder,
-                                importFileName,
+                                state.exportFileName,
+                                state.abstractSessions,
                                 state.exportHeader
                             )
-                        )
-                    )
-                    Toast.makeText(localContext, "Successfully imported", Toast.LENGTH_SHORT).show()
-                }) {
-                    Text(text = "Import")
-                }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    var checked = state.exportHeader
-                    Text(
-                        text = "Export Header",
-                        modifier = Modifier
-                            .height(15.dp)
-                            .width(90.dp),
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                    val thumbColor by animateColorAsState(
-                        targetValue = if (checked) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.onSurface,
-                        animationSpec = tween(durationMillis = 500)
-                    )
-                    val trackColor by animateColorAsState(
-                        targetValue = if (checked) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.surface,
-                        animationSpec = tween(durationMillis = 500)
-                    )
-                    Switch(
-                        checked = checked,
-                        onCheckedChange = {
-                            checked = it
-                            onEvent(ToolEvent.SetExportHeader(it))
-                        },
-                        colors = SwitchDefaults.colors(
-                            checkedThumbColor = thumbColor,
-                            checkedTrackColor = trackColor,
-                            uncheckedThumbColor = thumbColor,
-                            uncheckedTrackColor = trackColor
-                        ),
-                    )
+                            Toast.makeText(
+                                localContext,
+                                "Successfully exported",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }) {
+                            Text(text = "Export")
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            var checked = state.exportHeader
+                            Text(
+                                text = "Export Header",
+                                modifier = Modifier
+                                    .height(15.dp)
+                                    .width(90.dp),
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                            val thumbColor by animateColorAsState(
+                                targetValue = if (checked) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.onSurface,
+                                animationSpec = tween(durationMillis = 500)
+                            )
+                            val trackColor by animateColorAsState(
+                                targetValue = if (checked) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.surface,
+                                animationSpec = tween(durationMillis = 500)
+                            )
+                            Switch(
+                                checked = checked,
+                                onCheckedChange = {
+                                    checked = it
+                                    onEvent(ToolEvent.SetExportHeader(it))
+                                },
+                                colors = SwitchDefaults.colors(
+                                    checkedThumbColor = thumbColor,
+                                    checkedTrackColor = trackColor,
+                                    uncheckedThumbColor = thumbColor,
+                                    uncheckedTrackColor = trackColor
+                                ),
+                            )
+                        }
+                    }
                 }
             }
         }
