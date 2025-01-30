4ec0c4fe53431a83006c0f3026e73f50f3693d4d
Barry Burgle <barryburgle@gmail.com>
2025-01-30 15:55:57 +0100

    new holding leads events

--- app/src/main/java/com/barryburgle/gameapp/event/AbstractSessionEvent.kt
+++ app/src/main/java/com/barryburgle/gameapp/event/AbstractSessionEvent.kt
@@ -1,14 +1,16 @@
 package com.barryburgle.gameapp.event
 
 import com.barryburgle.gameapp.model.enums.SortType
+import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 sealed interface AbstractSessionEvent: GenericEvent {
     object SaveAbstractSession : AbstractSessionEvent
     data class ShowDialog(val addSession: Boolean, val updateSession: Boolean) :
         AbstractSessionEvent
-
+    object ShowLeadDialog :AbstractSessionEvent
     object HideDialog : AbstractSessionEvent
+    object HideLeadDialog : AbstractSessionEvent
     data class SetDate(val date: String) : AbstractSessionEvent
     data class SetStartHour(val startHour: String) : AbstractSessionEvent
     data class SetEndHour(val endHour: String) : AbstractSessionEvent
@@ -19,4 +21,10 @@ sealed interface AbstractSessionEvent: GenericEvent {
     data class SortSessions(val sortType: SortType) : AbstractSessionEvent
     data class DeleteSession(val abstractSession: AbstractSession) : AbstractSessionEvent
     data class EditSession(val abstractSession: AbstractSession) : AbstractSessionEvent
+    data class SetLead(val lead: Lead) : AbstractSessionEvent
+    data class SetLeadName(val name: String) : AbstractSessionEvent
+    data class SetLeadContact(val contact: String) : AbstractSessionEvent
+    data class SetLeadCountryName(val countryName: String) : AbstractSessionEvent
+    data class SetLeadNationality(val nationality: String) : AbstractSessionEvent
+    data class SetLeadAge(val age: String) : AbstractSessionEvent
 }
