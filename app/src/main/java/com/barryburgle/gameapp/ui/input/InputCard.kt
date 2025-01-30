124f465b1967cbbf5be63d6319c78f3199d1b432
Barry Burgle <barryburgle@gmail.com>
2025-01-30 15:37:38 +0100

    moved away function

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputCard.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -25,7 +24,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -278,24 +276,4 @@ fun describedQuantifier(
             }
         }
     }
-}
-
-@Composable
-fun leadName(
-    name: String, contactType: String, nationality: String, backgroundColor: Color
-) {
-    Text(
-        text = "${name} ${contactType} ${nationality}",
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = Modifier
-            .background(
-                color = backgroundColor, shape = RoundedCornerShape(5.dp)
-            )
-            .padding(8.dp)
-    )
-    Spacer(
-        modifier = Modifier
-            .width(5.dp)
-            .height(5.dp)
-    )
 }
