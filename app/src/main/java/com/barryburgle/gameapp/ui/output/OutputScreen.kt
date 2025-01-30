29b98f50ea2bce3e33d9c494874ae79a58fd28ca
Barry Burgle <barryburgle@gmail.com>
2025-01-30 16:28:02 +0100

    basic leads show

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputScreen.kt
@@ -1,7 +1,6 @@
 package com.barryburgle.gameapp.ui.output
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,10 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,7 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.barryburgle.gameapp.event.ChartTypeEvent
+import com.barryburgle.gameapp.ui.input.leadName
 import com.barryburgle.gameapp.ui.output.section.HistogramSection
 import com.barryburgle.gameapp.ui.output.section.MonthSection
 import com.barryburgle.gameapp.ui.output.section.SessionSection
@@ -58,7 +55,12 @@ fun OutputScreen(
                     horizontalArrangement = Arrangement.Start
                 ) {
                     Column {
-
+                        for (lead in state.leads) {
+                            leadName(
+                                lead = lead,
+                                backgroundColor = MaterialTheme.colorScheme.surface
+                            )
+                        }
                     }
                 }
             }
