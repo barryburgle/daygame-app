e864a300eef900294083bc0babd0e9c66cbfab0f
Barry Burgle <barryburgle@gmail.com>
2025-02-04 00:46:06 +0100

    new stats state

--- app/src/main/java/com/barryburgle/gameapp/ui/stats/state/StatsState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/state/StatsState.kt
@@ -1,7 +1,16 @@
 package com.barryburgle.gameapp.ui.stats.state
 
+import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
+import com.barryburgle.gameapp.model.stat.CategoryHistogram
+import com.barryburgle.gameapp.model.stat.Histogram
 
 data class StatsState(
-    val abstractSessions: List<AbstractSession> = emptyList()
+    val abstractSessions: List<AbstractSession> = emptyList(),
+    val leads: List<Lead> = emptyList(),
+    val setsHistogram: List<Histogram> = emptyList(),
+    val convosHistogram: List<Histogram> = emptyList(),
+    val contactsHistogram: List<Histogram> = emptyList(),
+    val nationalityHistogram: List<CategoryHistogram> = emptyList(),
+    val ageHistogram: List<Histogram> = emptyList(),
 )
