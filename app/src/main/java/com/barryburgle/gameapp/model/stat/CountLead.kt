78f0b5c4ae7a0f715d7eac5857dfc79ea92b27c9
Barry Burgle <barryburgle@gmail.com>
2025-02-04 00:43:20 +0100

    removed

--- app/src/main/java/com/barryburgle/gameapp/model/stat/CountLead.kt
+++
@@ -1,9 +0,0 @@
-package com.barryburgle.gameapp.model.stat
-
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-
-@Entity
-class CountLead(
-    @ColumnInfo(name = "count") var count: Int,
-)
