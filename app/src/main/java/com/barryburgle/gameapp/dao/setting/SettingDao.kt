5a1be5c10bbe36bb211cc79c468d265d428c7c46
Barry Burgle <barryburgle@gmail.com>
2025-02-06 11:06:08 +0100

    new methods to get import and export sessions and leads names

--- app/src/main/java/com/barryburgle/gameapp/dao/setting/SettingDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/setting/SettingDao.kt
@@ -10,8 +10,6 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SettingDao {
     companion object {
-        const val EXPORT_FILE_NAME_ID: String = "export_file_name"
-        const val IMPORT_FILE_NAME_ID: String = "import_file_name"
         const val EXPORT_FOLDER_ID: String = "export_folder"
         const val IMPORT_FOLDER_ID: String = "import_folder"
         const val EXPORT_HEADER_ID: String = "export_header"
@@ -19,8 +17,14 @@ interface SettingDao {
         const val LAST_SESSION_AVERAGE_QUANTITY_ID: String = "average_last"
         const val NOTIFICATION_TIME_ID: String = "notification_time"
         const val DEFAULT_MOVING_AVERAGE_WINDOW: Int = 4
-        const val DEFAULT_EXPORT_FILE_NAME: String = "daygame_export"
-        const val DEFAULT_IMPORT_FILE_NAME: String = "daygame_export_yyyy_mm_dd_hh_mm.csv"
+        const val EXPORT_SESSIONS_FILE_NAME_ID: String = "export_sessions_file_name"
+        const val IMPORT_SESSIONS_FILE_NAME_ID: String = "import_sessions_file_name"
+        const val DEFAULT_SESSIONS_EXPORT_FILE_NAME: String = "sessions_export"
+        const val DEFAULT_SESSIONS_IMPORT_FILE_NAME: String = "sessions_export_yyyy_mm_dd_hh_mm.csv"
+        const val EXPORT_LEADS_FILE_NAME_ID: String = "export_leads_file_name"
+        const val IMPORT_LEADS_FILE_NAME_ID: String = "import_leads_file_name"
+        const val DEFAULT_LEADS_EXPORT_FILE_NAME: String = "leads_export"
+        const val DEFAULT_LEADS_IMPORT_FILE_NAME: String = "leads_export_yyyy_mm_dd_hh_mm.csv"
         const val DEFAULT_EXPORT_FOLDER: String = "Download"
         const val DEFAULT_IMPORT_FOLDER: String = "Download"
         const val DEFAULT_EXPORT_HEADER_FLAG: String = "false"
@@ -34,11 +38,17 @@ interface SettingDao {
     @Query("SELECT * FROM setting WHERE id=:id")
     fun getById(id: String): Flow<Setting>
 
-    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_EXPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${EXPORT_FILE_NAME_ID}'")
-    fun getExportFilename(): Flow<String>
+    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_SESSIONS_EXPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${EXPORT_SESSIONS_FILE_NAME_ID}'")
+    fun getExportSessionsFilename(): Flow<String>
 
-    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_IMPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${IMPORT_FILE_NAME_ID}'")
-    fun getImportFilename(): Flow<String>
+    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_SESSIONS_IMPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${IMPORT_SESSIONS_FILE_NAME_ID}'")
+    fun getImportSessionsFilename(): Flow<String>
+
+    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_LEADS_EXPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${EXPORT_LEADS_FILE_NAME_ID}'")
+    fun getExportLeadsFilename(): Flow<String>
+
+    @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_LEADS_IMPORT_FILE_NAME}' ELSE value END FROM setting WHERE id = '${IMPORT_LEADS_FILE_NAME_ID}'")
+    fun getImportLeadsFilename(): Flow<String>
 
     @Query("SELECT CASE COUNT(*) WHEN 0 THEN '${DEFAULT_EXPORT_FOLDER}' ELSE value END FROM setting WHERE id = '${EXPORT_FOLDER_ID}'")
     fun getExportFolder(): Flow<String>
