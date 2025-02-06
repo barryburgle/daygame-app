6f9ce7a7c5cefefa1b208afab32eab9eaac6aa7c
Barry Burgle <barryburgle@gmail.com>
2025-02-06 11:06:22 +0100

    tool event update to set new state fields

--- app/src/main/java/com/barryburgle/gameapp/event/ToolEvent.kt
+++ app/src/main/java/com/barryburgle/gameapp/event/ToolEvent.kt
@@ -1,13 +1,17 @@
 package com.barryburgle.gameapp.event
 
+import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
 
 sealed interface ToolEvent {
-    data class SetExportFileName(val exportFileName: String) : ToolEvent
-    data class SetImportFileName(val importFileName: String) : ToolEvent
+    data class SetExportSessionsFileName(val exportSessionsFileName: String) : ToolEvent
+    data class SetImportSessionsFileName(val importSessionsFileName: String) : ToolEvent
+    data class SetExportLeadsFileName(val exportLeadsFileName: String) : ToolEvent
+    data class SetImportLeadsFileName(val importLeadsFileName: String) : ToolEvent
     data class SetExportFolder(val exportFolder: String) : ToolEvent
     data class SetImportFolder(val importFolder: String) : ToolEvent
     data class SetAbstractSessions(val abstractSessions: List<AbstractSession>) : ToolEvent
+    data class SetLeads(val leads: List<Lead>) : ToolEvent
     data class SetLastSessionAverageQuantity(val lastSessionAverageQuantity: String) : ToolEvent
     data class SetNotificationTime(val notificationTime: String) : ToolEvent
     data class SetExportHeader(val exportHeader: Boolean) : ToolEvent
