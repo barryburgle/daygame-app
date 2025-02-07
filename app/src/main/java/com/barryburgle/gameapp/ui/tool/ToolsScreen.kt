6396c76e43a8de216164f1ad4a26871fd6c5a237
Barry Burgle <barryburgle@gmail.com>
2025-02-07 10:10:12 +0100

    tools screen padding refactor

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolsScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolsScreen.kt
@@ -1,16 +1,22 @@
 package com.barryburgle.gameapp.ui.tool
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.event.ToolEvent
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
@@ -19,39 +25,69 @@ import com.barryburgle.gameapp.ui.tool.state.ToolsState
 @Composable
 fun ToolsScreen(
     state: ToolsState,
-    onEvent: (ToolEvent) -> Unit
+    onEvent: (ToolEvent) -> Unit,
+    spaceFromLeft: Dp,
+    spaceFromTop: Dp,
+    spaceFromBottom: Dp
 ) {
-    val spaceFromTop = 20.dp
-    val spaceFromBottom = 60.dp
     Scaffold { padding ->
+        val dataExchangeCardModifier = Modifier
+            .height(370.dp)
+            .width(LocalConfiguration.current.screenWidthDp.dp - spaceFromLeft * 2)
+            .shadow(
+                elevation = 5.dp, shape = MaterialTheme.shapes.large
+            )
+        val settingsCardModifier = Modifier
+            .width(LocalConfiguration.current.screenWidthDp.dp - spaceFromLeft * 2)
+            .shadow(
+                elevation = 5.dp, shape = MaterialTheme.shapes.large
+            )
         LazyColumn(
-            contentPadding = PaddingValues(16.dp),
             modifier = Modifier
                 .fillMaxSize()
-                .offset(y = spaceFromTop),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .offset(
+                    y = spaceFromTop + spaceFromLeft
+                ),
+            verticalArrangement = Arrangement.spacedBy(spaceFromLeft)
         ) {
             item {
-                DataExchangeCard(
-                    cardTitle = "Export",
-                    state = state,
-                    onEvent = onEvent
-                )
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.width(spaceFromLeft))
+                    DataExchangeCard(
+                        cardTitle = "Export",
+                        state = state,
+                        onEvent = onEvent,
+                        modifier = dataExchangeCardModifier
+                    )
+                }
             }
             item {
-                DataExchangeCard(
-                    cardTitle = "Import",
-                    state = state,
-                    onEvent = onEvent
-                )
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.width(spaceFromLeft))
+                    DataExchangeCard(
+                        cardTitle = "Import",
+                        state = state,
+                        onEvent = onEvent,
+                        modifier = dataExchangeCardModifier
+                    )
+                }
             }
             item {
-                SettingsCard(
-                    state = state,
-                    onEvent = onEvent
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.width(spaceFromLeft))
+                    SettingsCard(
+                        state = state,
+                        onEvent = onEvent,
+                        modifier = settingsCardModifier
+                    )
+                }
+            }
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .height(spaceFromTop + spaceFromBottom + spaceFromLeft * 2)
                 )
             }
-            item { Row(modifier = Modifier.height(spaceFromTop + spaceFromBottom)) {} }
         }
     }
 }
