63260bf44612ba857b00896d9fd1767da77e968c
Barry Burgle <barryburgle@gmail.com>
2025-02-07 10:09:54 +0100

    stats screen padding refactor

--- app/src/main/java/com/barryburgle/gameapp/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/StatsScreen.kt
@@ -1,17 +1,21 @@
 package com.barryburgle.gameapp.ui.stats
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.model.enums.ContactTypeEnum
@@ -25,108 +29,175 @@ import com.barryburgle.gameapp.ui.utilities.InsertInvite
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatsScreen(
-    state: StatsState
+    state: StatsState,
+    spaceFromLeft: Dp,
+    spaceFromTop: Dp,
+    spaceFromBottom: Dp
 ) {
-    val spaceFromTop = 20.dp
-    val spaceFromBottom = 60.dp
     val heigh: Dp = 200.dp
     val width: Dp = 320.dp
     Scaffold { padding ->
         InsertInvite(state.abstractSessions, "Session")
         if (state.abstractSessions.isNotEmpty()) {
+            val cardModifier = Modifier
+                .shadow(
+                    elevation = 5.dp,
+                    shape = MaterialTheme.shapes.large
+                )
+                .width(LocalConfiguration.current.screenWidthDp.dp - spaceFromLeft * 2)
             LazyColumn(
-                contentPadding = PaddingValues(16.dp),
                 modifier = Modifier
                     .fillMaxSize()
-                    .offset(y = spaceFromTop),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                    .offset(
+                        y = spaceFromTop + spaceFromLeft
+                    ),
+                verticalArrangement = Arrangement.spacedBy(spaceFromLeft)
             ) {
                 item {
-                    StatsCard(
-                        title = "Overall",
-                        description = "${GlobalStatsService.computeSpentHours(state.abstractSessions)} hours spent, on average a set each ${
-                            GlobalStatsService.computeAvgApproachTime(
-                                state.abstractSessions
-                            )
-                        } minutes",
-                        firstQuantifierQuantity = "${GlobalStatsService.computeSets(state.abstractSessions)}",
-                        firstQuantifierDescription = "Sets",
-                        secondQuantifierQuantity = "${GlobalStatsService.computeConvos(state.abstractSessions)}",
-                        secondQuantifierDescription = "Conversations",
-                        thirdQuantifierQuantity = "${GlobalStatsService.computeContacts(state.abstractSessions)}",
-                        thirdQuantifierDescription = "Contacts",
-                        firstPerformanceQuantity = "${GlobalStatsService.computeAvgConvoRatio(state.abstractSessions)} %",
-                        firstPerformanceDescription = "Conversation\nRatio",
-                        secondPerformanceQuantity = "${
-                            GlobalStatsService.computeAvgRejectionRatio(
-                                state.abstractSessions
-                            )
-                        } %",
-                        secondPerformanceDescription = "Rejection\nRatio",
-                        thirdPerformanceQuantity = "${
-                            GlobalStatsService.computeAvgContactRatio(
-                                state.abstractSessions
-                            )
-                        } %",
-                        thirdPerformanceDescription = "Contact\nRatio",
-                        fourthPerformanceQuantity = "${GlobalStatsService.computeAvgIndex(state.abstractSessions)}",
-                        fourthPerformanceDescription = "Average\nIndex"
-                    )
+                    Row {
+                        Spacer(
+                            modifier = Modifier
+                                .width(spaceFromLeft)
+                        )
+                        StatsCard(
+                            modifier = cardModifier,
+                            title = "Overall",
+                            description = "${GlobalStatsService.computeSpentHours(state.abstractSessions)} hours spent, on average a set each ${
+                                GlobalStatsService.computeAvgApproachTime(
+                                    state.abstractSessions
+                                )
+                            } minutes",
+                            firstQuantifierQuantity = "${GlobalStatsService.computeSets(state.abstractSessions)}",
+                            firstQuantifierDescription = "Sets",
+                            secondQuantifierQuantity = "${GlobalStatsService.computeConvos(state.abstractSessions)}",
+                            secondQuantifierDescription = "Conversations",
+                            thirdQuantifierQuantity = "${GlobalStatsService.computeContacts(state.abstractSessions)}",
+                            thirdQuantifierDescription = "Contacts",
+                            firstPerformanceQuantity = "${
+                                GlobalStatsService.computeAvgConvoRatio(
+                                    state.abstractSessions
+                                )
+                            } %",
+                            firstPerformanceDescription = "Conversation\nRatio",
+                            secondPerformanceQuantity = "${
+                                GlobalStatsService.computeAvgRejectionRatio(
+                                    state.abstractSessions
+                                )
+                            } %",
+                            secondPerformanceDescription = "Rejection\nRatio",
+                            thirdPerformanceQuantity = "${
+                                GlobalStatsService.computeAvgContactRatio(
+                                    state.abstractSessions
+                                )
+                            } %",
+                            thirdPerformanceDescription = "Contact\nRatio",
+                            fourthPerformanceQuantity = "${GlobalStatsService.computeAvgIndex(state.abstractSessions)}",
+                            fourthPerformanceDescription = "Average\nIndex"
+                        )
+                    }
                 }
                 item {
-                    val numbers: Int =
-                        state.leads.filter { lead -> lead.contact == ContactTypeEnum.NUMBER.getField() }.size
-                    val socials: Int =
-                        state.leads.filter { lead -> lead.contact == ContactTypeEnum.SOCIAL.getField() }.size
-                    StatsCard(
-                        title = "Leads",
-                        description = "On average a new lead each ${
-                            GlobalStatsService.computeAvgLeadTime(
-                                state.leads.size,
-                                state.abstractSessions
-                            )
-                        } minutes",
-                        firstQuantifierQuantity = "${state.leads.size}",
-                        firstQuantifierDescription = "Leads",
-                        secondQuantifierQuantity = "${numbers}",
-                        secondQuantifierDescription = "Numbers",
-                        thirdQuantifierQuantity = "${socials}",
-                        thirdQuantifierDescription = "Social Medias",
-                        firstPerformanceQuantity = "${
-                            GlobalStatsService.computeGenericRatio(
-                                state.leads.size,
-                                numbers
-                            )
-                        } %",
-                        firstPerformanceDescription = "Number\nRatio",
-                        secondPerformanceQuantity = "${
-                            GlobalStatsService.computeGenericRatio(
-                                state.leads.size,
-                                socials
-                            )
-                        } %",
-                        secondPerformanceDescription = "Social\nRatio"
-                    )
+                    Row {
+                        Spacer(
+                            modifier = Modifier
+                                .width(spaceFromLeft)
+                        )
+                        val numbers: Int =
+                            state.leads.filter { lead -> lead.contact == ContactTypeEnum.NUMBER.getField() }.size
+                        val socials: Int =
+                            state.leads.filter { lead -> lead.contact == ContactTypeEnum.SOCIAL.getField() }.size
+                        StatsCard(
+                            modifier = cardModifier,
+                            title = "Leads",
+                            description = "On average a new lead each ${
+                                GlobalStatsService.computeAvgLeadTime(
+                                    state.leads.size,
+                                    state.abstractSessions
+                                )
+                            } minutes",
+                            firstQuantifierQuantity = "${state.leads.size}",
+                            firstQuantifierDescription = "Leads",
+                            secondQuantifierQuantity = "${numbers}",
+                            secondQuantifierDescription = "Numbers",
+                            thirdQuantifierQuantity = "${socials}",
+                            thirdQuantifierDescription = "Social Medias",
+                            firstPerformanceQuantity = "${
+                                GlobalStatsService.computeGenericRatio(
+                                    state.leads.size,
+                                    numbers
+                                )
+                            } %",
+                            firstPerformanceDescription = "Number\nRatio",
+                            secondPerformanceQuantity = "${
+                                GlobalStatsService.computeGenericRatio(
+                                    state.leads.size,
+                                    socials
+                                )
+                            } %",
+                            secondPerformanceDescription = "Social\nRatio"
+                        )
+                    }
                 }
                 item {
-                    sectionTitleAndDescription(
-                        "Sessions Histograms",
-                        "Number of sessions in which you reached a certain amount of:"
-                    )
-                    LazyRow {
+                    Row {
+                        Spacer(
+                            modifier = Modifier
+                                .width(spaceFromLeft)
+                        )
+                        sectionTitleAndDescription(
+                            "Sessions Histograms",
+                            "Number of sessions in which you reached a certain amount of:"
+                        )
+                    }
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(7.dp)
+                    ) {
+                        item {
+                            Spacer(
+                                modifier = Modifier.width(spaceFromLeft - 7.dp)
+                            )
+                        }
                         SessionsHistogramsSection(state, heigh, width)
+                        item {
+                            Spacer(
+                                modifier = Modifier.width(spaceFromLeft - 7.dp)
+                            )
+                        }
                     }
                 }
                 item {
-                    sectionTitleAndDescription(
-                        "Leads Histograms",
-                        "Number of leads with specific characteristics:"
-                    )
-                    LazyRow {
+                    Row {
+                        Spacer(
+                            modifier = Modifier
+                                .width(spaceFromLeft)
+                        )
+                        sectionTitleAndDescription(
+                            "Leads Histograms",
+                            "Number of leads with specific characteristics:"
+                        )
+                    }
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(7.dp)
+                    ) {
+                        item {
+                            Spacer(
+                                modifier = Modifier.width(spaceFromLeft - 7.dp)
+                            )
+                        }
                         LeadsHistogramsSection(state, heigh, width)
+                        item {
+                            Spacer(
+                                modifier = Modifier.width(spaceFromLeft - 7.dp)
+                            )
+                        }
                     }
                 }
-                item { Row(modifier = Modifier.height(spaceFromTop + spaceFromBottom)) {} }
+                item {
+                    Spacer(
+                        modifier = Modifier
+                            .height(spaceFromTop + spaceFromBottom + spaceFromLeft * 2)
+                    )
+                }
             }
         }
     }
