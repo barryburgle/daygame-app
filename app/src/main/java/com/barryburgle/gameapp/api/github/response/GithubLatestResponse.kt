756e8e73a2dbc87847b2889793a291137e608550
Barry Burgle <barryburgle@gmail.com>
2025-03-05 17:22:33 +0100

    removed

--- app/src/main/java/com/barryburgle/gameapp/api/github/response/GithubLatestResponse.kt
+++ app/src/main/java/com/barryburgle/gameapp/api/github/response/GithubLatestResponse.kt
@@ -3,7 +3,6 @@ package com.barryburgle.gameapp.api.github.response
 data class GithubLatestResponse(
     val assets: List<AssetsItem>?,
     val tag_name: String = "",
-    val html_url: String = "",
     val published_at: String = "",
     val body: String? = ""
 )
