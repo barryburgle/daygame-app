f8ddec8695b1a88acce155501efcca049da175ad
Barry Burgle <barryburgle@gmail.com>
2025-03-06 12:19:24 +0100

    update interface

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/SettingsCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/SettingsCard.kt
@@ -1,5 +1,6 @@
 package com.barryburgle.gameapp.ui.tool
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,26 +10,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Timer
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.event.ToolEvent
 import com.barryburgle.gameapp.ui.tool.state.ToolsState
+import com.barryburgle.gameapp.ui.utilities.BasicAnimatedVisibility
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.datetime.time.TimePickerDefaults
 import com.vanpra.composematerialdialogs.datetime.time.timepicker
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import dev.jeziellago.compose.markdowntext.MarkdownText
 import java.time.LocalTime
 
 @ExperimentalMaterial3Api
@@ -36,7 +47,8 @@ import java.time.LocalTime
 fun SettingsCard(
     state: ToolsState,
     modifier: Modifier,
-    onEvent: (ToolEvent) -> Unit
+    onEvent: (ToolEvent) -> Unit,
+    currentVersion: String
 ) {
     val notificationHourDialogState = rememberMaterialDialogState()
     MaterialDialog(
@@ -90,7 +102,8 @@ fun SettingsCard(
                 Column(
                     modifier = Modifier
                         .padding(5.dp)
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceBetween
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
@@ -102,6 +115,7 @@ fun SettingsCard(
                             style = MaterialTheme.typography.titleLarge
                         )
                     }
+                    versionInfo(state, currentVersion, onEvent)
                 }
                 Row(
                     horizontalArrangement = Arrangement.SpaceAround,
@@ -124,15 +138,25 @@ fun SettingsCard(
                         modifier = Modifier.padding(16.dp)
                     ) {
                         Column(
-                            modifier = Modifier.width(150.dp)
+                            modifier = Modifier.width(150.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceBetween
                         ) {
                             Button(
                                 colors = ButtonDefaults.buttonColors(
                                     containerColor = MaterialTheme.colorScheme.primaryContainer
                                 ),
                                 onClick = { notificationHourDialogState.show() }) {
+                                Icon(
+                                    imageVector = Icons.Default.Timer,
+                                    contentDescription = "Reminder",
+                                    tint = MaterialTheme.colorScheme.onPrimary,
+                                    modifier = Modifier
+                                        .height(25.dp)
+                                )
+                                Spacer(modifier = Modifier.width(7.dp))
                                 Text(
-                                    text = "Sticking points reminder",
+                                    text = "Sticking points",
                                     textAlign = TextAlign.Center
                                 )
                             }
@@ -140,6 +164,131 @@ fun SettingsCard(
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun updateRedDot() {
+    Row(
+        modifier = Modifier
+            .width(8.dp)
+            .height(8.dp)
+            .background(
+                MaterialTheme.colorScheme.error,
+                shape = RoundedCornerShape(5.dp)
+            )
+    ) {}
+}
+
+@Composable
+fun versionInfo(
+    state: ToolsState,
+    currentVersion: String,
+    onEvent: (ToolEvent) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val latestVersion = state.latestAvailable
+        var info = "Daygame App $currentVersion"
+        if (state.latestAvailable != null && !state.latestAvailable.isEmpty()) {
+            Row(
+                modifier = Modifier.fillMaxWidth(0.2f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                updateRedDot()
+                Spacer(modifier = Modifier.width(7.dp))
+                val uriHandler = LocalUriHandler.current
+                Column(
+                    modifier = Modifier
+                        .background(
+                            MaterialTheme.colorScheme.primaryContainer,
+                            shape = RoundedCornerShape(50.dp)
+                        )
+                ) {
+                    IconButton(
+                        onClick = {
+                            uriHandler.openUri(state.latestDownloadUrl)
+                        }) {
+                        Icon(
+                            imageVector = Icons.Default.Download,
+                            contentDescription = "Update",
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            info = "Update to $latestVersion"
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = info,
+                textAlign = TextAlign.Left,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.fillMaxWidth(0.65f)
+            )
+            changelog(state, onEvent)
+        }
+    }
+    if (state.showChangelog) {
+        Spacer(modifier = Modifier.height(12.dp))
+    }
+    BasicAnimatedVisibility(
+        visibilityFlag = state.showChangelog,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MarkdownText(
+                    markdown = state.latestChangelog
+                        .split("\n")
+                        .drop(2)
+                        .joinToString("\n"),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun changelog(
+    state: ToolsState,
+    onEvent: (ToolEvent) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Changelog",
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        IconButton(onClick = {
+            onEvent(ToolEvent.SwitchShowChangelog)
+        }) {
+            Icon(
+                imageVector = if (state.showChangelog) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown,
+                contentDescription = "Changelog",
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .height(50.dp)
+            )
         }
     }
 }
