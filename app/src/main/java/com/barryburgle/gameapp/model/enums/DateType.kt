d448604138a8237950cb1d9341b79c19534cfece
Barry Burgle <barryburgle@gmail.com>
2025-03-11 23:51:44 +0100

    icon management

--- app/src/main/java/com/barryburgle/gameapp/model/enums/DateType.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/DateType.kt
@@ -1,5 +1,14 @@
 package com.barryburgle.gameapp.model.enums
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.DirectionsWalk
+import androidx.compose.material.icons.filled.Coffee
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Panorama
+import androidx.compose.material.icons.filled.Restaurant
+import androidx.compose.material.icons.filled.WineBar
+import androidx.compose.ui.graphics.vector.ImageVector
+
 enum class DateType(private val type: String) {
     DRINK("drink"),
     COFFEE("coffee"),
@@ -9,5 +18,18 @@ enum class DateType(private val type: String) {
 
     fun getType(): String {
         return type
+    }
+
+    companion object {
+        fun getIcon(type: String): ImageVector {
+            return when (type) {
+                DRINK.getType() -> Icons.Default.WineBar
+                COFFEE.getType() -> Icons.Default.Coffee
+                WALK.getType() -> Icons.AutoMirrored.Filled.DirectionsWalk
+                FOOD.getType() -> Icons.Default.Restaurant
+                EXPERIENCE.getType() -> Icons.Default.Panorama
+                else -> Icons.Default.Favorite
+            }
+        }
     }
 }
