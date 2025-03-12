7db83ec910cf6bb8a7a69f722d7668370d7942ef
Barry Burgle <barryburgle@gmail.com>
2025-03-12 23:44:11 +0100

    defined event functions

--- app/src/main/java/com/barryburgle/gameapp/event/DateEvent.kt
+++ app/src/main/java/com/barryburgle/gameapp/event/DateEvent.kt
@@ -11,19 +11,20 @@ sealed interface DateEvent : GenericEvent {
     data class SetLeadId(val leadId: Long) : DateEvent
     data class SetLocation(val location: String) : DateEvent
     data class SetMeetingDate(val date: String) : DateEvent
-    data class SetStartTime(val startTime: String) : DateEvent
-    data class SetEndTime(val endHour: String) : DateEvent
-    data class SetCost(val cost: Double) : DateEvent
-    data class SetDateNumber(val dateNumber: Long) : DateEvent
-    data class SetPull(val pull: Boolean) : DateEvent
-    data class SetBounce(val bounce: Boolean) : DateEvent
-    data class SetKiss(val kiss: Boolean) : DateEvent
-    data class SetLay(val lay: Boolean) : DateEvent
-    data class SetRecorded(val recorded: Boolean) : DateEvent
+    data class SetStartHour(val startHour: String) : DateEvent
+    data class SetEndHour(val endHour: String) : DateEvent
+    data class SetCost(val cost: String) : DateEvent
+    data class SetDateNumber(val dateNumber: String) : DateEvent
+    object SwitchPull: DateEvent
+    object SwitchBounce: DateEvent
+    object SwitchKiss: DateEvent
+    object SwitchLay: DateEvent
+    object SwitchRecorded: DateEvent
     data class SetStickingPoints(val stickingPoints: String) : DateEvent
     data class SetTweetUrl(val tweetUrl: String) : DateEvent
     data class SortDates(val sortType: DateSortType) : DateEvent
     data class DeleteDate(val date: Date) : DateEvent
     data class EditDate(val date: Date) : DateEvent
     data class EditLead(val lead: Lead, val isUpdatingLead: Boolean) : DateEvent
+    object EmptyLead : DateEvent
 }
--- app/src/main/java/com/barryburgle/gameapp/ui/date/DateViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/date/DateViewModel.kt
@@ -6,12 +6,15 @@ import com.barryburgle.gameapp.dao.date.DateDao
 import com.barryburgle.gameapp.dao.lead.LeadDao
 import com.barryburgle.gameapp.event.DateEvent
 import com.barryburgle.gameapp.model.enums.DateSortType
+import com.barryburgle.gameapp.service.date.DateService
 import com.barryburgle.gameapp.ui.CombineThree
 import com.barryburgle.gameapp.ui.date.state.DateState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class DateViewModel(
     private val dateDao: DateDao,
@@ -20,13 +23,15 @@ class DateViewModel(
     private val _state =
         MutableStateFlow(DateState())
     private val _dateSortType = MutableStateFlow(DateSortType.DATE)
+    private val _dateService = DateService()
+    private val _sortType = MutableStateFlow(DateSortType.DATE)
     private val _dates = _dateSortType.flatMapLatest { sortType ->
         when (sortType) {
             DateSortType.DATE -> dateDao.getByDate()
             DateSortType.LEAD -> dateDao.getByLead()
             DateSortType.LOCATION -> dateDao.getByLocation()
-            DateSortType.START_TIME -> dateDao.getByStartTime()
-            DateSortType.END_TIME -> dateDao.getByEndTime()
+            DateSortType.START_TIME -> dateDao.getByStartHour()
+            DateSortType.END_TIME -> dateDao.getByEndHour()
             DateSortType.DATE_TIME -> dateDao.getByDate() /*TODO: COMPUTE DATE TIME ON THE FLY AND SORT IN LIST*/
             DateSortType.COST -> dateDao.getByCost()
             DateSortType.DATE_NUMBER -> dateDao.getByDateNumber()
@@ -59,67 +64,221 @@ class DateViewModel(
 
     fun onEvent(event: DateEvent) {
         when (event) {
-            is DateEvent.SaveDate -> { /*TODO: write implementation*/
+            is DateEvent.SaveDate -> {
+                viewModelScope.launch {
+                    val date = _dateService.init(
+                        id = null,
+                        leadId = _state.value.leadId,
+                        location = if (_state.value.location.isBlank()) state.value.location else _state.value.location,
+                        date = if (_state.value.date.isBlank()) state.value.date else _state.value.date,
+                        startHour = if (_state.value.startHour.isBlank()) state.value.startHour else _state.value.startHour,
+                        endHour = if (_state.value.endHour.isBlank()) state.value.endHour else _state.value.endHour,
+                        cost = if (_state.value.cost.isBlank()) state.value.cost.toInt() else _state.value.cost.toInt(),
+                        dateNumber = if (_state.value.dateNumber.isBlank()) state.value.dateNumber.toInt() else _state.value.dateNumber.toInt(),
+                        dateType = _state.value.dateType,
+                        pull = _state.value.pull,
+                        bounce = _state.value.bounce,
+                        kiss = _state.value.kiss,
+                        lay = _state.value.lay,
+                        recorded = _state.value.recorded,
+                        stickingPoints = if (_state.value.stickingPoints.isBlank()) state.value.stickingPoints else _state.value.stickingPoints,
+                        tweetUrl = if (_state.value.tweetUrl.isBlank()) state.value.tweetUrl else _state.value.tweetUrl
+                    )
+                    if (state.value.isUpdatingDate) {
+                        date.id = state.value.editDate!!.id
+                        date.insertTime = state.value.editDate!!.insertTime
+                        dateDao.insert(date)
+                    }
+                    _state.update {
+                        it.copy(
+                            leadId = 0L,
+                            location = "",
+                            date = "",
+                            startHour = "",
+                            endHour = "",
+                            cost = "0",
+                            dateNumber = "0",
+                            dateType = "",
+                            pull = false,
+                            bounce = false,
+                            kiss = false,
+                            lay = false,
+                            recorded = false,
+                            stickingPoints = "",
+                            tweetUrl = "",
+                            sortType = DateSortType.DATE,
+                            isAddingDate = false,
+                            isUpdatingDate = false
+                        )
+                    }
+                }
             }
 
-            is DateEvent.ShowDialog -> { /*TODO: write implementation*/
+            is DateEvent.ShowDialog -> {
+                _state.update {
+                    it.copy(
+                        isAddingDate = event.addDate,
+                        isUpdatingDate = event.updateDate
+                    )
+                }
+                if (_state.value.isAddingDate) {
+                    onEvent(DateEvent.EmptyLead)
+                }
             }
 
-            is DateEvent.HideDialog -> { /*TODO: write implementation*/
+            is DateEvent.HideDialog -> {
+                _state.update {
+                    it.copy(
+                        isAddingDate = false,
+                        isUpdatingDate = false
+                    )
+                }
             }
 
-            is DateEvent.SetLeadId -> { /*TODO: write implementation*/
+            is DateEvent.SetLeadId -> {
+                _state.update {
+                    it.copy(
+                        leadId = event.leadId
+                    )
+                }
             }
 
-            is DateEvent.SetLocation -> { /*TODO: write implementation*/
+            is DateEvent.SetLocation -> {
+                _state.update {
+                    it.copy(
+                        location = event.location
+                    )
+                }
             }
 
-            is DateEvent.SetMeetingDate -> { /*TODO: write implementation*/
+            is DateEvent.SetMeetingDate -> {
+                _state.update {
+                    it.copy(
+                        date = event.date
+                    )
+                }
             }
 
-            is DateEvent.SetStartTime -> { /*TODO: write implementation*/
+            is DateEvent.SetStartHour -> {
+                _state.update {
+                    it.copy(
+                        startHour = event.startHour
+                    )
+                }
             }
 
-            is DateEvent.SetEndTime -> { /*TODO: write implementation*/
+            is DateEvent.SetEndHour -> {
+                _state.update {
+                    it.copy(
+                        endHour = event.endHour
+                    )
+                }
             }
 
-            is DateEvent.SetCost -> { /*TODO: write implementation*/
+            is DateEvent.SetCost -> {
+                _state.update {
+                    it.copy(
+                        cost = event.cost
+                    )
+                }
             }
 
-            is DateEvent.SetDateNumber -> { /*TODO: write implementation*/
+            is DateEvent.SetDateNumber -> {
+                _state.update {
+                    it.copy(
+                        dateNumber = event.dateNumber
+                    )
+                }
             }
 
-            is DateEvent.SetPull -> { /*TODO: write implementation*/
+            is DateEvent.SwitchPull -> {
+                _state.update {
+                    it.copy(
+                        pull = _state.value.pull.not()
+                    )
+                }
             }
 
-            is DateEvent.SetBounce -> { /*TODO: write implementation*/
+            is DateEvent.SwitchBounce -> {
+                _state.update {
+                    it.copy(
+                        bounce = _state.value.bounce.not()
+                    )
+                }
             }
 
-            is DateEvent.SetKiss -> { /*TODO: write implementation*/
+            is DateEvent.SwitchKiss -> {
+                _state.update {
+                    it.copy(
+                        kiss = _state.value.kiss.not()
+                    )
+                }
             }
 
-            is DateEvent.SetLay -> { /*TODO: write implementation*/
+            is DateEvent.SwitchLay -> {
+                _state.update {
+                    it.copy(
+                        lay = _state.value.lay.not()
+                    )
+                }
             }
 
-            is DateEvent.SetRecorded -> { /*TODO: write implementation*/
+            is DateEvent.SwitchRecorded -> {
+                _state.update {
+                    it.copy(
+                        recorded = _state.value.recorded.not()
+                    )
+                }
             }
 
-            is DateEvent.SetStickingPoints -> { /*TODO: write implementation*/
+            is DateEvent.SetStickingPoints -> {
+                _state.update {
+                    it.copy(
+                        stickingPoints = event.stickingPoints
+                    )
+                }
             }
 
-            is DateEvent.SetTweetUrl -> { /*TODO: write implementation*/
+            is DateEvent.SetTweetUrl -> {
+                _state.update {
+                    it.copy(
+                        tweetUrl = event.tweetUrl
+                    )
+                }
             }
 
-            is DateEvent.SortDates -> { /*TODO: write implementation*/
+            is DateEvent.SortDates -> {
+                _sortType.value = event.sortType
             }
 
-            is DateEvent.DeleteDate -> { /*TODO: write implementation*/
+            is DateEvent.DeleteDate -> {
+                viewModelScope.launch {
+                    dateDao.delete(event.date)
+                }
             }
 
-            is DateEvent.EditDate -> { /*TODO: write implementation*/
+            is DateEvent.EditDate -> {
+                _state.update {
+                    it.copy(
+                        editDate = event.date,
+                    )
+                }
             }
 
-            is DateEvent.EditLead -> { /*TODO: write implementation*/
+            is DateEvent.EditLead -> {
+                _state.update {
+                    it.copy(
+                        lead = event.lead,
+                    )
+                }
+            }
+
+            is DateEvent.EmptyLead -> {
+                _state.update {
+                    it.copy(
+                        lead = null
+                    )
+                }
             }
         }
     }
