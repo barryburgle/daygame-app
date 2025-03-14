79c7095b548ff95e2810b8e87861e0f2be432941
Barry Burgle <barryburgle@gmail.com>
2025-03-14 16:50:20 +0100

    improved with text description

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/ToggleIcon.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/ToggleIcon.kt
@@ -3,12 +3,16 @@ package com.barryburgle.gameapp.ui.utilities;
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable;
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,29 +26,36 @@ import androidx.compose.ui.unit.dp
 fun ToggleIcon(
     description: String, flag: Boolean, @DrawableRes icon: Int, onCheckedChange: () -> Unit
 ) {
-    IconButton(
-        onClick = {
-            onCheckedChange()
-        }, modifier = Modifier
-            .background(
-                color = MaterialTheme.colorScheme.primaryContainer,
-                shape = RoundedCornerShape(20.dp)
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        IconButton(
+            onClick = {
+                onCheckedChange()
+            }, modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .size(40.dp)
+        ) {
+            var color: Color = MaterialTheme.colorScheme.onPrimary
+            if (!flag) {
+                color = MaterialTheme.colorScheme.primary
+            }
+            Image(
+                painter = painterResource(icon),
+                contentDescription = description,
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(7.dp),
+                colorFilter = ColorFilter.tint(color)
             )
-            .size(40.dp)
-    ) {
-        var color: Color = MaterialTheme.colorScheme.onPrimary
-        if (!flag) {
-            color = MaterialTheme.colorScheme.primary
         }
-        Image(
-            painter = painterResource(icon),
-            contentDescription = description,
-            alignment = Alignment.Center,
-            contentScale = ContentScale.Fit,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(7.dp),
-            colorFilter = ColorFilter.tint(color)
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = description.replaceFirstChar { it.uppercase() },
+            style = MaterialTheme.typography.bodySmall
         )
     }
 }
