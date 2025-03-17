30408b6deb497a7759a7ac29184ea7bcf4a9d39a
Barry Burgle <barryburgle@gmail.com>
2025-03-17 23:45:39 +0100

    passing dates to state

--- app/src/main/java/com/barryburgle/gameapp/ui/stats/StatsViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/StatsViewModel.kt
@@ -2,9 +2,10 @@ package com.barryburgle.gameapp.ui.stats
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.barryburgle.gameapp.dao.date.DateDao
 import com.barryburgle.gameapp.dao.lead.LeadDao
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
-import com.barryburgle.gameapp.ui.CombineEight
+import com.barryburgle.gameapp.ui.CombineNine
 import com.barryburgle.gameapp.ui.stats.state.StatsState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -12,12 +13,14 @@ import kotlinx.coroutines.flow.stateIn
 
 class StatsViewModel(
     private val abstractSessionDao: AbstractSessionDao,
-    private val leadDao: LeadDao
+    private val leadDao: LeadDao,
+    private val dateDao: DateDao
 ) : ViewModel() {
     private val _state =
         MutableStateFlow(StatsState())
     private val _abstractSessions = abstractSessionDao.getAll()
     private val _leads = leadDao.getAll()
+    private val _dates = dateDao.getAll()
     private val _setsHistogram = abstractSessionDao.getSetsHistogram()
     private val _convosHistogram = abstractSessionDao.getConvosHistogram()
     private val _contactsHistogram = abstractSessionDao.getContactsHistogram()
@@ -25,19 +28,21 @@ class StatsViewModel(
     private val _nationalityHistogram = leadDao.getNationalityHistogram()
 
     val state =
-        CombineEight(
+        CombineNine(
             _state,
             _abstractSessions,
             _leads,
+            _dates,
             _setsHistogram,
             _convosHistogram,
             _contactsHistogram,
             _ageHistogram,
             _nationalityHistogram
-        ) { state, abstractSessions, leads, setsHistogram, convosHistogram, contactsHistogram, ageHistogram, nationalityHistogram ->
+        ) { state, abstractSessions, leads, dates, setsHistogram, convosHistogram, contactsHistogram, ageHistogram, nationalityHistogram ->
             state.copy(
                 abstractSessions = abstractSessions,
                 leads = leads,
+                dates = dates,
                 setsHistogram = setsHistogram,
                 convosHistogram = convosHistogram,
                 contactsHistogram = contactsHistogram,
--- app/src/main/java/com/barryburgle/gameapp/ui/stats/state/StatsState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/state/StatsState.kt
@@ -1,5 +1,6 @@
 package com.barryburgle.gameapp.ui.stats.state
 
+import com.barryburgle.gameapp.model.date.Date
 import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
 import com.barryburgle.gameapp.model.stat.CategoryHistogram
@@ -8,6 +9,7 @@ import com.barryburgle.gameapp.model.stat.Histogram
 data class StatsState(
     val abstractSessions: List<AbstractSession> = emptyList(),
     val leads: List<Lead> = emptyList(),
+    val dates: List<Date> = emptyList(),
     val setsHistogram: List<Histogram> = emptyList(),
     val convosHistogram: List<Histogram> = emptyList(),
     val contactsHistogram: List<Histogram> = emptyList(),
