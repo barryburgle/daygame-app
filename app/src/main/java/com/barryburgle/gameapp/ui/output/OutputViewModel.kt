9113724dea61561c27a1abbd48a8ba58cff0c1cb
Barry Burgle <barryburgle@gmail.com>
2025-03-17 23:12:39 +0100

    passing aggregated dates infos

--- app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/OutputViewModel.kt
@@ -4,11 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.barryburgle.gameapp.dao.lead.LeadDao
 import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.dao.session.AggregatedDatesDao
 import com.barryburgle.gameapp.dao.session.AggregatedSessionsDao
 import com.barryburgle.gameapp.dao.setting.SettingDao
 import com.barryburgle.gameapp.event.OutputEvent
 import com.barryburgle.gameapp.manager.SessionManager
-import com.barryburgle.gameapp.ui.CombineSix
+import com.barryburgle.gameapp.model.stat.AggregatedDates
+import com.barryburgle.gameapp.model.stat.AggregatedSessions
+import com.barryburgle.gameapp.ui.CombineEight
 import com.barryburgle.gameapp.ui.output.state.OutputState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -18,6 +21,7 @@ import kotlinx.coroutines.flow.update
 class OutputViewModel(
     private val abstractSessionDao: AbstractSessionDao,
     private val aggregatedSessionsDao: AggregatedSessionsDao,
+    private val aggregatedDatesDao: AggregatedDatesDao,
     private val settingDao: SettingDao,
     private val leadDao: LeadDao
 ) : ViewModel() {
@@ -26,26 +30,34 @@ class OutputViewModel(
     // TODO: let user set n for following query from tools screen with a writing query on db
     private val _abstractSessions = abstractSessionDao.getAllLimit(14)
     private val _leads = leadDao.getAll()
-    private val _weekStats = aggregatedSessionsDao.groupStatsByWeekNumber()
+    private val _sessionsByWeek = aggregatedSessionsDao.groupStatsByWeekNumber()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
-    private val _monthStats = aggregatedSessionsDao.groupStatsByMonth()
+    private val _sessionsByMonth = aggregatedSessionsDao.groupStatsByMonth()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    private val _datesByWeek = aggregatedDatesDao.groupStatsByWeekNumber()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    private val _datesByMonth = aggregatedDatesDao.groupStatsByMonth()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     private val _averageLast = settingDao.getAverageLast()
 
-    val state = CombineSix(
+    val state = CombineEight(
         _state,
         _abstractSessions,
         _leads,
-        _weekStats,
-        _monthStats,
+        _sessionsByWeek,
+        _sessionsByMonth,
+        _datesByWeek,
+        _datesByMonth,
         _averageLast
     )
-    { state, abstractSessions, leads, weekStats, monthStats, averageLast ->
+    { state, abstractSessions, leads, sessionsByWeek, sessionsByMonth, datesByWeek, datesByMonth, averageLast ->
         state.copy(
             abstractSessions = SessionManager.normalizeSessionsIds(abstractSessions),
             leads = leads,
-            weekStats = SessionManager.normalizeIds(weekStats),
-            monthStats = SessionManager.normalizeIds(monthStats),
+            sessionsByWeek = SessionManager.normalizeIds(sessionsByWeek) as List<AggregatedSessions>,
+            sessionsByMonth = SessionManager.normalizeIds(sessionsByMonth) as List<AggregatedSessions>,
+            datesByWeek = SessionManager.normalizeIds(datesByWeek) as List<AggregatedDates>,
+            datesByMonth = SessionManager.normalizeIds(datesByMonth) as List<AggregatedDates>,
             movingAverageWindow = averageLast
         )
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OutputState())
