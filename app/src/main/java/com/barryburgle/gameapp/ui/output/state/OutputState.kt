cea7df9a13bd04b98904d8bcc79848ff8b5999ce
Barry Burgle <barryburgle@gmail.com>
2025-03-17 23:10:36 +0100

    renamed and added fields

--- app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/output/state/OutputState.kt
@@ -2,13 +2,16 @@ package com.barryburgle.gameapp.ui.output.state
 
 import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
+import com.barryburgle.gameapp.model.stat.AggregatedDates
 import com.barryburgle.gameapp.model.stat.AggregatedSessions
 
 data class OutputState(
     val abstractSessions: List<AbstractSession> = emptyList(),
     val leads: List<Lead> = emptyList(),
-    val weekStats: List<AggregatedSessions> = emptyList(),
-    val monthStats: List<AggregatedSessions> = emptyList(),
+    val sessionsByWeek: List<AggregatedSessions> = emptyList(),
+    val sessionsByMonth: List<AggregatedSessions> = emptyList(),
+    val datesByWeek: List<AggregatedDates> = emptyList(),
+    val datesByMonth: List<AggregatedDates> = emptyList(),
     val movingAverageWindow: Int = 4,
     val showLeadsLegend: Boolean = false,
     val showIndexFormula: Boolean = false
