1ed8f48a19a549d6121f82d5da3c196476709ab1
Barry Burgle <barryburgle@gmail.com>
2025-03-21 10:51:23 +0100

    switch replaced with toggle

--- app/src/main/java/com/barryburgle/gameapp/ui/input/LeadDialog.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/LeadDialog.kt
@@ -1,7 +1,6 @@
 package com.barryburgle.gameapp.ui.input
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -10,11 +9,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -36,9 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.R
@@ -47,7 +42,7 @@ import com.barryburgle.gameapp.model.enums.ContactTypeEnum
 import com.barryburgle.gameapp.model.enums.CountryEnum
 import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.ui.input.state.InputState
-import com.barryburgle.gameapp.ui.tool.utils.Switch
+import com.barryburgle.gameapp.ui.utilities.ToggleIcon
 
 @Composable
 fun AddLeadDialog(
@@ -174,56 +169,20 @@ fun AddLeadDialog(
                 val isDarkTheme = isSystemInDarkTheme()
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth(0.7f)
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxHeight()
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(20.dp)
-                                .aspectRatio(1f)
-                        ) {
-                            Image(
-                                painter = painterResource(if (isDarkTheme) R.drawable.whatsapp_w else R.drawable.whatsapp_b),
-                                contentDescription = "Whatsapp Icon",
-                                alignment = Alignment.Center,
-                                contentScale = ContentScale.Crop
-                            )
-                        }
-                        Text(
-                            "Whatsapp"
-                        )
-                        Switch(numberFlag){
-                            onEvent(AbstractSessionEvent.SetLeadContact(ContactTypeEnum.NUMBER.getField()))
-                        }
-                    }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(20.dp)
-                                .aspectRatio(1f)
-                        ) {
-                            Image(
-                                painter = painterResource(if (isDarkTheme) R.drawable.instagram_w else R.drawable.instagram_b),
-                                contentDescription = "Instagram Icon",
-                                alignment = Alignment.Center,
-                                contentScale = ContentScale.Crop
-                            )
-                        }
-                        Text(
-                            "Instagram"
-                        )
-                        Switch(socialFlag){
-                            onEvent(AbstractSessionEvent.SetLeadContact(ContactTypeEnum.SOCIAL.getField()))
-                        }
-                    }
+                    Text(text = "", textAlign = TextAlign.Center)
+                    ToggleIcon(
+                        "Whatsapp",
+                        ContactTypeEnum.NUMBER.getField().equals(state.leadContact),
+                        if (isDarkTheme) R.drawable.whatsapp_w else R.drawable.whatsapp_b
+                    ) { onEvent(AbstractSessionEvent.SetLeadContact(ContactTypeEnum.NUMBER.getField())) }
+                    ToggleIcon(
+                        "Instagram",
+                        ContactTypeEnum.SOCIAL.getField().equals(state.leadContact),
+                        if (isDarkTheme) R.drawable.instagram_w else R.drawable.instagram_b
+                    ) { onEvent(AbstractSessionEvent.SetLeadContact(ContactTypeEnum.SOCIAL.getField())) }
                 }
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
