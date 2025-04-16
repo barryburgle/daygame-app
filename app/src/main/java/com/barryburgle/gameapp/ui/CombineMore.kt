ea936f1edc20fba556797c56c83b22fff55d211c
Barry Burgle <barryburgle@gmail.com>
2025-04-16 23:15:59 +0200

    new combine methods

--- app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
@@ -4,6 +4,26 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
 @Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, R> CombineSix(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    transform: suspend (T1, T2, T3, T4, T5, T6) -> R
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6) { args: Array<*> ->
+    transform(
+        args[0] as T1,
+        args[1] as T2,
+        args[2] as T3,
+        args[3] as T4,
+        args[4] as T5,
+        args[5] as T6
+    )
+}
+
+@Suppress("UNCHECKED_CAST")
 fun <T1, T2, T3, T4, T5, T6, T7, T8, R> CombineEight(
     flow: Flow<T1>,
     flow2: Flow<T2>,
@@ -73,7 +93,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R> CombineTwelve(
 }
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, R> CombineSeventeen(
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, R> CombineEighteen(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
@@ -91,7 +111,8 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow15: Flow<T15>,
     flow16: Flow<T16>,
     flow17: Flow<T17>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,) -> R
+    flow18: Flow<T18>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18) -> R
 ): Flow<R> = combine(
     flow,
     flow2,
@@ -110,6 +131,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow15,
     flow16,
     flow17,
+    flow18,
 ) { args: Array<*> ->
     transform(
         args[0] as T1,
@@ -129,6 +151,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
         args[14] as T15,
         args[15] as T16,
         args[16] as T17,
+        args[17] as T18,
     )
 }
 
