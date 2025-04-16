360939b2c3accd515ded887eedcb6f2fe9eb3a86
Barry Burgle <barryburgle@gmail.com>
2025-04-16 23:56:49 +0200

    setdialog called for adding and updating sets

--- app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/InputScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.GroupAdd
@@ -44,11 +43,8 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.event.GameEvent
-import com.barryburgle.gameapp.event.GenericEvent
 import com.barryburgle.gameapp.model.date.Date
-import com.barryburgle.gameapp.model.enums.DateSortType
 import com.barryburgle.gameapp.model.enums.EventTypeEnum
-import com.barryburgle.gameapp.model.enums.SortType
 import com.barryburgle.gameapp.model.game.SortableGameEvent
 import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
@@ -56,11 +52,9 @@ import com.barryburgle.gameapp.service.exchange.DataExchangeService
 import com.barryburgle.gameapp.ui.input.card.EventCard
 import com.barryburgle.gameapp.ui.input.dialog.DateDialog
 import com.barryburgle.gameapp.ui.input.dialog.SessionDialog
+import com.barryburgle.gameapp.ui.input.dialog.SetDialog
 import com.barryburgle.gameapp.ui.input.state.InputState
-import com.barryburgle.gameapp.ui.utilities.BasicAnimatedVisibility
 import com.barryburgle.gameapp.ui.utilities.InsertInvite
-import com.barryburgle.gameapp.ui.utilities.ScrollableSorter
-import com.barryburgle.gameapp.ui.utilities.SelectionRow
 import com.barryburgle.gameapp.ui.utilities.selection.MultiChoiceButton
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -158,6 +152,12 @@ fun InputScreen(
         }
         if (state.isUpdatingDate) {
             DateDialog(state = state, onEvent = onEvent, "Edit a date")
+        }
+        if (state.isAddingSet) {
+            SetDialog(state = state, onEvent = onEvent, "Add a set")
+        }
+        if (state.isUpdatingSet) {
+            SetDialog(state = state, onEvent = onEvent, "Edit a set")
         }
         if (state.justSaved && state.backupActive) {
             runBlocking {
