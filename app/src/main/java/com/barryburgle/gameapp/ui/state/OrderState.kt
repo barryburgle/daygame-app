efb0bfeafe1cb2f3c5d5582e6627dc177b182cf1
Barry Burgle <barryburgle@gmail.com>
2025-04-16 23:55:18 +0200

    integrating allsets list

--- app/src/main/java/com/barryburgle/gameapp/ui/state/OrderState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/state/OrderState.kt
@@ -4,6 +4,7 @@ import com.barryburgle.gameapp.model.date.Date
 import com.barryburgle.gameapp.model.enums.FieldEnum
 import com.barryburgle.gameapp.model.lead.Lead
 import com.barryburgle.gameapp.model.session.AbstractSession
+import com.barryburgle.gameapp.model.set.SingleSet
 
 open class OrderState(
     open val sortType: FieldEnum? = null,
@@ -17,6 +18,7 @@ open class OrderState(
     open var allSessions: List<AbstractSession> = emptyList(),
     open var allLeads: List<Lead> = emptyList(),
     open var allDates: List<Date> = emptyList(),
+    open var allSets: List<SingleSet> = emptyList(),
     open var backupActive: Boolean = true,
     open var lastBackup: Int = 3,
     open var justSaved: Boolean = false
