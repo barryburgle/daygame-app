7580fb9780b90f345a5b0a671e5ec282871b358b
Barry Burgle <barryburgle@gmail.com>
2025-04-18 15:33:23 +0200

    set dialog first version

---
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/dialog/SetDialog.kt
@@ -0,0 +1,402 @@
+package com.barryburgle.gameapp.ui.input.dialog
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.PinDrop
+import androidx.compose.material.icons.filled.SwapHoriz
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.barryburgle.gameapp.R
+import com.barryburgle.gameapp.event.GameEvent
+import com.barryburgle.gameapp.model.enums.CountryEnum
+import com.barryburgle.gameapp.model.enums.SetSortType
+import com.barryburgle.gameapp.service.FormatService
+import com.barryburgle.gameapp.ui.input.state.InputState
+import com.barryburgle.gameapp.ui.utilities.BasicAnimatedVisibility
+import com.barryburgle.gameapp.ui.utilities.DialogConstant
+import com.barryburgle.gameapp.ui.utilities.ToggleIcon
+import com.barryburgle.gameapp.ui.utilities.button.TweetLinkImportButton
+import com.barryburgle.gameapp.ui.utilities.dialog.DialogFormSectionDescription
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.date.DatePickerDefaults
+import com.vanpra.composematerialdialogs.datetime.date.datepicker
+import com.vanpra.composematerialdialogs.datetime.time.TimePickerDefaults
+import com.vanpra.composematerialdialogs.datetime.time.timepicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import java.time.LocalDate
+import java.time.LocalTime
+
+@Composable
+fun SetDialog(
+    state: InputState,
+    onEvent: (GameEvent) -> Unit,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    // TODO: make all the fields displayed in the dialog change when in edit mode (similarly to counters)
+    val localContext = LocalContext.current.applicationContext
+    val clipboardManager: ClipboardManager = LocalClipboardManager.current
+    val setDialogState = rememberMaterialDialogState()
+    val startHourDialogState = rememberMaterialDialogState()
+    val endHourDialogState = rememberMaterialDialogState()
+    var locationTextFieldExpanded by remember { mutableStateOf(false) }
+    if (state.isUpdatingSet) {
+        setState(state)
+    }
+    // TODO: re-use common parts in an inheritable event dialog
+    MaterialDialog(
+        dialogState = setDialogState, elevation = 10.dp, buttons = {
+            positiveButton(
+                "Ok", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+            negativeButton(
+                "Cancel", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+        }, shape = MaterialTheme.shapes.extraLarge
+    ) {
+        this.datepicker(
+            initialDate = if (state.isAddingSet || state.editSet == null) LocalDate.now() else FormatService.parseDate(
+                state.editSet.date
+            ), title = "Set date", colors = DatePickerDefaults.colors(
+                headerBackgroundColor = MaterialTheme.colorScheme.background,
+                headerTextColor = MaterialTheme.colorScheme.onPrimary,
+                dateActiveBackgroundColor = MaterialTheme.colorScheme.tertiary,
+                dateActiveTextColor = MaterialTheme.colorScheme.onPrimary,
+                dateInactiveBackgroundColor = MaterialTheme.colorScheme.onTertiary,
+                dateInactiveTextColor = MaterialTheme.colorScheme.background
+            )
+        ) {
+            onEvent(GameEvent.SetDate(it.toString()))
+        }
+    }
+    MaterialDialog(
+        dialogState = startHourDialogState, elevation = 10.dp, buttons = {
+            positiveButton(
+                "Ok", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+            negativeButton(
+                "Cancel", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+        }, shape = MaterialTheme.shapes.extraLarge
+    ) {
+        this.timepicker(
+            initialTime = if (state.isAddingSet || state.editDate == null) LocalTime.now() else FormatService.parseTime(
+                state.editSet!!.startHour
+            ), title = "Set start hour", colors = TimePickerDefaults.colors(
+                selectorColor = MaterialTheme.colorScheme.onPrimary,
+                activeBackgroundColor = MaterialTheme.colorScheme.tertiary,
+                activeTextColor = MaterialTheme.colorScheme.background,
+                inactiveBackgroundColor = MaterialTheme.colorScheme.primary,
+                inactiveTextColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            onEvent(GameEvent.SetStartHour(it.toString().substring(0, 5)))
+        }
+    }
+    MaterialDialog(
+        dialogState = endHourDialogState, elevation = 10.dp, buttons = {
+            positiveButton(
+                "Ok", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+            negativeButton(
+                "Cancel", textStyle = TextStyle(
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+            )
+        }, shape = MaterialTheme.shapes.extraLarge
+    ) {
+        this.timepicker(
+            initialTime = if (state.isAddingSet || state.editSet == null) LocalTime.now() else FormatService.parseTime(
+                state.editSet!!.endHour
+            ), title = "Set end hour", colors = TimePickerDefaults.colors(
+                selectorColor = MaterialTheme.colorScheme.onPrimary,
+                activeBackgroundColor = MaterialTheme.colorScheme.tertiary,
+                activeTextColor = MaterialTheme.colorScheme.background,
+                inactiveBackgroundColor = MaterialTheme.colorScheme.primary,
+                inactiveTextColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            onEvent(GameEvent.SetEndHour(it.toString().substring(0, 5)))
+        }
+    }
+    AlertDialog(modifier = modifier.shadow(elevation = 10.dp), onDismissRequest = {
+        onEvent(GameEvent.HideDialog)
+    }, title = {
+        Text(text = description)
+    }, text = {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(DialogConstant.ADD_LEAD_COLUMN_WIDTH),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.width(DialogConstant.TIME_COLUMN_WIDTH)
+                        ) {
+                            DialogFormSectionDescription(
+                                "Set set's:",
+                                DialogConstant.DESCRIPTION_FONT_SIZE
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(10.dp))
+                        var leadIcon = Icons.Default.Add
+                        Column(
+                            modifier = Modifier.width(DialogConstant.LEAD_COLUMN_WIDTH - DialogConstant.ADD_LEAD_COLUMN_WIDTH)
+                        ) {
+                            if (state.leadId == 0L) {
+                                DialogFormSectionDescription(
+                                    "Add lead:",
+                                    DialogConstant.DESCRIPTION_FONT_SIZE
+                                )
+                            } else {
+                                val lead =
+                                    state.allLeads.filter { lead -> lead.id == state.leadId }.get(0)
+                                DialogFormSectionDescription(
+                                    CountryEnum.getFlagByAlpha3(lead.nationality) + " " + lead.name + " " + lead.age,
+                                    DialogConstant.DESCRIPTION_FONT_SIZE
+                                )
+                                leadIcon = Icons.Default.SwapHoriz
+                            }
+                        }
+                        Column(
+                            modifier = Modifier.width(DialogConstant.ADD_LEAD_COLUMN_WIDTH)
+                        ) {
+                            IconButton(onClick = {
+                                onEvent(GameEvent.ShowLeadDialog(true, false))
+                            }) {
+                                Icon(
+                                    imageVector = leadIcon,
+                                    contentDescription = "Add lead",
+                                    tint = MaterialTheme.colorScheme.inversePrimary,
+                                    modifier = Modifier
+                                        .background(
+                                            MaterialTheme.colorScheme.secondaryContainer
+                                        )
+                                        .width(DialogConstant.ADD_LEAD_COLUMN_WIDTH)
+                                        .height(DialogConstant.ADD_LEAD_COLUMN_WIDTH)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                ) {
+                    timeInputButton(
+                        GenericDialog.getButtonTitle(
+                            state.date, "", "Date"
+                        ), setDialogState
+                    )
+                    timeInputButton(
+                        GenericDialog.getButtonTitle(state.startHour, "Start ", "Start Hour"),
+                        startHourDialogState
+                    )
+                    timeInputButton(
+                        GenericDialog.getButtonTitle(state.endHour, "End ", "End Hour"),
+                        endHourDialogState
+                    )
+                }
+                Spacer(modifier = Modifier.width(5.dp))
+                Column {
+                    Button(colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    ), onClick = { locationTextFieldExpanded = !locationTextFieldExpanded }) {
+                        // TODO: insert here a button that allows to choose from a list of sessions to which the set belongs to
+                        // TODO: add a switch that allow to automatically create a date from a set, and a selector in case the idate has
+                        // already been recorded on table
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceAround,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.PinDrop,
+                                contentDescription = state.dateType,
+                                tint = MaterialTheme.colorScheme.onPrimary,
+                                modifier = Modifier
+                                    .height(15.dp)
+                            )
+                            Text(
+                                text = "Location",
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                    TweetLinkImportButton(onEvent)
+                }
+            }
+            BasicAnimatedVisibility(
+                visibilityFlag = locationTextFieldExpanded,
+            ) {
+                Spacer(modifier = Modifier.height(7.dp))
+                OutlinedTextField(
+                    value = state.location,
+                    onValueChange = { onEvent(GameEvent.SetLocation(it)) },
+                    placeholder = { Text(text = "Location") },
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.height(80.dp)
+                )
+                Spacer(modifier = Modifier.height(7.dp))
+            }
+            BasicAnimatedVisibility(
+                visibilityFlag = !locationTextFieldExpanded,
+            ) {
+                Spacer(modifier = Modifier.height(7.dp))
+                OutlinedTextField(
+                    value = state.stickingPoints,
+                    onValueChange = { onEvent(GameEvent.SetStickingPoints(it)) },
+                    placeholder = { Text(text = "Sticking Points") },
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier.height(80.dp)
+                )
+                Spacer(modifier = Modifier.height(7.dp))
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                ToggleIcon(
+                    SetSortType.CONVERSATION.getField(),
+                    state.conversation,
+                    true,
+                    R.drawable.chat_b
+                ) {
+                    onEvent(GameEvent.SwitchConversation)
+                }
+                ToggleIcon(
+                    "contact",
+                    state.contact,
+                    true,
+                    R.drawable.contact_b
+                ) {
+                    onEvent(GameEvent.SwitchContact)
+                }
+                ToggleIcon(
+                    "instant\ndate",
+                    state.instant_date,
+                    true,
+                    R.drawable.idate_b
+                ) {
+                    onEvent(GameEvent.SwitchInstantDate)
+                }
+                ToggleIcon(
+                    "recorded",
+                    state.recorded,
+                    true,
+                    R.drawable.microphone_b
+                ) {
+                    onEvent(GameEvent.SwitchRecorded)
+                }
+            }
+        }
+    }, confirmButton = {
+        Box(
+            modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd
+        ) {
+            Row(
+                modifier = Modifier.width(250.dp), horizontalArrangement = Arrangement.End
+            ) {
+                Button(onClick = {
+                    onEvent(GameEvent.HideDialog)
+                }) {
+                    Text(text = "Cancel")
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = {
+                    if (state.leadId == 0L) {
+                        Toast.makeText(localContext, "Please choose a lead", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        onEvent(GameEvent.SaveDate)
+                        onEvent(GameEvent.HideDialog)
+                        onEvent(GameEvent.SwitchJustSaved)
+                        Toast.makeText(localContext, "Date saved", Toast.LENGTH_SHORT).show()
+                    }
+                }) {
+                    Text(text = "Save")
+                }
+            }
+        }
+    })
+}
+
+private fun setState(
+    state: InputState
+) {
+    if (state.editSet != null) {
+        state.date = state.editSet.date.substring(0, 10)
+        state.startHour = state.editSet.startHour.substring(11, 16)
+        state.endHour = state.editSet.endHour.substring(11, 16)
+        // TODO: solve following
+        // state.sessionId = state.editSet.sessionId!!
+        if (state.location.isBlank()) {
+            state.location = state.editSet.location!!
+        }
+        state.conversation = state.editSet.conversation
+        state.contact = state.editSet.contact
+        state.instant_date = state.editSet.instant_date
+        state.recorded = state.editSet.recorded
+        state.leadId = state.editSet.leadId!!
+        // TODO: solve following
+        // state.dateId = state.editSet.dateId!!
+        if (state.stickingPoints.isBlank()) {
+            state.stickingPoints = state.editSet.stickingPoints!!
+        }
+        state.tweetUrl = state.editSet.tweetUrl!!
+    }
+}
