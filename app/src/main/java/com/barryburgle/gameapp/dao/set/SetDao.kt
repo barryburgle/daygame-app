a7ec03bd5d9898a2f41d619d4ac80182cc8e3e4c
Barry Burgle <barryburgle@gmail.com>
2025-04-18 16:31:36 +0200

    set dao

---
+++ app/src/main/java/com/barryburgle/gameapp/dao/set/SetDao.kt
@@ -0,0 +1,24 @@
+package com.barryburgle.gameapp.dao.set
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import com.barryburgle.gameapp.model.set.SingleSet
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SetDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(singleSet: SingleSet): Long
+
+    @Delete
+    suspend fun delete(singleSet: SingleSet)
+
+    @Query("SELECT * from single_set ORDER BY id DESC, set_date DESC, start_hour DESC")
+    fun getAll(): Flow<List<SingleSet>>
+
+    // TODO: do other sorting and histogram methods
+}
