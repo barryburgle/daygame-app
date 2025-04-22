5d3054aed21d1ada450e0c9b03ca8c483fb9aaf5
Barry Burgle <barryburgle@gmail.com>
2025-04-22 21:58:45 +0200

    needed combine more methods

--- app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
@@ -4,22 +4,24 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, R> CombineSix(
+fun <T1, T2, T3, T4, T5, T6, T7, R> CombineSeven(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
     flow4: Flow<T4>,
     flow5: Flow<T5>,
     flow6: Flow<T6>,
-    transform: suspend (T1, T2, T3, T4, T5, T6) -> R
-): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6) { args: Array<*> ->
+    flow7: Flow<T7>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7) -> R
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6, flow7) { args: Array<*> ->
     transform(
         args[0] as T1,
         args[1] as T2,
         args[2] as T3,
         args[3] as T4,
         args[4] as T5,
-        args[5] as T6
+        args[5] as T6,
+        args[6] as T7
     )
 }
 
@@ -159,7 +161,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
 }
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, R> CombineTwentyOne(
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, R> CombineTwentyTwo(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
@@ -181,7 +183,8 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow19: Flow<T19>,
     flow20: Flow<T20>,
     flow21: Flow<T21>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21) -> R
+    flow22: Flow<T22>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22) -> R
 ): Flow<R> = combine(
     flow,
     flow2,
@@ -204,6 +207,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow19,
     flow20,
     flow21,
+    flow22,
 ) { args: Array<*> ->
     transform(
         args[0] as T1,
@@ -226,6 +230,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
         args[17] as T18,
         args[18] as T19,
         args[19] as T20,
-        args[20] as T21
+        args[20] as T21,
+        args[21] as T22
     )
 }
