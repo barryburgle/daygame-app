103f4abbedcd0ac35e37b1b59391681b84e4d4e8
Barry Burgle <barryburgle@gmail.com>
2025-05-08 00:13:14 +0200

    considering only the aggregated entities that belong to the last n periods

--- app/src/main/java/com/barryburgle/gameapp/dao/date/AggregatedDatesDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/date/AggregatedDatesDao.kt
@@ -2,15 +2,23 @@ package com.barryburgle.gameapp.dao.date
 
 import androidx.room.Dao
 import androidx.room.Query
+import com.barryburgle.gameapp.dao.setting.SettingDao
 import com.barryburgle.gameapp.model.stat.AggregatedDates
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AggregatedDatesDao {
 
-    @Query("SELECT COUNT(*) as dates, week_number as period_number, SUM(date_time)/60 as date_time_spent from meeting GROUP BY week_number")
+    companion object {
+        const val QUERY_DATES_BY_WEEKS =
+            "SELECT COUNT(*) as dates, week_number as period_number, SUM(date_time)/60 as date_time_spent from meeting WHERE period_number IN (${SettingDao.QUERY_LAST_WEEKS}) GROUP BY week_number"
+        const val QUERY_DATES_BY_MONTHS =
+            "SELECT COUNT(*) as dates, strftime('%m', meeting_date) as period_number, SUM(date_time)/60 as date_time_spent from meeting WHERE period_number IN (${SettingDao.QUERY_LAST_MONTHS}) GROUP BY strftime('%m', meeting_date)"
+    }
+
+    @Query(QUERY_DATES_BY_WEEKS)
     fun groupStatsByWeekNumber(): Flow<List<AggregatedDates>>
 
-    @Query("SELECT COUNT(*) as dates, strftime('%m', meeting_date) as period_number, SUM(date_time)/60 as date_time_spent from meeting GROUP BY strftime('%m', meeting_date)")
+    @Query(QUERY_DATES_BY_MONTHS)
     fun groupStatsByMonth(): Flow<List<AggregatedDates>>
 }
--- app/src/main/java/com/barryburgle/gameapp/dao/session/AggregatedSessionsDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/session/AggregatedSessionsDao.kt
@@ -9,9 +9,16 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface AggregatedSessionsDao {
 
-    @Query("SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, week_number as period_number, SUM(session_time)/60 as time_spent from abstract_session GROUP BY week_number")
+    companion object {
+        const val QUERY_SESSIONS_BY_WEEKS =
+            "SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, week_number as period_number, SUM(session_time)/60 as time_spent from abstract_session WHERE period_number IN (${SettingDao.QUERY_LAST_WEEKS}) GROUP BY week_number"
+        const val QUERY_SESSIONS_BY_MONTHS =
+            "SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, strftime('%m', session_date) as period_number, SUM(session_time)/60 as time_spent from abstract_session WHERE period_number IN (${SettingDao.QUERY_LAST_MONTHS}) GROUP BY strftime('%m', session_date)"
+    }
+
+    @Query(QUERY_SESSIONS_BY_WEEKS)
     fun groupStatsByWeekNumber(): Flow<List<AggregatedSessions>>
 
-    @Query("SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, strftime('%m', session_date) as period_number, SUM(session_time)/60 as time_spent from abstract_session GROUP BY strftime('%m', session_date)")
+    @Query(QUERY_SESSIONS_BY_MONTHS)
     fun groupStatsByMonth(): Flow<List<AggregatedSessions>>
 }
