fba7a9ea8067d67e48bcfe32b0dc3dc42ca50c3a
Barry Burgle <barryburgle@gmail.com>
2025-05-25 12:27:39 +0200

    passing state with all the entities to assert no entity is present

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/InsertInvite.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/InsertInvite.kt
@@ -1,37 +1,115 @@
 package com.barryburgle.gameapp.ui.utilities
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.GroupAdd
+import androidx.compose.material.icons.filled.PersonAddAlt1
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.barryburgle.gameapp.ui.state.AllEntityState
 
 @Composable
-fun InsertInvite(entities: List<*>, entitiesName: String, style: TextStyle) {
-    if (entities.isEmpty()) {
-        Column(
+fun InsertInvite(state: AllEntityState) {
+    if (state.allSessions.isEmpty() &&
+        state.allDates.isEmpty() &&
+        state.allSets.isEmpty() &&
+        state.allLeads.isEmpty()
+    ) {
+        Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.4f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "Insert new ${entitiesName}!",
-                    textAlign = TextAlign.Center,
-                    style = style
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Insert a new:",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+                entityDescription(
+                    "Session",
+                    imageVector = Icons.Default.GroupAdd
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                entityDescription(
+                    "Set",
+                    imageVector = Icons.Default.PersonAddAlt1
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                entityDescription(
+                    "Date",
+                    imageVector = Icons.Default.Favorite
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun entityDescription(
+    entityName: String,
+    imageVector: ImageVector?
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(15.dp)
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = entityName,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium
+            )
+            if (imageVector != null) {
+                Spacer(modifier = Modifier.width(12.dp))
+                Icon(
+                    imageVector = imageVector, contentDescription = "session"
                 )
             }
         }
