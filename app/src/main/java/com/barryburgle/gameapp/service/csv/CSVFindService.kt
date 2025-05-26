d0390670360f206c93bcbb3539b972dff288e8cf
Barry Burgle <barryburgle@gmail.com>
2025-05-26 10:29:03 +0200

    archive backups function

--- app/src/main/java/com/barryburgle/gameapp/service/csv/CSVFindService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/csv/CSVFindService.kt
@@ -2,9 +2,35 @@ package com.barryburgle.gameapp.service.csv
 
 import android.os.Environment
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 class CSVFindService {
+
+    companion object {
+        const val ARCHIVE_FOLDER: String = "archive"
+    }
+
     val localPath = Environment.getExternalStorageDirectory()
+    fun archiveBackups(exportFolder: String, backupFolder: String) {
+        val backupRelativePath = "/$exportFolder/$backupFolder"
+        var backupFiles: List<String> = findCsvFiles(backupRelativePath)
+        val backupDir = File(localPath, backupRelativePath)
+        val archiveDir = File(localPath, "/$backupRelativePath/$ARCHIVE_FOLDER")
+        if (!archiveDir.exists()) {
+            archiveDir.mkdirs()
+        }
+        for (file in backupFiles) {
+            val sourceFile = File(backupDir, file)
+            val destinationFile = File(archiveDir, file)
+            Files.move(
+                sourceFile.toPath(),
+                destinationFile.toPath(),
+                StandardCopyOption.REPLACE_EXISTING
+            )
+        }
+    }
+
     fun findCsvFiles(folder: String): List<String> {
         val dir = File(localPath, "/$folder")
         if (!dir.exists()) {
