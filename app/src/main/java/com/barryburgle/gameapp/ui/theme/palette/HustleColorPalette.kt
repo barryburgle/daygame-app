df4c506a6900b0baaeff7c6d601c1df01141f767
Barry Burgle <barryburgle@gmail.com>
2025-06-08 02:22:43 +0200

    color fix

--- app/src/main/java/com/barryburgle/gameapp/ui/theme/palette/HustleColorPalette.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/theme/palette/HustleColorPalette.kt
@@ -4,10 +4,10 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 import com.barryburgle.gameapp.ui.theme.Red30
 
-private val TappableColor =Color(0xFFBB3329)
-private val ContainerColor = Color(0xFFB00205)
+private val TappableColor =Color(0xFFD8574e)
+private val ContainerColor = Color(0xFF922820)
 private val InterestColor = Color(0xFF595959)
-private val BackgroundColor = Color(0xFFD9D9D9)
+private val BackgroundColor = Color(0xFFA7A7A7)
 private val SelectedColor = Color(0xFF020302)
 private val TextColor = Color(0xFFFFFEFE)
 
