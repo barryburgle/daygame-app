382bc89c24d9d77a81cf39cf0901b9d0f3b727ac
Barry Burgle <barryburgle@gmail.com>
2025-06-08 13:22:11 +0200

    tick circled

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/selection/MultiChoiceButton.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/selection/MultiChoiceButton.kt
@@ -1,10 +1,13 @@
 package com.barryburgle.gameapp.ui.utilities.selection
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +17,7 @@ import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -58,7 +62,19 @@ fun MultiChoiceButton(
                         onCheckedChange(index)
                     },
                     icon = {
-                        SegmentedButtonDefaults.Icon(selectedOptions[index])
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.size(20.dp)
+                        ) {
+                            val color = MaterialTheme.colorScheme.primaryContainer
+                            Canvas(modifier = Modifier.matchParentSize()) {
+                                drawCircle(
+                                    color = color,
+                                    radius = size.minDimension / 2f
+                                )
+                            }
+                            SegmentedButtonDefaults.Icon(selectedOptions[index])
+                        }
                     },
                     label = {
                         Text(label + " ($count)")
