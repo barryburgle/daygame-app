3973e712beb246e2deaba83c7d3cd69f2c92ba48
Barry Burgle <barryburgle@gmail.com>
2025-07-10 23:17:57 +0200

    dismiss button

---
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/dialog/DismissButton.kt
@@ -0,0 +1,26 @@
+package com.barryburgle.gameapp.ui.tool.dialog
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import com.barryburgle.gameapp.ui.utilities.button.ShadowButton
+
+@Composable
+fun DismissButton(
+    onClick: () -> Unit
+) {
+    ShadowButton(
+        onClick = onClick,
+        imageVector = Icons.Default.Add,
+        contentDescription = "Cancel",
+        color = MaterialTheme.colorScheme.primaryContainer,
+        iconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        boxModifier = Modifier
+            .rotate(-45f),
+        modifier = Modifier.scale(1.35f)
+    )
+}
