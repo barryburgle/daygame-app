f37fad39a58d753ec8a10b45eb8af392ec9bb186
Barry Burgle <barryburgle@gmail.com>
2025-07-10 13:01:00 +0200

    alignment

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/utils/RowTitle.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/utils/RowTitle.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.ui.utilities.text.title.SmallTitleText
@@ -27,6 +24,8 @@ fun RowTitle(leftTitle: String, rigthTitle: String, textFieldColumnWidth: Dp) {
             SmallTitleText(leftTitle)
         }
         Column(
+            modifier = Modifier.width(75.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             SmallTitleText(rigthTitle)
