b71d6c36120dd73c548fc275c206e42d5119b5d3
Barry Burgle <barryburgle@gmail.com>
2025-07-10 12:58:19 +0200

    shadow button for filename composable

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/utils/FilenameComposable.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/utils/FilenameComposable.kt
@@ -2,23 +2,17 @@ package com.barryburgle.gameapp.ui.tool.utils
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Replay
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +20,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.barryburgle.gameapp.model.enums.DataExchangeTypeEnum
+import com.barryburgle.gameapp.ui.utilities.button.ShadowButton
 import com.barryburgle.gameapp.ui.utilities.text.body.LittleBodyText
 
 @Composable
@@ -78,13 +73,7 @@ fun FilenameComposable(
                 Spacer(modifier = Modifier.width(0.dp))
                 if (icon != null) {
                     if (DataExchangeTypeEnum.IMPORT.type.equals(cardTitle, ignoreCase = true)) {
-                        IconButton(
-                            modifier = Modifier
-                                .background(
-                                    MaterialTheme.colorScheme.primaryContainer,
-                                    shape = RoundedCornerShape(40.dp)
-                                )
-                                .size(40.dp),
+                        ShadowButton(
                             onClick = {
                                 reloadFunction()
                                 Toast.makeText(
@@ -92,24 +81,13 @@ fun FilenameComposable(
                                     "Reloaded filename",
                                     Toast.LENGTH_SHORT
                                 ).show()
-                            }) {
-                            Icon(
-                                imageVector = Icons.Default.Replay,
-                                contentDescription = cardTitle,
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                                modifier = Modifier
-                                    .height(30.dp)
-                            )
-                        }
+                            },
+                            imageVector = Icons.Default.Replay,
+                            contentDescription = "Reload"
+                        )
                         Spacer(modifier = Modifier.width(0.dp))
                     }
-                    IconButton(
-                        modifier = Modifier
-                            .background(
-                                MaterialTheme.colorScheme.primaryContainer,
-                                shape = RoundedCornerShape(40.dp)
-                            )
-                            .size(40.dp),
+                    ShadowButton(
                         onClick = {
                             buttonFunction()
                             Toast.makeText(
@@ -117,15 +95,10 @@ fun FilenameComposable(
                                 "Successfully ${cardTitle.lowercase()}ed",
                                 Toast.LENGTH_SHORT
                             ).show()
-                        }) {
-                        Icon(
-                            imageVector = icon!!,
-                            contentDescription = cardTitle,
-                            tint = MaterialTheme.colorScheme.onPrimary,
-                            modifier = Modifier
-                                .height(30.dp)
-                        )
-                    }
+                        },
+                        imageVector = icon!!,
+                        contentDescription = "Filename Button"
+                    )
                 }
             }
         }
