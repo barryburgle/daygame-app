9dd608a07024dd8021c15c0ac0277b9203699bba
Barry Burgle <barryburgle@gmail.com>
2025-07-11 11:27:20 +0200

    passing not to cut image icons

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/button/ImageShadowButton.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/button/ImageShadowButton.kt
@@ -2,6 +2,8 @@ package com.barryburgle.gameapp.ui.utilities.button
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ImageShadowButton(
@@ -29,6 +32,7 @@ fun ImageShadowButton(
         color = color
     ) {
         Image(
+            modifier = Modifier.fillMaxWidth().padding(3.dp),
             painter = painterResource(icon),
             contentDescription = contentDescription,
             alignment = Alignment.Center,
