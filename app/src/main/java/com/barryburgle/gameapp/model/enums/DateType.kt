f6fa34034c04cda76708865be553d2fbcf39591a
Barry Burgle <barryburgle@gmail.com>
2025-07-12 17:20:28 +0200

    new method for date description

--- app/src/main/java/com/barryburgle/gameapp/model/enums/DateType.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/DateType.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.WineBar
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class DateType(private val type: String) {
+    // TODO: rename enum to DateTypeEnum
     DRINK("drink"),
     COFFEE("coffee"),
     WALK("walk"),
@@ -30,6 +31,37 @@ enum class DateType(private val type: String) {
                 EXPERIENCE.getType() -> Icons.Default.Panorama
                 else -> Icons.Default.Favorite
             }
+        }
+
+        fun getEmoji(type: String): String {
+            return when (type) {
+                DRINK.getType() -> "\uD83C\uDF78"
+                COFFEE.getType() -> "☕"
+                WALK.getType() -> "\uD83D\uDEB6\uD83D\uDEB6"
+                FOOD.getType() -> "\uD83C\uDF5C"
+                EXPERIENCE.getType() -> "\uD83C\uDFDE\uFE0F"
+                else -> "❤"
+            }
+        }
+
+        fun getDateNumber(dateNumber: Int, writeDate: Boolean): String {
+            // TODO: move to its own class in future if needed
+            var dateNumberSuffix = "th"
+            var dateNumberCount = dateNumber.toString()
+            if (dateNumber == 0) {
+                return "iDate"
+            } else if (dateNumber == 1) {
+                dateNumberSuffix = "st"
+            } else if (dateNumber == 2) {
+                dateNumberSuffix = "nd"
+            } else if (dateNumber == 3) {
+                dateNumberSuffix = "rd"
+            }
+            var dateDescription = dateNumberCount + dateNumberSuffix
+            if (writeDate) {
+                dateDescription = dateDescription + "date"
+            }
+            return dateDescription
         }
     }
 }
