1c967534a00e2ca894180258d72d7559e26937dd
Barry Burgle <barryburgle@gmail.com>
2025-07-12 17:17:18 +0200

    methods for event export

--- app/src/main/java/com/barryburgle/gameapp/model/game/EventModel.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/game/EventModel.kt
@@ -1,6 +1,8 @@
 package com.barryburgle.gameapp.model.game
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.barryburgle.gameapp.model.enums.CountryEnum
+import com.barryburgle.gameapp.model.lead.Lead
 
 interface EventModel {
 
@@ -13,4 +15,31 @@ interface EventModel {
     fun getEventDescription(): String
 
     fun getEventStickingPoints(): String?
+
+    fun shareReport(leads: List<Lead>): String
+
+    fun reportLeads(leads: List<Lead>): String {
+        var leadsReport = ""
+        if (!leads.isEmpty()) {
+            leadsReport = ":"
+            leads.forEach {
+                leadsReport =
+                    leadsReport + " " + CountryEnum.getFlagByAlpha3(it.nationality) + " " + it.age + "yo,"
+            }
+            leadsReport = leadsReport.dropLast(1)
+        }
+        return leadsReport
+    }
+
+    fun pluralMaker(quantity: Int): String {
+        return if (quantity > 1 || quantity == 0) "s" else ""
+    }
+
+    fun stickingPointsReport(stickingPoints: String?): String {
+        return if (stickingPoints != null && stickingPoints.isBlank()) "" else "\n\nSticking points:\n$stickingPoints"
+    }
+
+    fun checkmarkReport(flag: Boolean): String {
+        return if (flag) ": ✅" else ": ❌"
+    }
 }
