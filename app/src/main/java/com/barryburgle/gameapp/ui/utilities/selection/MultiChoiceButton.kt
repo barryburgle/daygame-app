a17278cd1dafaeff20d046ff80753bc65ce1429f
Barry Burgle <barryburgle@gmail.com>
2025-07-18 00:27:55 +0200

    removed circles

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/selection/MultiChoiceButton.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/selection/MultiChoiceButton.kt
@@ -1,13 +1,10 @@
 package com.barryburgle.gameapp.ui.utilities.selection
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -17,7 +14,6 @@ import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -61,23 +57,8 @@ fun MultiChoiceButton(
                         selectedOptionsToDisplay[index] = !selectedOptionsToDisplay[index]
                         onCheckedChange(index)
                     },
-                    icon = {
-                        Box(
-                            contentAlignment = Alignment.Center,
-                            modifier = Modifier.size(20.dp)
-                        ) {
-                            val color = MaterialTheme.colorScheme.primaryContainer
-                            Canvas(modifier = Modifier.matchParentSize()) {
-                                drawCircle(
-                                    color = color,
-                                    radius = size.minDimension / 2f
-                                )
-                            }
-                            SegmentedButtonDefaults.Icon(selectedOptions[index])
-                        }
-                    },
                     label = {
-                        Text(label + " ($count)")
+                        Text(label)
                     },
                     colors = SegmentedButtonDefaults.colors(
                         activeContentColor = MaterialTheme.colorScheme.onPrimary,
