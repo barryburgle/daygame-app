7348df15dc627da327098a60cfd7033fb8870b82
Barry Burgle <barryburgle@gmail.com>
2025-07-24 16:27:01 +0200

    combine eight to nine

--- app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
@@ -26,7 +26,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, R> CombineSeven(
 }
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, R> CombineEigth(
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, R> CombineNine(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
@@ -35,8 +35,9 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, R> CombineEigth(
     flow6: Flow<T6>,
     flow7: Flow<T7>,
     flow8: Flow<T8>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8) -> R
-): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6, flow7, flow8) { args: Array<*> ->
+    flow9: Flow<T9>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6, flow7, flow8, flow9) { args: Array<*> ->
     transform(
         args[0] as T1,
         args[1] as T2,
@@ -45,7 +46,8 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, R> CombineEigth(
         args[4] as T5,
         args[5] as T6,
         args[6] as T7,
-        args[7] as T8
+        args[7] as T8,
+        args[8] as T9
     )
 }
 
