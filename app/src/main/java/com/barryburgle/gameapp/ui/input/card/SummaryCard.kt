8d42e542b0aac77f46b090359d17d3f1485f6b38
Barry Burgle <barryburgle@gmail.com>
2025-10-17 00:24:05 +0200

    removed because unused

--- app/src/main/java/com/barryburgle/gameapp/ui/input/card/SummaryCard.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/card/SummaryCard.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ClipboardManager
