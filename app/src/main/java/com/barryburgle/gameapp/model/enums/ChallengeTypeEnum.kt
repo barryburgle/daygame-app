cf00dbe61009588136b2084ac61bead908450d1f
Barry Burgle <barryburgle@gmail.com>
2026-01-17 14:40:14 +0100

    types of challenges

---
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/ChallengeTypeEnum.kt
@@ -0,0 +1,51 @@
+package com.barryburgle.gameapp.model.enums
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContactPhone
+import androidx.compose.material.icons.filled.DirectionsRun
+import androidx.compose.material.icons.filled.EmojiEvents
+import androidx.compose.material.icons.filled.RecordVoiceOver
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class ChallengeTypeEnum(
+    private val type: String,
+    private val description: String,
+    private val icon: ImageVector
+) {
+    SET("set", "Sets", Icons.Default.DirectionsRun),
+    CONTACT("contact", "Contacts", Icons.Default.ContactPhone),
+    CONVERSATION("conversation", "Conversations", Icons.Default.RecordVoiceOver);
+    // TODO: add here goals on percs and indexes later. For a future task, the challenge type will determine how the challenge completion is computed from raw data (sessions, dates, sets)
+
+    fun getType(): String {
+        return type
+    }
+
+    fun getDescription(): String {
+        return description
+    }
+
+    fun getIcon(): ImageVector {
+        return icon
+    }
+
+    companion object {
+        fun getIcon(type: String): ImageVector {
+            return when (type) {
+                SET.getType() -> SET.icon
+                CONTACT.getType() -> CONTACT.icon
+                CONVERSATION.getType() -> CONVERSATION.icon
+                else -> Icons.Default.EmojiEvents
+            }
+        }
+
+        fun getDescription(type: String): String {
+            return when (type) {
+                SET.getType() -> SET.getDescription()
+                CONTACT.getType() -> CONTACT.getDescription()
+                CONVERSATION.getType() -> CONVERSATION.getDescription()
+                else -> "Type"
+            }
+        }
+    }
+}
