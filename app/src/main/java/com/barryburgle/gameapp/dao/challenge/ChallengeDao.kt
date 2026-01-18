a981df161ec5fb5a660ca966890688066f226943
Barry Burgle <barryburgle@gmail.com>
2026-01-18 17:04:39 +0100

    first basic challenge dao

---
+++ app/src/main/java/com/barryburgle/gameapp/dao/challenge/ChallengeDao.kt
@@ -0,0 +1,33 @@
+package com.barryburgle.gameapp.dao.challenge
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import com.barryburgle.gameapp.model.challenge.Challenge
+import com.barryburgle.gameapp.model.stat.CategoryHistogram
+import com.barryburgle.gameapp.model.stat.Histogram
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ChallengeDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(challenge: Challenge): Long
+
+    @Delete
+    suspend fun delete(challenge: Challenge)
+
+    @Query("DELETE FROM challenge")
+    suspend fun deleteAll()
+
+    @Query("SELECT * from challenge ORDER BY insert_time DESC")
+    fun getAll(): Flow<List<Challenge>>
+
+    @Query("SELECT challenge_type as category, COUNT(*) as frequency from challenge GROUP BY challenge_type ORDER BY challenge_type")
+    fun getTypeHistogram(): Flow<List<CategoryHistogram>>
+
+    @Query("SELECT CAST(JULIANDAY(end_date) - JULIANDAY(start_date) AS INTEGER) as metric, COUNT(*) as frequency from challenge GROUP BY metric ORDER BY metric")
+    fun getDurationHistogram(): Flow<List<Histogram>>
+}
