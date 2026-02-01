d64edf6c66b013571f7775a41745bb1b05a7a8f3
Barry Burgle <barryburgle@gmail.com>
2026-02-01 12:41:26 +0100

    removed unused functions and added new ones

--- app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/CombineMore.kt
@@ -4,7 +4,25 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, R> CombineSeven(
+fun <T1, T2, T3, T4, T5, R> CombineFive(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    transform: suspend (T1, T2, T3, T4, T5) -> R
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5) { args: Array<*> ->
+    transform(
+        args[0] as T1,
+        args[1] as T2,
+        args[2] as T3,
+        args[3] as T4,
+        args[4] as T5,
+    )
+}
+
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, T7, T8, R> CombineEight(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
@@ -12,8 +30,9 @@ fun <T1, T2, T3, T4, T5, T6, T7, R> CombineSeven(
     flow5: Flow<T5>,
     flow6: Flow<T6>,
     flow7: Flow<T7>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7) -> R
-): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6, flow7) { args: Array<*> ->
+    flow8: Flow<T8>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8) -> R
+): Flow<R> = combine(flow, flow2, flow3, flow4, flow5, flow6, flow7, flow8) { args: Array<*> ->
     transform(
         args[0] as T1,
         args[1] as T2,
@@ -21,9 +40,37 @@ fun <T1, T2, T3, T4, T5, T6, T7, R> CombineSeven(
         args[3] as T4,
         args[4] as T5,
         args[5] as T6,
-        args[6] as T7
+        args[6] as T7,
+        args[7] as T8,
     )
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, R> CombineNine(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    flow9: Flow<T9>,
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R
+): Flow<R> =
+    combine(flow, flow2, flow3, flow4, flow5, flow6, flow7, flow8, flow9) { args: Array<*> ->
+        transform(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5,
+            args[5] as T6,
+            args[6] as T7,
+            args[7] as T8,
+            args[8] as T9,
+        )
+    }
 
 @Suppress("UNCHECKED_CAST")
 fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> CombineTen(
@@ -61,96 +108,6 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> CombineTen(
         args[7] as T8,
         args[8] as T9,
         args[9] as T10,
-    )
-}
-
-@Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R> CombineEleven(
-    flow: Flow<T1>,
-    flow2: Flow<T2>,
-    flow3: Flow<T3>,
-    flow4: Flow<T4>,
-    flow5: Flow<T5>,
-    flow6: Flow<T6>,
-    flow7: Flow<T7>,
-    flow8: Flow<T8>,
-    flow9: Flow<T9>,
-    flow10: Flow<T10>,
-    flow11: Flow<T11>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) -> R
-): Flow<R> = combine(
-    flow,
-    flow2,
-    flow3,
-    flow4,
-    flow5,
-    flow6,
-    flow7,
-    flow8,
-    flow9,
-    flow10,
-    flow11
-) { args: Array<*> ->
-    transform(
-        args[0] as T1,
-        args[1] as T2,
-        args[2] as T3,
-        args[3] as T4,
-        args[4] as T5,
-        args[5] as T6,
-        args[6] as T7,
-        args[7] as T8,
-        args[8] as T9,
-        args[9] as T10,
-        args[10] as T11,
-    )
-}
-
-@Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, R> CombineThirteen(
-    flow: Flow<T1>,
-    flow2: Flow<T2>,
-    flow3: Flow<T3>,
-    flow4: Flow<T4>,
-    flow5: Flow<T5>,
-    flow6: Flow<T6>,
-    flow7: Flow<T7>,
-    flow8: Flow<T8>,
-    flow9: Flow<T9>,
-    flow10: Flow<T10>,
-    flow11: Flow<T11>,
-    flow12: Flow<T12>,
-    flow13: Flow<T13>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) -> R
-): Flow<R> = combine(
-    flow,
-    flow2,
-    flow3,
-    flow4,
-    flow5,
-    flow6,
-    flow7,
-    flow8,
-    flow9,
-    flow10,
-    flow11,
-    flow12,
-    flow13,
-) { args: Array<*> ->
-    transform(
-        args[0] as T1,
-        args[1] as T2,
-        args[2] as T3,
-        args[3] as T4,
-        args[4] as T5,
-        args[5] as T6,
-        args[6] as T7,
-        args[7] as T8,
-        args[8] as T9,
-        args[9] as T10,
-        args[10] as T11,
-        args[11] as T12,
-        args[12] as T13,
     )
 }
 
@@ -317,7 +274,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, R> C
 }
 
 @Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, R> CombineNineteen(
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, R> CombineEighteen(
     flow: Flow<T1>,
     flow2: Flow<T2>,
     flow3: Flow<T3>,
@@ -336,8 +293,7 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow16: Flow<T16>,
     flow17: Flow<T17>,
     flow18: Flow<T18>,
-    flow19: Flow<T19>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19) -> R
+    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18) -> R
 ): Flow<R> = combine(
     flow,
     flow2,
@@ -357,7 +313,6 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
     flow16,
     flow17,
     flow18,
-    flow19,
 ) { args: Array<*> ->
     transform(
         args[0] as T1,
@@ -377,82 +332,6 @@ fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17,
         args[14] as T15,
         args[15] as T16,
         args[16] as T17,
-        args[17] as T18,
-        args[18] as T19,
-    )
-}
-
-@Suppress("UNCHECKED_CAST")
-fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, R> CombineTwentyTwo(
-    flow: Flow<T1>,
-    flow2: Flow<T2>,
-    flow3: Flow<T3>,
-    flow4: Flow<T4>,
-    flow5: Flow<T5>,
-    flow6: Flow<T6>,
-    flow7: Flow<T7>,
-    flow8: Flow<T8>,
-    flow9: Flow<T9>,
-    flow10: Flow<T10>,
-    flow11: Flow<T11>,
-    flow12: Flow<T12>,
-    flow13: Flow<T13>,
-    flow14: Flow<T14>,
-    flow15: Flow<T15>,
-    flow16: Flow<T16>,
-    flow17: Flow<T17>,
-    flow18: Flow<T18>,
-    flow19: Flow<T19>,
-    flow20: Flow<T20>,
-    flow21: Flow<T21>,
-    flow22: Flow<T22>,
-    transform: suspend (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22) -> R
-): Flow<R> = combine(
-    flow,
-    flow2,
-    flow3,
-    flow4,
-    flow5,
-    flow6,
-    flow7,
-    flow8,
-    flow9,
-    flow10,
-    flow11,
-    flow12,
-    flow13,
-    flow14,
-    flow15,
-    flow16,
-    flow17,
-    flow18,
-    flow19,
-    flow20,
-    flow21,
-    flow22,
-) { args: Array<*> ->
-    transform(
-        args[0] as T1,
-        args[1] as T2,
-        args[2] as T3,
-        args[3] as T4,
-        args[4] as T5,
-        args[5] as T6,
-        args[6] as T7,
-        args[7] as T8,
-        args[8] as T9,
-        args[9] as T10,
-        args[10] as T11,
-        args[11] as T12,
-        args[12] as T13,
-        args[13] as T14,
-        args[14] as T15,
-        args[15] as T16,
-        args[16] as T17,
-        args[17] as T18,
-        args[18] as T19,
-        args[19] as T20,
-        args[20] as T21,
-        args[21] as T22,
+        args[17] as T18
     )
 }
