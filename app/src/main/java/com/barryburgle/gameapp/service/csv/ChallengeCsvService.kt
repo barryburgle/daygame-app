92849db6d237b890a0f5f532ef4a2e62a019e808
Barry Burgle <barryburgle@gmail.com>
2026-02-07 13:56:14 +0100

    fully working achieved challenges csv service

--- app/src/main/java/com/barryburgle/gameapp/service/csv/ChallengeCsvService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/csv/ChallengeCsvService.kt
@@ -1,8 +1,9 @@
 package com.barryburgle.gameapp.service.csv
 
+import com.barryburgle.gameapp.model.challenge.AchievedChallenge
 import com.barryburgle.gameapp.model.challenge.Challenge
 
-class ChallengeCsvService : AbstractCsvService<Challenge>() {
+class ChallengeCsvService : AbstractCsvService<AchievedChallenge>() {
 
     companion object {
         private const val CHALLENGES_BACKUP_FILENAME: String = "challenges_backup"
@@ -12,18 +13,19 @@ class ChallengeCsvService : AbstractCsvService<Challenge>() {
         return CHALLENGES_BACKUP_FILENAME
     }
 
-    override fun exportSingleRow(challenge: Challenge): Array<String> {
+    override fun exportSingleRow(achievedChallenge: AchievedChallenge): Array<String> {
         val challengeFieldList = mutableListOf<String>()
-        challengeFieldList.add(challenge.id.toString())
-        challengeFieldList.add(challenge.insertTime)
-        challengeFieldList.add(cleaEmptyField(challenge.name))
-        challengeFieldList.add(cleaEmptyField(challenge.description))
-        challengeFieldList.add(challenge.startDate)
-        challengeFieldList.add(challenge.endDate)
-        challengeFieldList.add(challenge.type)
-        challengeFieldList.add(challenge.goal.toString())
-        challengeFieldList.add(cleaEmptyField(challenge.tweetUrl))
-        challengeFieldList.add(challenge.dayOfWeek.toString())
+        challengeFieldList.add(achievedChallenge.challenge.id.toString())
+        challengeFieldList.add(achievedChallenge.challenge.insertTime)
+        challengeFieldList.add(cleaEmptyField(achievedChallenge.challenge.name))
+        challengeFieldList.add(cleaEmptyField(achievedChallenge.challenge.description))
+        challengeFieldList.add(achievedChallenge.challenge.startDate)
+        challengeFieldList.add(achievedChallenge.challenge.endDate)
+        challengeFieldList.add(achievedChallenge.challenge.type)
+        challengeFieldList.add(achievedChallenge.challenge.goal.toString())
+        challengeFieldList.add(achievedChallenge.achieved.toString())
+        challengeFieldList.add(cleaEmptyField(achievedChallenge.challenge.tweetUrl))
+        challengeFieldList.add(achievedChallenge.challenge.dayOfWeek.toString())
         return challengeFieldList.toTypedArray()
     }
 
@@ -38,29 +40,32 @@ class ChallengeCsvService : AbstractCsvService<Challenge>() {
         leadFieldList.add("end_date")
         leadFieldList.add("challenge_type")
         leadFieldList.add("goal")
+        leadFieldList.add("achieved")
         leadFieldList.add("tweet_url")
         leadFieldList.add("day_of_week")
         return leadFieldList.toTypedArray()
     }
 
-    override fun mapImportRow(fields: Array<String>): Challenge {
-        return Challenge(
-            importLong(fields[0]),
-            fields[1],
-            fields[2],
-            fields[3],
-            fields[4],
-            fields[5],
-            fields[6],
-            importLong(fields[7])!!.toInt(),
-            fields[8],
-            importLong(fields[9])!!.toInt(),
+    override fun mapImportRow(fields: Array<String>): AchievedChallenge {
+        return AchievedChallenge(
+            Challenge(
+                importLong(fields[0]),
+                fields[1],
+                fields[2],
+                fields[3],
+                fields[4],
+                fields[5],
+                fields[6],
+                importLong(fields[7])!!.toInt(),
+                fields[9],
+                importLong(fields[10])!!.toInt()
+            )
         )
     }
 
-    override fun isEntityValid(challenge: Challenge): Boolean {
+    override fun isEntityValid(achievedChallenge: AchievedChallenge): Boolean {
         // TODO: do better check on data validity on most of the fields
-        if (challenge.id == 0L || challenge.id == null || challenge.insertTime.isEmpty()) {
+        if (achievedChallenge.challenge.id == 0L || achievedChallenge.challenge.id == null || achievedChallenge.challenge.insertTime.isEmpty()) {
             return false
         }
         return true
