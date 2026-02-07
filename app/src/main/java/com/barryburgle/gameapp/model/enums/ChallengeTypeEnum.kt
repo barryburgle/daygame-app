b3191e6a718179858a8e15784fca3410bd1bb1dd
Barry Burgle <barryburgle@gmail.com>
2026-02-07 21:47:41 +0100

    integrations needed for ChallengeBody values and description display

--- app/src/main/java/com/barryburgle/gameapp/model/enums/ChallengeTypeEnum.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/ChallengeTypeEnum.kt
@@ -10,11 +10,12 @@ import androidx.compose.ui.graphics.vector.ImageVector
 enum class ChallengeTypeEnum(
     private val type: String,
     private val description: String,
+    private val successVerb: String,
     private val icon: ImageVector
 ) {
-    SET("set", "Sets", Icons.Default.DirectionsRun),
-    CONVERSATION("conversation", "Conversations", Icons.Default.RecordVoiceOver),
-    CONTACT("contact", "Contacts", Icons.Default.ContactPhone);
+    SET("set", "Sets", "done", Icons.Default.DirectionsRun),
+    CONVERSATION("conversation", "Conversations", "had", Icons.Default.RecordVoiceOver),
+    CONTACT("contact", "Contacts", "taken", Icons.Default.ContactPhone);
     // TODO: add here goals on percs and indexes later. For a future task, the challenge type will determine how the challenge completion is computed from raw data (sessions, dates, sets)
 
     fun getType(): String {
@@ -25,11 +26,24 @@ enum class ChallengeTypeEnum(
         return description
     }
 
+    fun getSuccessVerb(): String {
+        return successVerb
+    }
+
     fun getIcon(): ImageVector {
         return icon
     }
 
     companion object {
+        fun isTypeAchievedInteger(type: String): Boolean {
+            return when (type) {
+                SET.getType() -> true
+                CONTACT.getType() -> true
+                CONVERSATION.getType() -> true
+                else -> false
+            }
+        }
+
         fun getIcon(type: String): ImageVector {
             return when (type) {
                 SET.getType() -> SET.icon
@@ -45,6 +59,15 @@ enum class ChallengeTypeEnum(
                 CONTACT.getType() -> CONTACT.getDescription()
                 CONVERSATION.getType() -> CONVERSATION.getDescription()
                 else -> "Type"
+            }
+        }
+
+        fun getValue(type: String): ChallengeTypeEnum {
+            return when (type) {
+                SET.getType() -> SET
+                CONTACT.getType() -> CONTACT
+                CONVERSATION.getType() -> CONVERSATION
+                else -> SET
             }
         }
     }
