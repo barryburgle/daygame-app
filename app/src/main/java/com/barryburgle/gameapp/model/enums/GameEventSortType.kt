8190ef6312eb3745b7126d988154270fbe6b5e8d
Barry Burgle <barryburgle@gmail.com>
2026-02-07 22:40:05 +0100

    removed because challenges do not allow ordering by those properties

--- app/src/main/java/com/barryburgle/gameapp/model/enums/GameEventSortType.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/GameEventSortType.kt
@@ -2,8 +2,6 @@ package com.barryburgle.gameapp.model.enums
 
 enum class GameEventSortType(private val field: String) : FieldEnum {
     DATE("Date"),
-    START_HOUR("Hour"),
-    TIME_SPENT("Duration"),
     DAY_OF_WEEK("Day of Week");
 
     override fun getField(): String {
