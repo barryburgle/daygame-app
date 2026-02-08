8d30e414175691e78028dc037a8b84830d8e24d0
Barry Burgle <barryburgle@gmail.com>
2026-02-08 23:44:42 +0100

    centralized completion ratio computation and achieved challenge report

--- app/src/main/java/com/barryburgle/gameapp/model/challenge/AchievedChallenge.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/challenge/AchievedChallenge.kt
@@ -2,6 +2,7 @@ package com.barryburgle.gameapp.model.challenge
 
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.room.Embedded
+import com.barryburgle.gameapp.model.enums.ChallengeTypeEnum
 import com.barryburgle.gameapp.model.game.EventModel
 import com.barryburgle.gameapp.model.lead.Lead
 
@@ -36,6 +37,41 @@ open class AchievedChallenge(
 
     override fun shareReport(leads: List<Lead>): String {
         return challenge.shareReport(leads)
+    }
+
+    fun getCompletionPerc(): Double {
+        return achieved / challenge.goal
+    }
+
+    fun getAchievedChallengeReport(isCurrentChallengeSummary: Boolean): String {
+        var completePercentage = getCompletionPerc()
+        var reportPrefix = if (isCurrentChallengeSummary) "• Current " else "\uD83C\uDFC6 New "
+        var report = reportPrefix + shareReport(emptyList())
+        var achievedToPrint = achieved.toString()
+        if (ChallengeTypeEnum.isTypeAchievedInteger(
+                challenge.type
+            )
+        ) {
+            achievedToPrint = achieved.toInt().toString()
+        }
+        val achievedPrefix =
+            "\n\nAchieved: ${achievedToPrint} ${challenge.type}s\n"
+        if (completePercentage >= 1) {
+            report += achievedPrefix + "████████████████████ 100%"
+        } else {
+            completePercentage *= 20
+            val intCompletePercentage = completePercentage.toInt()
+            var completionBar = ""
+            for (i in 1..intCompletePercentage) {
+                completionBar += "█"
+            }
+            for (i in 1..(20 - intCompletePercentage)) {
+                completionBar += "░"
+            }
+            completionBar += " ${intCompletePercentage * 5}%"
+            report += achievedPrefix + completionBar
+        }
+        return report
     }
 
     constructor() : this(
