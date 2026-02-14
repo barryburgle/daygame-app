554efe77322e97bdc02c51bc96d4bfe332b087ff
Barry Burgle <barryburgle@gmail.com>
2026-02-14 23:28:40 +0100

    challenge medal service

---
+++ app/src/main/java/com/barryburgle/gameapp/service/challenge/ChallengeMedalService.kt
@@ -0,0 +1,22 @@
+package com.barryburgle.gameapp.service.challenge
+
+import com.barryburgle.gameapp.model.challenge.AchievedChallenge
+import com.barryburgle.gameapp.model.enums.ChallengeMedalEnum
+import com.barryburgle.gameapp.service.FormatService
+import java.time.LocalDate
+
+class ChallengeMedalService {
+
+    companion object {
+        fun getMedal(achievedChallenge: AchievedChallenge): ChallengeMedalEnum {
+            val completionPerc = achievedChallenge.getCompletionPerc()
+            return when {
+                completionPerc >= 1 -> ChallengeMedalEnum.GOLD
+                completionPerc < 1 && completionPerc >= 0.9 -> ChallengeMedalEnum.SILVER
+                completionPerc < 0.9 && completionPerc >= 0.7 -> ChallengeMedalEnum.BRONZE
+                completionPerc < 0.7 && LocalDate.now() <= FormatService.parseDate(achievedChallenge.challenge.endDate) -> ChallengeMedalEnum.ONGOING
+                else -> ChallengeMedalEnum.NONE
+            }
+        }
+    }
+}
