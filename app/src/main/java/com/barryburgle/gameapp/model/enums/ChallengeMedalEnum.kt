10a3b149ef06089d9587aa7c9d2197dd1ab9770f
Barry Burgle <barryburgle@gmail.com>
2026-02-19 23:45:00 +0100

    colors directly in the enum

--- app/src/main/java/com/barryburgle/gameapp/model/enums/ChallengeMedalEnum.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/enums/ChallengeMedalEnum.kt
@@ -3,20 +3,15 @@ package com.barryburgle.gameapp.model.enums
 import androidx.compose.ui.graphics.Color
 import com.barryburgle.gameapp.R
 
-val Gold = Color(0xFFFFD700)
-val Silver = Color(0xFFA9A9A9)
-val Bronze = Color(0xFFCD7F32)
-val Steel = Color(0xFF757575)
-
 enum class ChallengeMedalEnum(
     private val description: String,
     private val icon: Int,
     private val color: Color
 ) {
-    GOLD("Gold\nMedal", R.drawable.gold_medal, Gold),
-    SILVER("Silver\nMedal", R.drawable.silver_medal, Silver),
-    BRONZE("Bronze\nMedal", R.drawable.bronze_medal, Bronze),
-    NONE("Completed", R.drawable.completed_medal, Steel),
+    GOLD("Gold\nMedal", R.drawable.gold_medal, Color(0xFFFFD700)),
+    SILVER("Silver\nMedal", R.drawable.silver_medal, Color(0xFFA9A9A9)),
+    BRONZE("Bronze\nMedal", R.drawable.bronze_medal, Color(0xFFCD7F32)),
+    NONE("Completed", R.drawable.completed_medal, Color(0xFF757575)),
     ONGOING("Ongoing", R.drawable.ongoing, Color.Transparent);
 
 
