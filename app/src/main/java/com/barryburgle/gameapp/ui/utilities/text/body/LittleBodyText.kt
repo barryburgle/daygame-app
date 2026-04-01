8883820c6869489420f297700b60461dc238b31d
Barry Burgle <barryburgle@gmail.com>
2026-04-01 22:59:40 +0200

    optional new color and modifier args

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/text/body/LittleBodyText.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/text/body/LittleBodyText.kt
@@ -3,12 +3,19 @@ package com.barryburgle.gameapp.ui.utilities.text.body
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun LittleBodyText(text: String) {
+fun LittleBodyText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onPrimary
+) {
     Text(
         text = text,
         style = MaterialTheme.typography.bodySmall,
-        color = MaterialTheme.colorScheme.onPrimary
+        color = color,
+        modifier = modifier
     )
 }
