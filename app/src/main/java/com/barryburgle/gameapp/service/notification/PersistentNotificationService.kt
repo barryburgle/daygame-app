a65d0b0f45dec54a8d3dfcbc2130f85a82d55a46
Barry Burgle <barryburgle@gmail.com>
2026-04-29 23:24:44 +0200

    persistent notification service

---
+++ app/src/main/java/com/barryburgle/gameapp/service/notification/PersistentNotificationService.kt
@@ -0,0 +1,51 @@
+package com.barryburgle.gameapp.service.notification
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.barryburgle.gameapp.R
+
+class PersistentNotificationService : Service() {
+
+    companion object {
+        const val ACTION_STOP = "com.barryburgle.gameapp.STOP_SESSION"
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val eventName = intent?.getStringExtra("EVENT_NAME") ?: "Active Event"
+        val stopIntent = Intent(this, PersistentNotificationService::class.java).apply {
+            action = ACTION_STOP
+        }
+        val stopPendingIntent = PendingIntent.getService(
+            this,
+            0,
+            stopIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val notification = NotificationCompat.Builder(
+            this,
+            NotificationService.LIVE_SESSION_NOTIFICATION_CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.notification)
+            .setContentTitle("Live session")
+            .setContentText("Tap to open or use buttons below")
+            .setOngoing(true)
+            .addAction(R.drawable.badge, "Stop Session", stopPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(100, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
+        } else {
+            startForeground(100, notification)
+        }
+
+        return START_STICKY
+    }
+}
