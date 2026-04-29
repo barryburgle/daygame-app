77b5cac90817b8627c3d505834df68bd33f838bb
Barry Burgle <barryburgle@gmail.com>
2026-04-30 01:47:38 +0200

    persistent notification logic draft

--- app/src/main/java/com/barryburgle/gameapp/service/notification/PersistentNotificationService.kt
+++ app/src/main/java/com/barryburgle/gameapp/service/notification/PersistentNotificationService.kt
@@ -8,24 +8,120 @@ import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.barryburgle.gameapp.R
+import com.barryburgle.gameapp.dao.session.AbstractSessionDao
+import com.barryburgle.gameapp.database.GameAppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class PersistentNotificationService : Service() {
 
     companion object {
-        const val ACTION_STOP = "com.barryburgle.gameapp.STOP_SESSION"
+        const val LIVE_SESSIONS_START_HOUR = "LIVE SESSIONS START HOUR"
+        const val ACTION_NEW_SET = "ACTION_NEW_SET"
+        const val ACTION_NEW_CONVERSATION = "ACTION_NEW_CONVERSATION"
+        const val ACTION_NEW_CONTACT = "ACTION_NEW_CONTACT"
+    }
+
+    private fun handleNewSetAction(intent: Intent?, abstractSessionDao: AbstractSessionDao): Int {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val lastSession = abstractSessionDao.getLastSession().first()
+                lastSession.let { session ->
+                    val updatedSets = session.sets + 1
+                    val updatedSession = session.copy(sets = updatedSets)
+                    abstractSessionDao.insert(updatedSession)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return updateNotification(intent)
+    }
+
+    private fun handleNewConversationAction(
+        intent: Intent?,
+        abstractSessionDao: AbstractSessionDao
+    ): Int {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val lastSession = abstractSessionDao.getLastSession().first()
+                lastSession.let { session ->
+                    val updatedConversations = session.conversations + 1
+                    val updatedSession = session.copy(conversations = updatedConversations)
+                    abstractSessionDao.insert(updatedSession)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return updateNotification(intent)
+    }
+
+    private fun handleNewContactAction(
+        intent: Intent?,
+        abstractSessionDao: AbstractSessionDao
+    ): Int {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val lastSession = abstractSessionDao.getLastSession().first()
+                lastSession.let { session ->
+                    val updatedContacts = session.contacts + 1
+                    val updatedSession = session.copy(contacts = updatedContacts)
+                    abstractSessionDao.insert(updatedSession)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return updateNotification(intent)
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val eventName = intent?.getStringExtra("EVENT_NAME") ?: "Active Event"
-        val stopIntent = Intent(this, PersistentNotificationService::class.java).apply {
-            action = ACTION_STOP
+        val database = GameAppDatabase.getInstance(applicationContext)
+        val abstractSessionDao = database!!.abstractSessionDao
+        when (intent?.action) {
+            ACTION_NEW_SET -> {
+                return handleNewSetAction(intent, abstractSessionDao)
+            }
+
+            ACTION_NEW_CONVERSATION -> {
+                return START_STICKY
+            }
+
+            ACTION_NEW_CONTACT -> {
+                return START_STICKY
+            }
         }
-        val stopPendingIntent = PendingIntent.getService(
+        return updateNotification(intent)
+    }
+
+    fun updateNotification(intent: Intent?): Int {
+        val parsedStartHour = "Started at " + intent?.getStringExtra(LIVE_SESSIONS_START_HOUR)
+        val newSetPendingIntent = PendingIntent.getService(
             this,
             0,
-            stopIntent,
+            Intent(this, PersistentNotificationService::class.java).apply {
+                action = ACTION_NEW_SET
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val newConversationPendingIntent = PendingIntent.getService(
+            this,
+            1,
+            Intent(this, PersistentNotificationService::class.java).apply {
+                action = ACTION_NEW_CONVERSATION
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val newContactPendingIntent = PendingIntent.getService(
+            this,
+            2,
+            Intent(this, PersistentNotificationService::class.java).apply {
+                action = ACTION_NEW_CONTACT
+            },
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
         val notification = NotificationCompat.Builder(
@@ -34,11 +130,11 @@ class PersistentNotificationService : Service() {
         )
             .setSmallIcon(R.drawable.notification)
             .setContentTitle("Live session")
-            .setContentText("[Start time here + recap] + tap to add lead")
+            .setContentText("${parsedStartHour} [ + recap + tap to add lead]")
             .setOngoing(true)
-            .addAction(0, "New set", stopPendingIntent)
-            .addAction(0, "New conversation", stopPendingIntent)
-            .addAction(0, "New contact", stopPendingIntent)
+            .addAction(0, "New set", newSetPendingIntent)
+            .addAction(0, "New conversation", newConversationPendingIntent)
+            .addAction(0, "New contact", newContactPendingIntent)
             .setPriority(NotificationCompat.PRIORITY_LOW)
             .build()
 
