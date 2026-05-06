5799d93f00e8ae90fb6a878223b0957186cfe28b
Barry Burgle <barryburgle@gmail.com>
2026-05-06 18:42:47 +0200

    simpler receiver with title and content

--- app/src/main/java/com/barryburgle/gameapp/notification/NotificationReceiver.kt
+++ app/src/main/java/com/barryburgle/gameapp/notification/NotificationReceiver.kt
@@ -3,23 +3,19 @@ package com.barryburgle.gameapp.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.barryburgle.gameapp.service.FormatService
 import com.barryburgle.gameapp.service.notification.NotificationService
 
 class NotificationReceiver : BroadcastReceiver() {
 
-    // TODO: integrate daggerhilt for injection of dependecies in this class
-
     override fun onReceive(context: Context?, intent: Intent?) {
-        val lastSessionStickingPoints =
-            intent?.getStringExtra(AndroidNotificationScheduler.STICKING_POINTS_ID) ?: return
-        val lastSessionDate = FormatService.getDate(
-            intent?.getStringExtra(AndroidNotificationScheduler.DATE_ID) ?: return
-        )
         val notificationService = NotificationService(context)
+        val notificationTitle =
+            intent?.getStringExtra(AndroidNotificationScheduler.NOTIFICATION_TITLE) ?: return
+        val notificationContent =
+            intent?.getStringExtra(AndroidNotificationScheduler.NOTIFICATION_CONTENT) ?: return
         notificationService.showNotification(
-            lastSessionDate,
-            lastSessionStickingPoints
+            notificationTitle,
+            notificationContent
         )
     }
 }
