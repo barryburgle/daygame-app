a5c175c18ade3f40022ff0ac700bc9896c884ca0
Barry Burgle <barryburgle@gmail.com>
2026-06-23 09:42:08 +0200

    second version

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/timeline/Timeline.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/timeline/Timeline.kt
@@ -1,15 +1,19 @@
 package com.barryburgle.gameapp.ui.utilities.timeline
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -33,23 +37,26 @@ fun Timeline(
         rememberVectorPainter(ImageVector.vectorResource(id = R.drawable.conversation_action))
     val contactPainter =
         rememberVectorPainter(ImageVector.vectorResource(id = R.drawable.contact_action))
+    val primaryContainerColor = MaterialTheme.colorScheme.primaryContainer
     val startTime = FormatService.parseTime(abstractSession.startHour)
     val sessionDuration = abstractSession.sessionTime
-
-    Box(
+    Spacer(modifier = Modifier.height(10.dp))
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
-            .height(80.dp)
+            .height(20.dp)
             .padding(horizontal = 16.dp)
     ) {
         Canvas(modifier = Modifier.fillMaxWidth()) {
             val width = size.width
             val midY = size.height / 2
             drawLine(
-                color = Color.Gray,
+                color = primaryContainerColor,
                 start = Offset(0f, midY),
                 end = Offset(width, midY),
-                strokeWidth = 10.dp.toPx()
+                strokeWidth = 10.dp.toPx(),
+                cap = StrokeCap.Round
             )
             pinPoints.forEach { pin ->
                 val pinTime = FormatService.parseTime(pin.localTimestamp.substring(0, 16) + 'Z')
@@ -66,19 +73,21 @@ fun Timeline(
                     }
                     painter?.let {
                         val iconSize = 24.dp.toPx()
+                        val circleRadius =
+                            16.dp.toPx()
+                        drawCircle(
+                            color = Color.White,
+                            radius = circleRadius,
+                            center = Offset(xPos, midY)
+                        )
                         translate(
                             left = xPos - (iconSize / 2),
-                            top = midY - iconSize - 4.dp.toPx()
+                            top = midY - (iconSize / 2)
                         ) {
                             with(it) {
                                 draw(size = Size(iconSize, iconSize))
                             }
                         }
-                        drawCircle(
-                            color = Color.White,
-                            radius = 3.dp.toPx(),
-                            center = Offset(xPos, midY)
-                        )
                     }
                 }
             }
