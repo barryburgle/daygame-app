195b6ee9c1ca9933bf0614ed106cfd72875caa93
Barry Burgle <barryburgle@gmail.com>
2026-07-31 10:41:06 +0200

    longer card

--- app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolsScreen.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/tool/ToolsScreen.kt
@@ -63,7 +63,7 @@ fun ToolsScreen(
         },
     ) { padding ->
         val dataExchangeCardModifier = Modifier
-            .height(870.dp)
+            .height(880.dp)
             .width(LocalConfiguration.current.screenWidthDp.dp - spaceFromLeft * 2)
             .shadow(
                 elevation = 5.dp, shape = MaterialTheme.shapes.large
