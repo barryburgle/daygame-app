7cb0d91bf97d8a4fc97ba2f8c621caa2d7299c47
Barry Burgle <barryburgle@gmail.com>
2026-08-09 12:46:08 +0200

    pinpoint time scatter card v1

---
+++ app/src/main/java/com/barryburgle/gameapp/ui/stats/PinPointScatterCard.kt
@@ -0,0 +1,122 @@
+package com.barryburgle.gameapp.ui.stats
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.barryburgle.gameapp.model.session.PinPoint
+import com.barryburgle.gameapp.ui.stats.chart.OutputScatterChart
+import com.barryburgle.gameapp.ui.utilities.button.IconShadowButton
+import com.barryburgle.gameapp.ui.utilities.text.body.LittleBodyText
+import com.barryburgle.gameapp.ui.utilities.text.title.LargeTitleText
+
+@ExperimentalMaterial3Api
+@Composable
+fun OutputScatterCard(
+    modifier: Modifier,
+    title: String,
+    statCardIcon: ImageVector,
+    description: String,
+    allPinPoints: List<PinPoint>
+) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        MaterialTheme.colorScheme.primaryContainer,
+                        MaterialTheme.colorScheme.secondaryContainer
+                    ),
+                )
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Icon(
+                                imageVector = statCardIcon,
+                                contentDescription = title,
+                                tint = MaterialTheme.colorScheme.onPrimary,
+                                modifier = Modifier.height(25.dp)
+                            )
+                            Spacer(modifier = Modifier.width(7.dp))
+                            LargeTitleText(title)
+                        }
+                        Row(
+                            modifier = Modifier
+                                .width(60.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            IconShadowButton(
+                                onClick = {
+                                    // TODO: substitute with dropdown checkbox pinpoint-type menu
+                                },
+                                imageVector = Icons.Default.Share,
+                                contentDescription = "Share Stats"
+                            )
+                        }
+                    }
+                    LittleBodyText(description)
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                    ) {
+                        OutputScatterChart(
+                            pinPoints = allPinPoints
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
