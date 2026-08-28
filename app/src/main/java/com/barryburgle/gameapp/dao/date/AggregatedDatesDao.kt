b6571c52360a4381fd8dee94dd039cb41290c26f
Barry Burgle <barryburgle@gmail.com>
2026-08-29 01:36:59 +0200

    weekly labels

--- app/src/main/java/com/barryburgle/gameapp/dao/date/AggregatedDatesDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/date/AggregatedDatesDao.kt
@@ -11,7 +11,7 @@ interface AggregatedDatesDao {
 
     companion object {
         const val QUERY_DATES_BY_WEEKS =
-            "SELECT COUNT(*) as dates, CAST(strftime('%Y', meeting_date) as INTEGER) as year_number, CAST(week_number as INTEGER) as period_number, SUM(date_time)/60 as date_time_spent from meeting GROUP BY strftime('%Y', meeting_date), week_number"
+            "SELECT COUNT(*) as dates, CAST(strftime('%Y', meeting_date) as INTEGER) as year_number, CAST(week_number as INTEGER) as period_number, '[' || strftime('%d', date(meeting_date, '-6 days', 'weekday 0')) || '-' || strftime('%d', date(meeting_date, '-6 days', 'weekday 0', '+6 days')) || ']\n' || strftime('%m', date(meeting_date, '-6 days', 'weekday 0')) || '-' || substr(strftime('%Y', date(meeting_date, '-6 days', 'weekday 0')), 3, 2) as label, SUM(date_time)/60 as date_time_spent from meeting GROUP BY strftime('%Y', meeting_date), week_number"
         const val QUERY_DATES_BY_MONTHS =
             "SELECT COUNT(*) as dates, CAST(strftime('%Y', meeting_date) as INTEGER) as year_number, CAST(strftime('%m', meeting_date) as INTEGER) as period_number, CAST(strftime('%Y', meeting_date) as INTEGER) || '-' || CAST(strftime('%m', meeting_date) as INTEGER) as label, SUM(date_time)/60 as date_time_spent from meeting GROUP BY strftime('%Y', meeting_date), strftime('%m', meeting_date)"
     }
--- app/src/main/java/com/barryburgle/gameapp/dao/session/AggregatedSessionsDao.kt
+++ app/src/main/java/com/barryburgle/gameapp/dao/session/AggregatedSessionsDao.kt
@@ -11,8 +11,8 @@ interface AggregatedSessionsDao {
 
     companion object {
         const val QUERY_SESSIONS_BY_WEEKS =
-            "SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, CAST(strftime('%Y', session_date) as INTEGER) as year_number, CAST(week_number as INTEGER) as period_number, SUM(session_time)/60 as time_spent from abstract_session GROUP BY strftime('%Y', session_date), week_number"
-        const val QUERY_SESSIONS_BY_MONTHS =
+            "SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, CAST(strftime('%Y', session_date) as INTEGER) as year_number, CAST(week_number as INTEGER) as period_number, '[' || strftime('%d', date(session_date, '-6 days', 'weekday 0')) || '-' || strftime('%d', date(session_date, '-6 days', 'weekday 0', '+6 days')) || ']\n' || strftime('%m', date(session_date, '-6 days', 'weekday 0')) || '-' || substr(strftime('%Y', date(session_date, '-6 days', 'weekday 0')), 3, 2) as label, SUM(session_time)/60 as time_spent from abstract_session GROUP BY strftime('%Y', session_date), week_number"
+        const val QUERY_SESSIONS_BY_MONTHS=
             "SELECT SUM(sets) as sets, SUM(convos) as convos, SUM(contacts) as contacts, AVG(`index`) as avg_index, AVG(convo_ratio) as avg_convo_ratio, AVG(contact_ratio)  as avg_contact_ratio, CAST(strftime('%Y', session_date) as INTEGER) as year_number, CAST(strftime('%m', session_date) as INTEGER) as period_number, CAST(strftime('%Y', session_date) as INTEGER) || '-' || CAST(strftime('%m', session_date) as INTEGER) as label, SUM(session_time)/60 as time_spent from abstract_session GROUP BY strftime('%Y', session_date), strftime('%m', session_date)"
     }
 
