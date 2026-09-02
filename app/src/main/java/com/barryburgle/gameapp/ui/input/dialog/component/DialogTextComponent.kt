0dd6115e3c0dcb100ee8118c393d09f2fb89eb66
Barry Burgle <barryburgle@gmail.com>
2026-09-02 23:42:40 +0200

    refactored dialog text component

--- app/src/main/java/com/barryburgle/gameapp/ui/input/dialog/component/DialogTextComponent.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/dialog/component/DialogTextComponent.kt
@@ -1,57 +1,84 @@
 package com.barryburgle.gameapp.ui.input.dialog.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.barryburgle.gameapp.ui.utilities.button.IconShadowButton
+import com.barryburgle.gameapp.ui.utilities.button.LittleIconButton
 import com.barryburgle.gameapp.ui.utilities.text.body.LittleBodyText
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DialogTextComponent(
     value: String,
     placeholder: String,
-    height: Dp,
     emptyValue: String,
+    validContent: Boolean,
+    onCopyClick: () -> Unit,
     onValueChange: (String) -> Unit
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.large)
+            .background(MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.07f)),
+        verticalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(
-            value = value,
-            onValueChange = { onValueChange(it) },
-            placeholder = { LittleBodyText(placeholder) },
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier
-                .height(height)
-                .fillMaxWidth(0.75f)
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(
-            modifier = Modifier
-                .height(height),
-            verticalArrangement = Arrangement.Center
+        Row(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            IconShadowButton(
-                onClick = {
-                    onValueChange(emptyValue)
-                },
-                imageVector = Icons.Default.Delete,
-                contentDescription = "Delete " + placeholder
-            )
+            Column(modifier = Modifier.fillMaxWidth(0.85f)) {
+                TextField(
+                    value = value,
+                    onValueChange = { onValueChange(it) },
+                    placeholder = { LittleBodyText(placeholder) },
+                    shape = MaterialTheme.shapes.large,
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        errorContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+            if (validContent) {
+                Column(
+                    modifier = Modifier.height(100.dp),
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    LittleIconButton(
+                        onClick = {
+                            onValueChange(emptyValue)
+                        },
+                        imageVector = Icons.Default.Delete,
+                        height = 15.dp
+                    )
+                    LittleIconButton(
+                        onClick = onCopyClick,
+                        imageVector = Icons.Default.ContentCopy,
+                        height = 15.dp
+                    )
+                }
+            }
         }
     }
 }
