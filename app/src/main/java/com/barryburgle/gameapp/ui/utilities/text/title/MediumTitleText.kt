54de987f9254b1614b3392b01bf4b3a6b039209f
Barry Burgle <barryburgle@gmail.com>
2026-09-23 12:33:31 +0200

    pass color

--- app/src/main/java/com/barryburgle/gameapp/ui/utilities/text/title/MediumTitleText.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/utilities/text/title/MediumTitleText.kt
@@ -3,25 +3,27 @@ package com.barryburgle.gameapp.ui.utilities.text.title
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun MediumTitleText(
     text: String,
-    black: Boolean = false
+    black: Boolean = false,
+    color: Color = MaterialTheme.colorScheme.onPrimary
 ) {
     if (black) {
         Text(
             text = text,
             style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.onPrimary,
+            color = color,
             fontWeight = FontWeight.Black
         )
     } else {
         Text(
             text = text,
             style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.onPrimary
+            color = color
         )
     }
 }
