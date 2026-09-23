cbdefd7172a2f6382001252e53e6362a83723527
Barry Burgle <barryburgle@gmail.com>
2026-09-23 14:16:32 +0200

    ping dao

---
+++ app/src/main/java/com/barryburgle/gameapp/dao/ping/PingDao.kt
@@ -0,0 +1,24 @@
+package com.barryburgle.gameapp.dao.ping
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import com.barryburgle.gameapp.model.ping.Ping
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PingDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insert(ping: Ping): Long
+
+    @Insert(onConflict = REPLACE)
+    suspend fun batchInsert(pings: List<Ping>)
+
+    @Query("SELECT * FROM ping ORDER BY id DESC")
+    fun getAll(): Flow<List<Ping>>
+
+    @Query("DELETE FROM ping WHERE id = :id")
+    suspend fun deleteById(id: Long)
+}
