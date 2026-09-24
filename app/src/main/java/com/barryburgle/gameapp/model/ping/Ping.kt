0b7fe1016d066af6e5309c9b9fe5c4ba0aa7c0c5
Barry Burgle <barryburgle@gmail.com>
2026-09-24 12:31:46 +0200

    equal & hash methods

--- app/src/main/java/com/barryburgle/gameapp/model/ping/Ping.kt
+++ app/src/main/java/com/barryburgle/gameapp/model/ping/Ping.kt
@@ -15,4 +15,28 @@ open class Ping(
     @ColumnInfo(name = "lead_ids") var leadIds: List<Long> = emptyList()
 ) {
     constructor() : this(0, "", null, null, null, null, emptyList())
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Ping) return false
+
+        return id == other.id &&
+                title == other.title &&
+                body == other.body &&
+                link == other.link &&
+                pic == other.pic &&
+                audio == other.audio &&
+                leadIds == other.leadIds
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + (body?.hashCode() ?: 0)
+        result = 31 * result + (link?.hashCode() ?: 0)
+        result = 31 * result + (pic?.hashCode() ?: 0)
+        result = 31 * result + (audio?.hashCode() ?: 0)
+        result = 31 * result + leadIds.hashCode()
+        return result
+    }
 }
