6e4a2ab1c83fd7cf3b9a356c774bccd08914d7c5
Barry Burgle <barryburgle@gmail.com>
2026-09-26 01:24:42 +0200

    removed as field removed from parent class

--- app/src/main/java/com/barryburgle/gameapp/ui/input/state/InputState.kt
+++ app/src/main/java/com/barryburgle/gameapp/ui/input/state/InputState.kt
@@ -151,7 +151,6 @@ data class InputState(
     exportSetsFileName,
     exportChallengesFileName,
     exportPinPointsFileName,
-    "",// Not passing a valid exportPingsFileName because it is not used from InputScreen features for any pings export
     "",// Not passing a valid exportSettingsFileName because it is not used from InputScreen features for any settings export
     exportFolder,
     backupFolder,
